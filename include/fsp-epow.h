4b03c677619ee827037c6203523662257c082254
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2014-07-30 15:01:36 +1000

    epow: Fix licence header in fsp-epow.h
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- include/fsp-epow.h
+++ include/fsp-epow.h
@@ -1,10 +1,17 @@
-/*
- * (C) Copyright IBM Corp., 2013 and provided pursuant to the Technology
- * Licensing Agreement between Google Inc. and International Business
- * Machines Corporation, IBM License Reference Number AA130103030256 and
- * confidentiality governed by the Parties’ Mutual Nondisclosure Agreement
- * number V032404DR, executed by the parties on November 6, 2007, and
- * Supplement V032404DR-3 dated August 16, 2012 (the “NDA”).
+/* Copyright 2013-2014 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 /*
