e8d8528692f69046238ddfc0901189821fb9e4d5
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2014-07-30 15:02:56 +1000

    libflash: Fix licence header in ffs.h
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- libflash/ffs.h
+++ libflash/ffs.h
@@ -13,28 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-/* IBM_PROLOG_BEGIN_TAG                                                   */
-/* This is an automatically generated prolog.                             */
-/*                                                                        */
-/* $Source: src/usr/pnor/ffs.h $                                          */
-/*                                                                        */
-/* IBM CONFIDENTIAL                                                       */
-/*                                                                        */
-/* COPYRIGHT International Business Machines Corp. 2012,2013              */
-/*                                                                        */
-/* p1                                                                     */
-/*                                                                        */
-/* Object Code Only (OCO) source materials                                */
-/* Licensed Internal Code Source Materials                                */
-/* IBM HostBoot Licensed Internal Code                                    */
-/*                                                                        */
-/* The source code for this program is not published or otherwise         */
-/* divested of its trade secrets, irrespective of what has been           */
-/* deposited with the U.S. Copyright Office.                              */
-/*                                                                        */
-/* Origin: 30                                                             */
-/*                                                                        */
-/* IBM_PROLOG_END_TAG                                                     */
 /*
  * Copyright (c) International Business Machines Corp., 2012
  *
