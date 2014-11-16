ff97b2af8d39fed079e949732fc0b45fa0a6a3a9
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2014-11-17 10:43:07 +1100

    phb3: Increase some timeouts (SW283991)
    This increase various timeouts as per CQ SW283991 which should help
    with some external drawers and GPUs.
    
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>

--- hw/phb3.c
+++ hw/phb3.c
@@ -3615,8 +3615,10 @@ static void phb3_init_utl(struct phb3 *p)
 	 */
 	out_be64(p->regs + UTL_PCIE_TAGS_ALLOC,            0x0800000000000000);
 
-	/* Init_82: PCI Express port control */
-	out_be64(p->regs + UTL_PCIE_PORT_CONTROL,          0x8588006000000000);
+	/* Init_82: PCI Express port control
+	 * SW283991: Set Outbound Non-Posted request timeout to 16ms (RTOS).
+	 */
+	out_be64(p->regs + UTL_PCIE_PORT_CONTROL,          0x8588007000000000);
 
 	/* Init_83..85: Clean & setup port errors */
 	out_be64(p->regs + UTL_PCIE_PORT_STATUS,           0xffdfffffffffffff);
@@ -3871,8 +3873,10 @@ static void phb3_init_hw(struct phb3 *p)
 	if (p->rev == PHB3_REV_MURANO_DD20)
 		phb3_write_reg_asb(p, PHB_TCE_WATERMARK,	0x0003000000030302);
 
-	/* Init_142 - PHB3 - Timeout Control Register 1 */
-	out_be64(p->regs + PHB_TIMEOUT_CTRL1,			0x1713132016200000);
+	/* Init_142 - PHB3 - Timeout Control Register 1
+	 * SW283991: Increase timeouts
+	 */
+	out_be64(p->regs + PHB_TIMEOUT_CTRL1,			0x1715152016200000);
 
 	/* Init_143 - PHB3 - Timeout Control Register 2 */
 	out_be64(p->regs + PHB_TIMEOUT_CTRL2,			0x2320d71600000000);
