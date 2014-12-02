open-power/skiboot

commit cf6f4e8912d29fb89ce85c84834607065ad595a5

fsp/elog: Create a logging frontend
In order to support fsp-less machines we need to be able to log errors
using a BMC or some other mechanism. Currently the error logging code
is tightly coupled to the platform making it difficult to add
different platforms.

This patch factors out the generic parts of the error logging code in
preparation for adding different logging backends. It also adds a
generic mechanism for pre-allocating a specific number of objects.

Signed-off-by: Alistair Popple <alistair@popple.id.au>
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

core/errorlog.c
@@ -1,0 +1,176 @@
+/* Copyright 2013-2014 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* This file contains the front end for OPAL error logging. It is used
+ * to construct a struct errorlog representing the event/error to be
+ * logged which is then passed to the platform specific backend to log
+ * the actual errors.
+ */
+#include <skiboot.h>
+#include <lock.h>
+#include <errorlog.h>
+#include <fsp-elog.h>
+#include <pool.h>
+
+/*
+ * Maximum number buffers that are pre-allocated
+ * to hold elogs that are reported on Sapphire and
+ * powernv.
+ */
+#define ELOG_WRITE_MAX_RECORD		64
+
+/* Platform Log ID as per the spec */
+static uint32_t sapphire_elog_id = 0xB0000000;
+/* Reserved for future use */
+/* static uint32_t powernv_elog_id = 0xB1000000; */
+
+/* Pool to allocate elog messages from */
+static struct pool elog_pool;
+static struct lock elog_lock = LOCK_UNLOCKED;
+
+static struct errorlog *get_write_buffer(int opal_event_severity)
+{
+	struct errorlog *buf;
+
+	lock(&elog_lock);
+	if (opal_event_severity == OPAL_ERROR_PANIC)
+		buf = pool_get(&elog_pool, POOL_HIGH);
+	else
+		buf = pool_get(&elog_pool, POOL_NORMAL);
+	unlock(&elog_lock);
+	return buf;
+}
+
+int opal_elog_update_user_dump(struct errorlog *buf, unsigned char *data,
+					      uint32_t tag, uint16_t size)
+{
+	char *buffer;
+	struct elog_user_data_section *tmp;
+
+	if (!buf) {
+		prerror("ELOG: Cannot update user data. Buffer is invalid\n");
+		return -1;
+	}
+
+	buffer = (char *)buf->user_data_dump + buf->user_section_size;
+	if ((buf->user_section_size + size) > OPAL_LOG_MAX_DUMP) {
+		prerror("ELOG: Size of dump data overruns buffer\n");
+		return -1;
+	}
+
+	tmp = (struct elog_user_data_section *)buffer;
+	tmp->tag = tag;
+	tmp->size = size + sizeof(struct elog_user_data_section) - 1;
+	memcpy(tmp->data_dump, data, size);
+
+	buf->user_section_size += tmp->size;
+	buf->user_section_count++;
+	return 0;
+}
+
+/* Reporting of error via struct errorlog */
+struct errorlog *opal_elog_create(struct opal_err_info *e_info)
+{
+	struct errorlog *buf;
+
+	buf = get_write_buffer(e_info->sev);
+	if (buf) {
+		buf->error_event_type = e_info->err_type;
+		buf->component_id = e_info->cmp_id;
+		buf->subsystem_id = e_info->subsystem;
+		buf->event_severity = e_info->sev;
+		buf->event_subtype = e_info->event_subtype;
+		buf->reason_code = e_info->reason_code;
+		buf->elog_origin = ORG_SAPPHIRE;
+
+		lock(&elog_lock);
+		buf->plid = ++sapphire_elog_id;
+		unlock(&elog_lock);
+	}
+
+	return buf;
+}
+
+void opal_elog_complete(struct errorlog *buf, bool success)
+{
+	if (!success)
+		printf("Unable to log error\n");
+
+	lock(&elog_lock);
+	pool_free_object(&elog_pool, buf);
+	unlock(&elog_lock);
+}
+
+void log_error(struct opal_err_info *e_info, void *data, uint16_t size,
+	       const char *fmt, ...)
+{
+	struct errorlog *buf;
+	int tag = 0x44455343;  /* ASCII of DESC */
+	va_list list;
+	char err_msg[250];
+
+	va_start(list, fmt);
+	vsnprintf(err_msg, sizeof(err_msg), fmt, list);
+	va_end(list);
+
+	/* Log the error on to Sapphire console */
+	prerror("%s", err_msg);
+
+	buf = opal_elog_create(e_info);
+	if (buf == NULL)
+		prerror("ELOG: Error getting buffer to log error\n");
+	else {
+		opal_elog_update_user_dump(buf, err_msg, tag, strlen(err_msg));
+		/* Append any number of call out dumps */
+		if (e_info->call_out)
+			e_info->call_out(buf, data, size);
+		if (elog_fsp_commit(buf))
+			prerror("ELOG: Re-try error logging\n");
+	}
+}
+
+void log_simple_error(struct opal_err_info *e_info, const char *fmt, ...)
+{
+	struct errorlog *buf;
+	int tag = 0x44455343;  /* ASCII of DESC */
+	va_list list;
+	char err_msg[250];
+
+	va_start(list, fmt);
+	vsnprintf(err_msg, sizeof(err_msg), fmt, list);
+	va_end(list);
+
+	/* Log the error on to Sapphire console */
+	prerror("%s", err_msg);
+
+	buf = opal_elog_create(e_info);
+	if (buf == NULL)
+		prerror("ELOG: Error getting buffer to log error\n");
+	else {
+		opal_elog_update_user_dump(buf, err_msg, tag, strlen(err_msg));
+		if (elog_fsp_commit(buf))
+			prerror("ELOG: Re-try error logging\n");
+	}
+}
+
+int elog_init(void)
+{
+	/* pre-allocate memory for records */
+	if (pool_init(&elog_pool, sizeof(struct errorlog), ELOG_WRITE_MAX_RECORD, 1))
+		return OPAL_RESOURCE;
+
+	return 0;
+}

include/errorlog.h
@@ -97,6 +97,10 @@
 /* Max user dump size is 14K	*/
 #define OPAL_LOG_MAX_DUMP	14336
 
+/* Origin of error, elog_origin */
+#define ORG_SAPPHIRE	1
+#define ORG_POWERNV	2
+
 /* Multiple user data sections */
 struct __attribute__((__packed__))elog_user_data_section {
 	uint32_t tag;
@@ -132,4 +136,41 @@ struct __attribute__((__packed__)) errorlog {
 	char user_data_dump[OPAL_LOG_MAX_DUMP];
 	struct list_node link;
 };
+
+struct opal_err_info {
+	uint32_t reason_code;
+	uint8_t err_type;
+	uint16_t cmp_id;
+	uint8_t subsystem;
+	uint8_t sev;
+	uint8_t event_subtype;
+	void (*call_out)(struct errorlog *buf, void *data, uint16_t size);
+};
+
+#define DEFINE_LOG_ENTRY(reason, type, id, subsys,			\
+severity, subtype, callout_func) struct opal_err_info err_##reason =	\
+{ .reason_code = reason, .err_type = type, .cmp_id = id,		\
+.subsystem = subsys, .sev = severity, .event_subtype = subtype,		\
+.call_out = callout_func }
+
+/* This is wrapper around the error log function, which creates
+ * and commits the error to FSP.
+ * Used for simple error logging
+ */
+void log_simple_error(struct opal_err_info *e_info, const char *fmt, ...) __attribute__ ((format (printf, 2, 3)));
+void log_error(struct opal_err_info *e_info, void *data, uint16_t size,
+		const char *fmt, ...) __attribute__ ((format (printf, 4, 5)));
+
+int opal_elog_update_user_dump(struct errorlog *buf, unsigned char *data,
+						uint32_t tag, uint16_t size);
+
+struct errorlog *opal_elog_create(struct opal_err_info *e_info);
+
+/* Called by the backend after an error has been logged by the
+ * backend. If the error could not be logged successfully success is
+ * set to false. */
+void opal_elog_complete(struct errorlog *elog, bool success);
+
+int elog_init(void);
+
 #endif /* __ERRORLOG_H */

include/fsp-elog.h
@@ -193,16 +193,6 @@ enum opal_reasoncode {
 				= OPAL_FP | 0x10,
 };
 
-struct opal_err_info {
-	uint32_t reason_code;
-	uint8_t err_type;
-	uint16_t cmp_id;
-	uint8_t subsystem;
-	uint8_t sev;
-	uint8_t event_subtype;
-	void (*call_out)(struct errorlog *buf, void *data, uint16_t size);
-};
-
 #define DEFINE_LOG_ENTRY(reason, type, id, subsys,			\
 severity, subtype, callout_func) struct opal_err_info err_##reason =	\
 { .reason_code = reason, .err_type = type, .cmp_id = id,		\

.gitignore
@@ -27,6 +27,7 @@ core/test/run-mem_region_release_unused
 core/test/run-mem_region_release_unused_noalloc
 core/test/run-msg
 core/test/run-pel
+core/test/run-pool
 core/test/run-trace
 core/test/*-gcov
 external/dump_trace

core/Makefile.inc
@@ -6,7 +6,7 @@ CORE_OBJS += malloc.o lock.o cpu.o utils.o fdt.o opal.o interrupts.o
 CORE_OBJS += timebase.o opal-msg.o pci.o pci-opal.o fast-reboot.o
 CORE_OBJS += device.o exceptions.o trace.o affinity.o vpd.o
 CORE_OBJS += hostservices.o platform.o nvram.o flash-nvram.o hmi.o
-CORE_OBJS += console-log.o ipmi.o time-utils.o pel.o
+CORE_OBJS += console-log.o ipmi.o time-utils.o pel.o pool.o errorlog.o
 CORE=core/built-in.o
 
 $(CORE): $(CORE_OBJS:%=core/%)

core/pool.c
@@ -1,0 +1,80 @@
+/* Copyright 2013-2014 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* This file provides some functions to manage a pool of pre-allocated
+ * objects. It also provides a method to reserve a pre-defined number
+ * of objects for higher priorty requests. The allocations follow the
+ * following rules:
+ *
+ * 1. An allocation will succeed at any priority if there is more than
+ *    the reserved number of objects free.
+ * 2. Only high priority allocations will succeed when there are less
+ *    than the reserved number of objects free.
+ * 3. When an allocation is freed it is always added to the high priority
+ *    pool if there are less than the reserved number of allocations
+ *    available.
+ */
+
+#include <pool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ccan/list/list.h>
+
+void* pool_get(struct pool *pool, enum pool_priority priority)
+{
+	void *obj;
+
+	if (!pool->free_count ||
+	    ((pool->free_count <= pool->reserved) && priority == POOL_NORMAL))
+		return NULL;
+
+	pool->free_count--;
+	obj = (void *) list_pop_(&pool->free_list, 0);
+	assert(obj);
+	memset(obj, 0, pool->obj_size);
+	return obj;
+}
+
+void pool_free_object(struct pool *pool, void *obj)
+{
+	pool->free_count++;
+	list_add_tail(&pool->free_list,
+		      (struct list_node *) (obj));
+}
+
+int pool_init(struct pool *pool, size_t obj_size, int count, int reserved)
+{
+	int i;
+
+	if (obj_size < sizeof(struct list_node))
+		obj_size = sizeof(struct list_node);
+
+	assert(count >= reserved);
+	pool->buf = malloc(obj_size*count);
+	if (!pool->buf)
+		return -1;
+
+	pool->obj_size = obj_size;
+	pool->free_count = count;
+	pool->reserved = reserved;
+	list_head_init(&pool->free_list);
+
+	for(i = 0; i < count; i++)
+		list_add_tail(&pool->free_list,
+			      (struct list_node *) (pool->buf + obj_size*i));
+
+	return 0;
+}

commit 28a178751b4195e546e0e8d429f988793c18f848

elog: Clean up error logging headers
Commit cf6f4e8912d29fb89ce85c84834607065ad595a5 introduced a platform
independent frontend for error logging. However it failed to move the
generic parts of the fsp-elog.h header into the platform independent
one, instead relying on the fact that up until now fsp-elog.h was
included whenever a function needed to log errors.

This patch moves the platform independent defines into the frontend
header file (errorlog.h) and removes the include of the platform
specific header in generic code paths.

Signed-off-by: Alistair Popple <alistair@popple.id.au>
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

core/errorlog.c
@@ -22,7 +22,7 @@
 #include <skiboot.h>
 #include <lock.h>
 #include <errorlog.h>
-#include <fsp-elog.h>
+#include <errorlog.h>
 #include <pool.h>
 
 /*

core/pel.c
@@ -1,5 +1,5 @@
 #include <string.h>
-#include <fsp-elog.h>
+#include <errorlog.h>
 #include <device.h>
 #include <fsp.h>
 #include <pel.h>

hw/fsp/fsp-codeupdate.c
@@ -21,7 +21,7 @@
 #include <lock.h>
 #include <device.h>
 #include <ccan/endian/endian.h>
-#include <fsp-elog.h>
+#include <errorlog.h>
 
 #include "fsp-codeupdate.h"
 

hw/fsp/fsp-dump.c
@@ -40,7 +40,7 @@
 #include <lock.h>
 #include <device.h>
 #include <skiboot.h>
-#include <fsp-elog.h>
+#include <errorlog.h>
 
 /*
  * Max outstanding dumps to retrieve

hw/fsp/fsp-leds.c
@@ -30,7 +30,7 @@
 #include <timebase.h>
 #include <hdata/spira.h>
 #include <hdata/hdata.h>
-#include <fsp-elog.h>
+#include <errorlog.h>
 
 #include "fsp-leds.h"
 

hw/fsp/fsp-mdst-table.c
@@ -31,7 +31,7 @@
 #include <opal.h>
 #include <lock.h>
 #include <skiboot.h>
-#include <fsp-elog.h>
+#include <errorlog.h>
 #include <fsp-mdst-table.h>
 
 /*

hw/fsp/fsp-mem-err.c
@@ -19,7 +19,7 @@
 #include <opal-msg.h>
 #include <lock.h>
 #include <fsp.h>
-#include <fsp-elog.h>
+#include <errorlog.h>
 
 /* debug message prefix */
 #define PREFIX			"FSPMEMERR: "

hw/fsp/fsp-nvram.c
@@ -19,7 +19,7 @@
 #include <opal.h>
 #include <lock.h>
 #include <device.h>
-#include <fsp-elog.h>
+#include <errorlog.h>
 
 /*
  * The FSP NVRAM API operates in "blocks" of 4K. It is entirely exposed

hw/fsp/fsp-op-panel.c
@@ -21,7 +21,7 @@
 #include <device.h>
 #include <processor.h>
 #include <opal-msg.h>
-#include <fsp-elog.h>
+#include <errorlog.h>
 
 DEFINE_LOG_ENTRY(OPAL_RC_PANEL_WRITE, OPAL_PLATFORM_ERR_EVT, OPAL_OP_PANEL,
 		 OPAL_MISC_SUBSYSTEM, OPAL_PREDICTIVE_ERR_GENERAL,

hw/fsp/fsp-rtc.c
@@ -21,7 +21,7 @@
 #include <time.h>
 #include <time-utils.h>
 #include <opal-msg.h>
-#include <fsp-elog.h>
+#include <errorlog.h>
 #include <device.h>
 
 /*

hw/fsp/fsp-sensor.c
@@ -35,7 +35,7 @@
 #include <device.h>
 #include <spcn.h>
 #include <opal-msg.h>
-#include<fsp-elog.h>
+#include<errorlog.h>
 
 #define INVALID_DATA	((uint32_t)-1)
 

hw/fsp/fsp-surveillance.c
@@ -19,7 +19,7 @@
 #include <processor.h>
 #include <timebase.h>
 #include <fsp-sysparam.h>
-#include <fsp-elog.h>
+#include <errorlog.h>
 
 static bool fsp_surv_state = false;
 static bool fsp_surv_ack_pending = false;

hw/fsp/fsp.c
@@ -34,7 +34,7 @@
 #include <trace.h>
 #include <timebase.h>
 #include <cpu.h>
-#include <fsp-elog.h>
+#include <errorlog.h>
 #include <opal.h>
 #include <opal-msg.h>
 

hw/lpc-uart.c
@@ -21,7 +21,7 @@
 #include <device.h>
 #include <interrupts.h>
 #include <processor.h>
-#include <fsp-elog.h>
+#include <errorlog.h>
 #include <trace.h>
 #include <timebase.h>
 #include <cpu.h>

hw/lpc.c
@@ -21,7 +21,7 @@
 #include <chip.h>
 #include <lpc.h>
 #include <timebase.h>
-#include <fsp-elog.h>
+#include <errorlog.h>
 
 DEFINE_LOG_ENTRY(OPAL_RC_LPC_READ, OPAL_PLATFORM_ERR_EVT, OPAL_LPC,
 		 OPAL_MISC_SUBSYSTEM, OPAL_PREDICTIVE_ERR_GENERAL,

hw/occ.c
@@ -23,7 +23,7 @@
 #include <fsp.h>
 #include <timebase.h>
 #include <hostservices.h>
-#include <fsp-elog.h>
+#include <errorlog.h>
 
 /* OCC Communication Area for PStates */
 

hw/slw.c
@@ -26,7 +26,7 @@
 #include <chiptod.h>
 #include <interrupts.h>
 #include <timebase.h>
-#include <fsp-elog.h>
+#include <errorlog.h>
 #include <libfdt/libfdt.h>
 
 #ifdef __HAVE_LIBPORE__

hw/xscom.c
@@ -21,7 +21,7 @@
 #include <device.h>
 #include <chip.h>
 #include <centaur.h>
-#include <fsp-elog.h>
+#include <errorlog.h>
 
 /* Mask of bits to clear in HMER before an access */
 #define HMER_CLR_MASK	(~(SPR_HMER_XSCOM_FAIL | \

include/errorlog.h
@@ -17,6 +17,10 @@
 #ifndef __ERRORLOG_H
 #define __ERRORLOG_H
 
+#include <opal.h>
+#include <stdint.h>
+#include <ccan/list/list.h>
+
 /* Classification of error/events type reported on OPAL */
 /* Platform Events/Errors: Report Machine Check Interrupt */
 #define OPAL_PLATFORM_ERR_EVT		0x01
@@ -147,6 +151,172 @@ struct opal_err_info {
 	void (*call_out)(struct errorlog *buf, void *data, uint16_t size);
 };
 
+/* Component IDs */
+/* In PEL error log format, Creator ID is hypervisor
+ * But we can have various component ID to distinguish
+ * which component in hypervisor is reporting the error
+ * This is 2 bytes long,
+ *	first byte corresponds to Component IDs
+ *	Second byte is reserved for the Reason code.
+ * Component ID is mapped to readable 4-digit ascii
+ * character name in FSP and displayed.
+ */
+/* SAPPHIRE components */
+#define OPAL_CODEUPDATE			0x4355	/* CU */
+#define OPAL_CONSOLE			0x434E	/* CN */
+#define OPAL_CEC			0x4345	/* CE */
+#define OPAL_CHIP			0x4348	/* CH */
+#define OPAL_ELOG			0x454C	/* EL */
+#define OPAL_NVRAM			0x4E56	/* NV */
+#define OPAL_RTC			0x5254	/* RT */
+#define OPAL_SURVEILLANCE		0x5355	/* SU */
+#define OPAL_SYSPARAM			0x5350	/* SP */
+#define OPAL_LPC			0x4C50	/* LP */
+#define OPAL_UART			0x5541	/* UA */
+#define OPAL_OCC			0x4F43	/* OC */
+#define OPAL_OP_PANEL			0x4F50	/* OP */
+#define OPAL_PHB3			0x5048	/* PH */
+#define OPAL_PSI			0x5053	/* PS */
+#define OPAL_VPD			0x5650	/* VP */
+#define OPAL_XSCOM			0x5853	/* XS */
+#define OPAL_PCI			0x5043	/* PC */
+#define OPAL_MISC			0x4D49	/* MI */
+#define OPAL_ATTN			0x4154	/* AT */
+#define OPAL_MEM_ERR			0x4D45	/* ME */
+#define OPAL_CENTAUR			0x4354	/* CT */
+#define OPAL_MFSI			0x4D46	/* MF */
+#define OPAL_DUMP			0x4455	/* DU */
+#define OPAL_LED			0x4C45	/* LE */
+#define OPAL_SENSOR			0x5345	/* SE */
+#define OPAL_SLW			0x534C	/* SL */
+#define OPAL_FSP			0x4650	/* FP */
+#define OPAL_I2C			0x4943	/* IC */
+
+/* SAPPHIRE SRC componenet ID*/
+#define OPAL_CU				0x1000
+#define OPAL_CN				0x2000
+#define OPAL_CE				0x3000
+#define OPAL_CH				0x4000
+#define OPAL_EL				0x5000
+#define OPAL_NV				0x6000
+#define OPAL_RT				0x7000
+#define OPAL_SU				0x8000
+#define OPAL_SP				0x9000
+#define OPAL_LP				0xa000
+#define OPAL_UA				0xb000
+#define OPAL_OC				0xc000
+#define OPAL_OP				0xd000
+#define OPAL_PH				0xe000
+#define OPAL_PS				0xf000
+#define OPAL_VP				0x1000
+#define OPAL_XS				0x1100
+#define OPAL_PC				0x1200
+#define OPAL_MI				0x1300
+#define OPAL_AT				0x1400
+#define OPAL_ME				0x1500
+#define OPAL_CT				0x1600
+#define OPAL_MF				0x1700
+#define OPAL_DU				0x1800
+#define OPAL_LE				0x1900
+#define OPAL_SE				0x2000
+#define OPAL_SL				0x2100
+#define OPAL_FP				0x2200
+#define OPAL_IC				0x2300
+
+enum opal_reasoncode {
+/* code update */
+	OPAL_RC_CU_FLASH        = OPAL_CU | 0x10,
+	OPAL_RC_CU_INIT         = OPAL_CU | 0x11,
+	OPAL_RC_CU_SG_LIST      = OPAL_CU | 0x12,
+	OPAL_RC_CU_COMMIT	= OPAL_CU | 0x13,
+	OPAL_RC_CU_MSG		= OPAL_CU | 0x14,
+	OPAL_RC_CU_NOTIFY       = OPAL_CU | 0x15,
+	OPAL_RC_CU_MARKER_LID	= OPAL_CU | 0x16,
+/* NVRAM */
+	OPAL_RC_NVRAM_INIT      = OPAL_NV | 0x10,
+	OPAL_RC_NVRAM_OPEN	= OPAL_NV | 0x11,
+	OPAL_RC_NVRAM_SIZE      = OPAL_NV | 0x12,
+	OPAL_RC_NVRAM_WRITE     = OPAL_NV | 0x13,
+	OPAL_RC_NVRAM_READ      = OPAL_NV | 0x14,
+/* CENTAUR */
+	OPAL_RC_CENTAUR_INIT    = OPAL_CT | 0x10,
+	OPAL_RC_CENTAUR_RW_ERR  = OPAL_CT | 0x11,
+/* MFSI */
+	OPAL_RC_MFSI_RW_ERR     = OPAL_MF | 0x10,
+/* UART */
+	OPAL_RC_UART_INIT       = OPAL_UA | 0x10,
+/* OCC */
+	OPAL_RC_OCC_RESET       = OPAL_OC | 0x10,
+	OPAL_RC_OCC_LOAD        = OPAL_OC | 0x11,
+	OPAL_RC_OCC_PSTATE_INIT = OPAL_OC | 0x12,
+	OPAL_RC_OCC_TIMEOUT	= OPAL_OC | 0x13,
+/* RTC	*/
+	OPAL_RC_RTC_READ	= OPAL_RT | 0x10,
+	OPAL_RC_RTC_TOD		= OPAL_RT | 0x11,
+/* SURVEILLANCE */
+	OPAL_RC_SURVE_INIT      = OPAL_SU | 0x10,
+	OPAL_RC_SURVE_STATUS	= OPAL_SU | 0x11,
+	OPAL_RC_SURVE_ACK	= OPAL_SU | 0x12,
+/* SYSPARAM */
+	OPAL_RC_SYSPARM_INIT    = OPAL_SP | 0x10,
+	OPAL_RC_SYSPARM_MSG     = OPAL_SP | 0x11,
+/* LPC */
+	OPAL_RC_LPC_READ        = OPAL_LP | 0x10,
+	OPAL_RC_LPC_WRITE       = OPAL_LP | 0x11,
+/* OP_PANEL */
+	OPAL_RC_PANEL_WRITE     = OPAL_OP | 0x10,
+/* PSI */
+	OPAL_RC_PSI_INIT        = OPAL_PS | 0x10,
+	OPAL_RC_PSI_IRQ_RESET   = OPAL_PS | 0x11,
+/* XSCOM */
+	OPAL_RC_XSCOM_RW		= OPAL_XS | 0x10,
+	OPAL_RC_XSCOM_INDIRECT_RW	= OPAL_XS | 0x11,
+	OPAL_RC_XSCOM_RESET		= OPAL_XS | 0x12,
+/* PCI */
+	OPAL_RC_PCI_INIT_SLOT   = OPAL_PC | 0x10,
+	OPAL_RC_PCI_ADD_SLOT    = OPAL_PC | 0x11,
+	OPAL_RC_PCI_SCAN        = OPAL_PC | 0x12,
+	OPAL_RC_PCI_RESET_PHB   = OPAL_PC | 0x10,
+/* ATTN */
+	OPAL_RC_ATTN		= OPAL_AT | 0x10,
+/* MEM_ERR */
+	OPAL_RC_MEM_ERR_RES	= OPAL_ME | 0x10,
+	OPAL_RC_MEM_ERR_DEALLOC	= OPAL_ME | 0x11,
+/* DUMP */
+	OPAL_RC_DUMP_INIT	= OPAL_DU | 0x10,
+	OPAL_RC_DUMP_LIST	= OPAL_DU | 0x11,
+	OPAL_RC_DUMP_ACK	= OPAL_DU | 0x12,
+	OPAL_RC_DUMP_MDST_INIT	= OPAL_DU | 0x13,
+	OPAL_RC_DUMP_MDST_UPDATE= OPAL_DU | 0x14,
+	OPAL_RC_DUMP_MDST_ADD	= OPAL_DU | 0x15,
+	OPAL_RC_DUMP_MDST_REMOVE= OPAL_DU | 0x16,
+/* LED	*/
+	OPAL_RC_LED_SPCN	= OPAL_LE | 0x10,
+	OPAL_RC_LED_BUFF	= OPAL_LE | 0x11,
+	OPAL_RC_LED_LC		= OPAL_LE | 0x12,
+	OPAL_RC_LED_STATE	= OPAL_LE | 0x13,
+	OPAL_RC_LED_SUPPORT	= OPAL_LE | 0x14,
+/* SENSOR */
+	OPAL_RC_SENSOR_INIT	= OPAL_SE | 0x10,
+	OPAL_RC_SENSOR_READ	= OPAL_SE | 0x11,
+	OPAL_RC_SENSOR_ASYNC_COMPLETE
+				= OPAL_SE | 0x12,
+/* SLW */
+	OPAL_RC_SLW_INIT	= OPAL_SL | 0x10,
+	OPAL_RC_SLW_SET		= OPAL_SL | 0x11,
+	OPAL_RC_SLW_GET		= OPAL_SL | 0x12,
+	OPAL_RC_SLW_REG		= OPAL_SL | 0x13,
+/* FSP	*/
+	OPAL_RC_FSP_POLL_TIMEOUT
+				= OPAL_FP | 0x10,
+/* I2C */
+	OPAL_RC_I2C_INIT	= OPAL_IC | 0X10,
+	OPAL_RC_I2C_START_REQ	= OPAL_IC | 0X11,
+	OPAL_RC_I2C_TIMEOUT	= OPAL_IC | 0x12,
+	OPAL_RC_I2C_TRANSFER	= OPAL_IC | 0x13,
+	OPAL_RC_I2C_RESET	= OPAL_IC | 0x14,
+};
+
 #define DEFINE_LOG_ENTRY(reason, type, id, subsys,			\
 severity, subtype, callout_func) struct opal_err_info err_##reason =	\
 { .reason_code = reason, .err_type = type, .cmp_id = id,		\
@@ -161,10 +331,12 @@ void log_simple_error(struct opal_err_info *e_info, const char *fmt, ...) __attr
 void log_error(struct opal_err_info *e_info, void *data, uint16_t size,
 		const char *fmt, ...) __attribute__ ((format (printf, 4, 5)));
 
-int opal_elog_update_user_dump(struct errorlog *buf, unsigned char *data,
-						uint32_t tag, uint16_t size);
+#define e_info(reason_code) err_##reason_code
 
 struct errorlog *opal_elog_create(struct opal_err_info *e_info) __warn_unused_result;
+
+int opal_elog_update_user_dump(struct errorlog *buf, unsigned char *data,
+						uint32_t tag, uint16_t size);
 
 /* Called by the backend after an error has been logged by the
  * backend. If the error could not be logged successfully success is

include/fsp-elog.h
@@ -35,188 +35,9 @@ enum elog_head_state {
 	ELOG_STATE_REJECTED,    /* resend all pending logs to linux */
 };
 
-/* Component IDs */
-/* In PEL error log format, Creator ID is hypervisor
- * But we can have various component ID to distinguish
- * which component in hypervisor is reporting the error
- * This is 2 bytes long,
- *	first byte corresponds to Component IDs
- *	Second byte is reserved for the Reason code.
- * Component ID is mapped to readable 4-digit ascii
- * character name in FSP and displayed.
- */
-/* SAPPHIRE components */
-#define OPAL_CODEUPDATE			0x4355	/* CU */
-#define OPAL_CONSOLE			0x434E	/* CN */
-#define OPAL_CEC			0x4345	/* CE */
-#define OPAL_CHIP			0x4348	/* CH */
-#define OPAL_ELOG			0x454C	/* EL */
-#define OPAL_NVRAM			0x4E56	/* NV */
-#define OPAL_RTC			0x5254	/* RT */
-#define OPAL_SURVEILLANCE		0x5355	/* SU */
-#define OPAL_SYSPARAM			0x5350	/* SP */
-#define OPAL_LPC			0x4C50	/* LP */
-#define OPAL_UART			0x5541	/* UA */
-#define OPAL_OCC			0x4F43	/* OC */
-#define OPAL_OP_PANEL			0x4F50	/* OP */
-#define OPAL_PHB3			0x5048	/* PH */
-#define OPAL_PSI			0x5053	/* PS */
-#define OPAL_VPD			0x5650	/* VP */
-#define OPAL_XSCOM			0x5853	/* XS */
-#define OPAL_PCI			0x5043	/* PC */
-#define OPAL_MISC			0x4D49	/* MI */
-#define OPAL_ATTN			0x4154	/* AT */
-#define OPAL_MEM_ERR			0x4D45	/* ME */
-#define OPAL_CENTAUR			0x4354	/* CT */
-#define OPAL_MFSI			0x4D46	/* MF */
-#define OPAL_DUMP			0x4455	/* DU */
-#define OPAL_LED			0x4C45	/* LE */
-#define OPAL_SENSOR			0x5345	/* SE */
-#define OPAL_SLW			0x534C	/* SL */
-#define OPAL_FSP			0x4650	/* FP */
-#define OPAL_I2C			0x4943	/* IC */
-
-/* SAPPHIRE SRC componenet ID*/
-#define OPAL_CU				0x1000
-#define OPAL_CN				0x2000
-#define OPAL_CE				0x3000
-#define OPAL_CH				0x4000
-#define OPAL_EL				0x5000
-#define OPAL_NV				0x6000
-#define OPAL_RT				0x7000
-#define OPAL_SU				0x8000
-#define OPAL_SP				0x9000
-#define OPAL_LP				0xa000
-#define OPAL_UA				0xb000
-#define OPAL_OC				0xc000
-#define OPAL_OP				0xd000
-#define OPAL_PH				0xe000
-#define OPAL_PS				0xf000
-#define OPAL_VP				0x1000
-#define OPAL_XS				0x1100
-#define OPAL_PC				0x1200
-#define OPAL_MI				0x1300
-#define OPAL_AT				0x1400
-#define OPAL_ME				0x1500
-#define OPAL_CT				0x1600
-#define OPAL_MF				0x1700
-#define OPAL_DU				0x1800
-#define OPAL_LE				0x1900
-#define OPAL_SE				0x2000
-#define OPAL_SL				0x2100
-#define OPAL_FP				0x2200
-#define OPAL_IC				0x2300
-
-enum opal_reasoncode {
-/* code update */
-	OPAL_RC_CU_FLASH        = OPAL_CU | 0x10,
-	OPAL_RC_CU_INIT         = OPAL_CU | 0x11,
-	OPAL_RC_CU_SG_LIST      = OPAL_CU | 0x12,
-	OPAL_RC_CU_COMMIT	= OPAL_CU | 0x13,
-	OPAL_RC_CU_MSG		= OPAL_CU | 0x14,
-	OPAL_RC_CU_NOTIFY       = OPAL_CU | 0x15,
-	OPAL_RC_CU_MARKER_LID	= OPAL_CU | 0x16,
-/* NVRAM */
-	OPAL_RC_NVRAM_INIT      = OPAL_NV | 0x10,
-	OPAL_RC_NVRAM_OPEN	= OPAL_NV | 0x11,
-	OPAL_RC_NVRAM_SIZE      = OPAL_NV | 0x12,
-	OPAL_RC_NVRAM_WRITE     = OPAL_NV | 0x13,
-	OPAL_RC_NVRAM_READ      = OPAL_NV | 0x14,
-/* CENTAUR */
-	OPAL_RC_CENTAUR_INIT    = OPAL_CT | 0x10,
-	OPAL_RC_CENTAUR_RW_ERR  = OPAL_CT | 0x11,
-/* MFSI */
-	OPAL_RC_MFSI_RW_ERR     = OPAL_MF | 0x10,
-/* UART */
-	OPAL_RC_UART_INIT       = OPAL_UA | 0x10,
-/* OCC */
-	OPAL_RC_OCC_RESET       = OPAL_OC | 0x10,
-	OPAL_RC_OCC_LOAD        = OPAL_OC | 0x11,
-	OPAL_RC_OCC_PSTATE_INIT = OPAL_OC | 0x12,
-	OPAL_RC_OCC_TIMEOUT	= OPAL_OC | 0x13,
-/* RTC	*/
-	OPAL_RC_RTC_READ	= OPAL_RT | 0x10,
-	OPAL_RC_RTC_TOD		= OPAL_RT | 0x11,
-/* SURVEILLANCE */
-	OPAL_RC_SURVE_INIT      = OPAL_SU | 0x10,
-	OPAL_RC_SURVE_STATUS	= OPAL_SU | 0x11,
-	OPAL_RC_SURVE_ACK	= OPAL_SU | 0x12,
-/* SYSPARAM */
-	OPAL_RC_SYSPARM_INIT    = OPAL_SP | 0x10,
-	OPAL_RC_SYSPARM_MSG     = OPAL_SP | 0x11,
-/* LPC */
-	OPAL_RC_LPC_READ        = OPAL_LP | 0x10,
-	OPAL_RC_LPC_WRITE       = OPAL_LP | 0x11,
-/* OP_PANEL */
-	OPAL_RC_PANEL_WRITE     = OPAL_OP | 0x10,
-/* PSI */
-	OPAL_RC_PSI_INIT        = OPAL_PS | 0x10,
-	OPAL_RC_PSI_IRQ_RESET   = OPAL_PS | 0x11,
-/* XSCOM */
-	OPAL_RC_XSCOM_RW		= OPAL_XS | 0x10,
-	OPAL_RC_XSCOM_INDIRECT_RW	= OPAL_XS | 0x11,
-	OPAL_RC_XSCOM_RESET		= OPAL_XS | 0x12,
-/* PCI */
-	OPAL_RC_PCI_INIT_SLOT   = OPAL_PC | 0x10,
-	OPAL_RC_PCI_ADD_SLOT    = OPAL_PC | 0x11,
-	OPAL_RC_PCI_SCAN        = OPAL_PC | 0x12,
-	OPAL_RC_PCI_RESET_PHB   = OPAL_PC | 0x10,
-/* ATTN */
-	OPAL_RC_ATTN		= OPAL_AT | 0x10,
-/* MEM_ERR */
-	OPAL_RC_MEM_ERR_RES	= OPAL_ME | 0x10,
-	OPAL_RC_MEM_ERR_DEALLOC	= OPAL_ME | 0x11,
-/* DUMP */
-	OPAL_RC_DUMP_INIT	= OPAL_DU | 0x10,
-	OPAL_RC_DUMP_LIST	= OPAL_DU | 0x11,
-	OPAL_RC_DUMP_ACK	= OPAL_DU | 0x12,
-	OPAL_RC_DUMP_MDST_INIT	= OPAL_DU | 0x13,
-	OPAL_RC_DUMP_MDST_UPDATE= OPAL_DU | 0x14,
-	OPAL_RC_DUMP_MDST_ADD	= OPAL_DU | 0x15,
-	OPAL_RC_DUMP_MDST_REMOVE= OPAL_DU | 0x16,
-/* LED	*/
-	OPAL_RC_LED_SPCN	= OPAL_LE | 0x10,
-	OPAL_RC_LED_BUFF	= OPAL_LE | 0x11,
-	OPAL_RC_LED_LC		= OPAL_LE | 0x12,
-	OPAL_RC_LED_STATE	= OPAL_LE | 0x13,
-	OPAL_RC_LED_SUPPORT	= OPAL_LE | 0x14,
-/* SENSOR */
-	OPAL_RC_SENSOR_INIT	= OPAL_SE | 0x10,
-	OPAL_RC_SENSOR_READ	= OPAL_SE | 0x11,
-	OPAL_RC_SENSOR_ASYNC_COMPLETE
-				= OPAL_SE | 0x12,
-/* SLW */
-	OPAL_RC_SLW_INIT	= OPAL_SL | 0x10,
-	OPAL_RC_SLW_SET		= OPAL_SL | 0x11,
-	OPAL_RC_SLW_GET		= OPAL_SL | 0x12,
-	OPAL_RC_SLW_REG		= OPAL_SL | 0x13,
-/* FSP	*/
-	OPAL_RC_FSP_POLL_TIMEOUT
-				= OPAL_FP | 0x10,
-/* I2C */
-	OPAL_RC_I2C_INIT	= OPAL_IC | 0X10,
-	OPAL_RC_I2C_START_REQ	= OPAL_IC | 0X11,
-	OPAL_RC_I2C_TIMEOUT	= OPAL_IC | 0x12,
-	OPAL_RC_I2C_TRANSFER	= OPAL_IC | 0x13,
-	OPAL_RC_I2C_RESET	= OPAL_IC | 0x14,
-};
-
-#define DEFINE_LOG_ENTRY(reason, type, id, subsys,			\
-severity, subtype, callout_func) struct opal_err_info err_##reason =	\
-{ .reason_code = reason, .err_type = type, .cmp_id = id,		\
-.subsystem = subsys, .sev = severity, .event_subtype = subtype,		\
-.call_out = callout_func }
-
 /* Generate src from opal reason code (src_comp) */
 #define generate_src_from_comp(src_comp)  (OPAL_SRC_TYPE_ERROR << 24 | \
 				OPAL_FAILING_SUBSYSTEM << 16 | src_comp)
-
-#define e_info(reason_code) err_##reason_code
-
-struct errorlog *opal_elog_create(struct opal_err_info *e_info) __warn_unused_result;
-
-int opal_elog_update_user_dump(struct errorlog *buf, unsigned char *data,
-						uint32_t tag, uint16_t size);
 
 int elog_fsp_commit(struct errorlog *buf) __warn_unused_result;
 
@@ -228,13 +49,5 @@ bool opal_elog_read(uint64_t *buffer, uint64_t opal_elog_size,
 bool opal_elog_ack(uint64_t ack_id) __warn_unused_result;
 
 void opal_resend_pending_logs(void);
-
-/* This is wrapper around the error log function, which creates
- * and commits the error to FSP.
- * Used for simple error logging
- */
-void log_simple_error(struct opal_err_info *e_info, const char *fmt, ...) __attribute__ ((format (printf, 2, 3)));
-void log_error(struct opal_err_info *e_info, void *data, uint16_t size,
-		const char *fmt, ...) __attribute__ ((format (printf, 4, 5)));
 
 #endif /* __ELOG_H */

core/test/run-pel.c
@@ -42,7 +42,7 @@ const void *dt_prop_get(const struct dt_node *node __unused, const char *prop __
 	return dt_prop;
 }
 
-int fsp_rtc_get_cached_tod(uint32_t *year_month_day,
+int rtc_cache_get_datetime(uint32_t *year_month_day,
 			   uint64_t *hour_minute_second_millisecond)
 {
 	*year_month_day = 0;