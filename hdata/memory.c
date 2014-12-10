open-power/skiboot

commit f1a3a19d723524659bc91c8f65f3ed769a612c9b

Fix warning of potential unbound stack usage in hdata/memory.c
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

hdata/memory.c
@@ -116,8 +116,10 @@ static bool add_address_range(struct dt_node *root,
 {
 	struct dt_node *mem;
 	u64 reg[2];
-	char name[sizeof("memory@") + STR_MAX_CHARS(reg[0])];
+	char *name;
 	u32 chip_id;
+
+	name = (char*)malloc(sizeof("memory@") + STR_MAX_CHARS(reg[0]));
 
 	prlog(PR_DEBUG, "  Range: 0x%016llx..0x%016llx "
 	      "on Chip 0x%x mattr: 0x%x\n",
@@ -148,6 +150,8 @@ static bool add_address_range(struct dt_node *root,
 	if (be16_to_cpu(id->flags) & MS_AREA_SHARED)
 		dt_add_property_cells(mem, DT_PRIVATE "share-id",
 				      be16_to_cpu(id->share_id));
+
+	free(name);
 
 	return true;
 }

commit 378495dcc88b4d5c4ae1210f3c72c037a42187d8

Fix bug/warning in hdata/memory.c I introduced in f1a3a19
Passing the wrong parameter to snprintf (sizeof(char*)) rather
than actual length of the string. GCC 4.9 told us about it (and broke
make check when I enabled -Werror)

hdata/test/../memory.c: In function ‘add_address_range’:

hdata/test/../memory.c:144:23: error: argument to ‘sizeof’ in ‘snprintf’ call is the same expression as the destination; did you mean to provide an explicit length? [-Werror=sizeof-pointer-memaccess]

Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

hdata/memory.c
@@ -118,8 +118,9 @@ static bool add_address_range(struct dt_node *root,
 	u64 reg[2];
 	char *name;
 	u32 chip_id;
+	size_t namesz = sizeof("memory@") + STR_MAX_CHARS(reg[0]);
 
-	name = (char*)malloc(sizeof("memory@") + STR_MAX_CHARS(reg[0]));
+	name = (char*)malloc(namesz);
 
 	prlog(PR_DEBUG, "  Range: 0x%016llx..0x%016llx "
 	      "on Chip 0x%x mattr: 0x%x\n",
@@ -141,7 +142,7 @@ static bool add_address_range(struct dt_node *root,
 			return true;
 		}
 	}
-	snprintf(name, sizeof(name), "memory@%llx", (long long)reg[0]);
+	snprintf(name, namesz, "memory@%llx", (long long)reg[0]);
 
 	mem = dt_new(root, name);
 	dt_add_property_string(mem, "device_type", "memory");