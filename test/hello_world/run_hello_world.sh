4f1f5c2dda167d556c704d1821a321949a68a62c
Stewart Smith <stewart@linux.vnet.ibm.com>
2014-12-15 15:32:09 +1100

    Add hello_world tiny payload to test with mambo (if available)
    Just calls OPAL_CONSOLE_WRITE with "Hello World!" and with mambo
    we can execute this tiny boot test in not much time at all.
    
    Good little sanity check.
    
    Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

---
+++ test/hello_world/run_hello_world.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+
+if [ -z "$MAMBO_PATH" ]; then
+    MAMBO_PATH=/opt/ibm/systemsim-p8/
+fi
+
+if [ -z "$MAMBO_BINARY" ]; then
+    MAMBO_BINARY="/run/pegasus/power8"
+fi
+
+if [ ! -x "$MAMBO_PATH/$MAMBO_BINARY" ]; then
+    echo 'Could not find executable MAMBO_BINARY. Skipping hello_world test';
+    exit 0;
+fi
+
+if [ ! -x `which expect` ]; then
+    echo 'Could not find expect binary. Skipping hello_world test';
+    exit 0;
+fi
+
+
+export SKIBOOT_ZIMAGE=`pwd`/test/hello_world/hello_kernel/hello_kernel
+
+# Currently getting some core dumps from mambo, so disable them!
+OLD_ULIMIT_C=`ulimit -c`
+ulimit -c 0
+
+( cd external/mambo; 
+cat <<EOF | expect
+set timeout 30
+spawn $MAMBO_PATH/$MAMBO_BINARY -n -f skiboot.tcl
+expect {
+timeout { send_user "\nTimeout waiting for hello world\n"; exit 1 }
+eof { send_user "\nUnexpected EOF\n;" exit 1 }
+"Hello World!"
+}
+
+close
+exit 0
+EOF
+)
+ulimit -c $OLD_ULIMIT_C
+echo
+exit 0;
