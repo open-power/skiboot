7e8dc30b32bf5083aa5bda56b10601d42f1921b3
Jeremy Kerr <jk@ozlabs.org>
2015-01-07 09:59:00 +0800

    platforms/firestone: Add Firestone platform
    Based on Habanero.
    
    Signed-off-by: Jeremy Kerr <jk@ozlabs.org>
    Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

---
+++ platforms/astbmc/firestone.c
@@ -0,0 +1,44 @@
+/* Copyright 2013-2014 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#include <skiboot.h>
+#include <device.h>
+#include <console.h>
+#include <chip.h>
+
+#include "astbmc.h"
+
+static bool firestone_probe(void)
+{
+	if (!dt_node_is_compatible(dt_root, "ibm,firestone"))
+		return false;
+
+	/* Lot of common early inits here */
+	astbmc_early_init();
+
+	return true;
+}
+
+
+DECLARE_PLATFORM(firestone) = {
+	.name			= "Firestone",
+	.probe			= firestone_probe,
+	.init			= astbmc_init,
+	.external_irq		= astbmc_ext_irq,
+	.cec_power_down         = astbmc_ipmi_power_down,
+	.cec_reboot             = astbmc_ipmi_reboot,
+};
