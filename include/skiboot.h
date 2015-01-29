d6fb45e3b29df30e30ac400cfb8b030719207213
Joel Stanley <joel@jms.id.au>
2015-01-29 14:47:54 +1030

    hw/ipmi: Disable flash access when requested
    BMC based systems contain a PNOR to provide flash storage. The host
    normally has exclusive access to the PNOR, however the BMC may use IPMI
    to request access to perform functions such as update the firmware.
    
    Indicate to users of the flash that the device is busy by taking the
    lock, and setting a per-flash busy flag, which causes flash operations
    to return OPAL_BUSY.
    
    Minor changes from Jeremy Kerr
    
    Signed-off-by: Joel Stanley <joel@jms.id.au>
    Signed-off-by: Jeremy Kerr <jk@ozlabs.org>

--- include/skiboot.h
+++ include/skiboot.h
@@ -201,6 +201,9 @@ struct flash_chip;
 extern int flash_register(struct flash_chip *chip, bool is_system_flash);
 extern bool flash_load_resource(enum resource_id id, uint32_t subid,
 		void *buf, size_t *len);
+extern bool flash_reserve(void);
+extern void flash_release(void);
+
 
 /* NVRAM support */
 extern void nvram_init(void);
