ce48d7efe84848e5ac49600477d029f5202ce5f2
Cyril Bur <cyril.bur@au1.ibm.com>
2015-05-22 13:52:59 +1000

    pflash: Create make dist target for pflash release tarballs
    In order to be able to compile and ship pflash independently to the rest of
    skiboot as it is a separate userland tool we're going to need a tarball of all
    the required files to build it.
    
    This commit adds a make dist target which copies libflash and ccan alongside
    pflash sources and inserts a stripped down Makefile into a tarball.
    
    Signed-off-by: Cyril Bur <cyril.bur@au1.ibm.com>
    Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

---
+++ external/pflash/rules.mk
@@ -0,0 +1,38 @@
+ARCH=$(shell ./get_arch.sh $(CROSS_COMPILE))
+
+ifeq ($(ARCH),ARCH_POWERPC)
+ARCH_OBJS = powerpc_io.o sfc-ctrl.o
+else
+ifeq ($(ARCH),ARCH_ARM)
+ARCH_OBJS = arm_io.o
+else
+$(error Unsupported architecture $(ARCH))
+endif
+endif
+
+.DEFAULT_GOAL := all
+
+CFLAGS  = -O2 -Wall -I.
+LDFLAGS	= -lrt
+OBJS    = pflash.o progress.o ast-sf-ctrl.o version.o
+OBJS	+= libflash/libflash.o libflash/libffs.o libflash/ecc.o
+OBJS	+= $(ARCH_OBJS)
+EXE     = pflash
+
+CC	= $(CROSS_COMPILE)gcc
+
+PFLASH_VERSION ?= $(shell ./make_version.sh $(EXE))
+
+version.c: make_version.sh .version
+	@(if [ "a$(PFLASH_VERSION)" = "a" ]; then \
+	echo "#error You need to set SKIBOOT_VERSION environment variable" > $@ ;\
+	else \
+	echo "const char version[] = \"$(PFLASH_VERSION)\";" ;\
+	fi) > $@
+
+%.o : %.c
+	$(CC) $(CFLAGS) -c $< -o $@
+
+$(EXE): $(OBJS)
+	$(CC) $(CFLAGS) $(LDFLAGS) $^ -o $@
+
