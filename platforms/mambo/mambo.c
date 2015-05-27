63686ab585b990121672f3249ee12197e014952f
Stewart Smith <stewart@linux.vnet.ibm.com>
2015-05-27 11:35:36 +1000

    Add Mambo platform
    By adding an explicit mambo platform we can do tricks like a fake
    NVRAM, actually get RTC from mambo and generally be a bit better
    in a simulator
    
    Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

---
+++ platforms/mambo/mambo.c
@@ -0,0 +1,81 @@
+/* Copyright 2015 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#include <skiboot.h>
+#include <device.h>
+#include <console.h>
+#include <chip.h>
+#include <opal-api.h>
+#include <opal-internal.h>
+#include <time-utils.h>
+#include <time.h>
+
+extern int64_t mambo_get_time(void);
+
+static bool mambo_probe(void)
+{
+	if (!dt_find_by_path(dt_root, "/mambo"))
+		return false;
+
+	return true;
+}
+
+static int64_t mambo_rtc_read(uint32_t *ymd, uint64_t *hmsm)
+{
+	int64_t mambo_time;
+	struct tm t;
+	time_t mt;
+
+	if (!ymd || !hmsm)
+		return OPAL_PARAMETER;
+
+	mambo_time = mambo_get_time();
+	mt = mambo_time >> 32;
+	gmtime_r(&mt, &t);
+	tm_to_datetime(&t, ymd, hmsm);
+
+	return OPAL_SUCCESS;
+}
+
+static void mambo_rtc_init(void)
+{
+	struct dt_node *np = dt_new(opal_node, "rtc");
+	dt_add_property_strings(np, "compatible", "ibm,opal-rtc");
+
+	opal_register(OPAL_RTC_READ, mambo_rtc_read, 2);
+}
+
+static void mambo_platform_init(void)
+{
+	force_dummy_console();
+	mambo_rtc_init();
+}
+
+static int64_t mambo_cec_power_down(uint64_t request __unused)
+{
+	if (chip_quirk(QUIRK_MAMBO_CALLOUTS))
+		mambo_sim_exit();
+
+	return OPAL_UNSUPPORTED;
+}
+
+DECLARE_PLATFORM(mambo) = {
+	.name			= "Mambo",
+	.probe			= mambo_probe,
+	.init		= mambo_platform_init,
+	.cec_power_down = mambo_cec_power_down,
+};
