open-power/skiboot

commit 87690bd19dbb10e5b1ca004e7eab6a573153516b

cpu: Don't hold lock while waiting
Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

core/cpu.c
@@ -638,9 +638,10 @@ static int64_t opal_reinit_cpus(uint64_t flags)
 	int64_t rc = OPAL_SUCCESS;
 	int i;
 
-	lock(&reinit_lock);
-
 	prerror("OPAL: Trying a CPU re-init with flags: 0x%llx\n", flags);
+
+ again:
+	lock(&reinit_lock);
 
 	for (cpu = first_cpu(); cpu; cpu = next_cpu(cpu)) {
 		if (cpu == this_cpu())
@@ -651,8 +652,11 @@ static int64_t opal_reinit_cpus(uint64_t flags)
 			 * where we are still, wait a bit and try again
 			 */
 			for (i = 0; (i < 1000) &&
-				     (cpu->state == cpu_state_os); i++)
+				     (cpu->state == cpu_state_os); i++) {
+				unlock(&reinit_lock);
 				time_wait_ms(1);
+				goto again;
+			}
 			if (cpu->state == cpu_state_os) {
 				prerror("OPAL: CPU 0x%x not in OPAL !\n", cpu->pir);
 				rc = OPAL_WRONG_STATE;

commit 9ee56b55077d1bfb3df478abf7e5371a47b1bcd1

cpu: Fix hang issue in opal_reinit_cpus()
Commit 87690bd19dbb introduced a label "again" so as to avoid holding
the lock while waiting. But this leads to hang in scenarios like kdump,
where 'cpu_state_os' will be the state for all offline cpus. Actaully,
the wait loop doesn't really take off with the goto statement in it.
This patch tries to fix this problem by removing the goto statement
and unlocking/locking within the wait loop instead.

Signed-off-by: Hari Bathini <hbathini@linux.vnet.ibm.com>
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

core/cpu.c
@@ -644,7 +644,6 @@ static int64_t opal_reinit_cpus(uint64_t flags)
 
 	prerror("OPAL: Trying a CPU re-init with flags: 0x%llx\n", flags);
 
- again:
 	lock(&reinit_lock);
 
 	for (cpu = first_cpu(); cpu; cpu = next_cpu(cpu)) {
@@ -659,7 +658,7 @@ static int64_t opal_reinit_cpus(uint64_t flags)
 				     (cpu->state == cpu_state_os); i++) {
 				unlock(&reinit_lock);
 				time_wait_ms(1);
-				goto again;
+				lock(&reinit_lock);
 			}
 			if (cpu->state == cpu_state_os) {
 				prerror("OPAL: CPU 0x%x not in OPAL !\n", cpu->pir);