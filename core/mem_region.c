open-power/skiboot

commit 58033e44b927c80257a72037aeed2cab682e0c85

core/mem_region: Add mem_range_is_reserved()
This change adds a function to check whether a range of memory is
covered by one or more reservations.

Signed-off-by: Jeremy Kerr <jk@ozlabs.org>
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

core/mem_region.c
@@ -789,6 +789,49 @@ struct mem_region *find_mem_region(const char *name)
 	return NULL;
 }
 
+bool mem_range_is_reserved(uint64_t start, uint64_t size)
+{
+	uint64_t end = start + size;
+	struct mem_region *region;
+
+	/* We may have the range covered by a number of regions, which could
+	 * appear in any order. So, we look for a region that covers the
+	 * start address, and bump start up to the end of that region.
+	 *
+	 * We repeat until we've either bumped past the end of the range,
+	 * or we didn't find a matching region.
+	 *
+	 * This has a worst-case of O(n^2), but n is well bounded by the
+	 * small number of reservations.
+	 */
+	for (;;) {
+		bool found = false;
+
+		list_for_each(&regions, region, list) {
+			if (!region_is_reserved(region))
+				continue;
+
+			/* does this region overlap the start address, and
+			 * have a non-zero size? */
+			if (region->start <= start &&
+					region->start + region->len > start &&
+					region->len) {
+				start = region->start + region->len;
+				found = true;
+			}
+		}
+
+		/* 'end' is the first byte outside of the range */
+		if (start >= end)
+			return true;
+
+		if (!found)
+			break;
+	}
+
+	return false;
+}
+
 void adjust_cpu_stacks_alloc(void)
 {
 	/* CPU stacks start at 0, then when we know max possible PIR,

include/mem_region.h
@@ -74,4 +74,6 @@ void mem_reserve_hw(const char *name, uint64_t start, uint64_t len);
 
 struct mem_region *find_mem_region(const char *name);
 
+bool mem_range_is_reserved(uint64_t start, uint64_t size);
+
 #endif /* __MEMORY_REGION */

core/test/Makefile.check
@@ -8,6 +8,7 @@ CORE_TEST := core/test/run-device \
 	core/test/run-mem_region_release_unused \
 	core/test/run-mem_region_release_unused_noalloc \
 	core/test/run-mem_region_reservations \
+	core/test/run-mem_range_is_reserved \
 	core/test/run-nvram-format \
 	core/test/run-trace core/test/run-msg \
 	core/test/run-pel \

core/test/run-mem_range_is_reserved.c
@@ -1,0 +1,216 @@
+/* Copyright 2015 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *	http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <config.h>
+
+#define BITS_PER_LONG (sizeof(long) * 8)
+/* Don't include this, it's PPC-specific */
+#define __CPU_H
+static unsigned int cpu_max_pir = 1;
+struct cpu_thread {
+	unsigned int			chip_id;
+};
+
+#include <stdlib.h>
+
+static void *real_malloc(size_t size)
+{
+	return malloc(size);
+}
+
+static void real_free(void *p)
+{
+	return free(p);
+}
+
+#undef malloc
+#undef free
+#undef realloc
+
+#include <skiboot.h>
+#include <mem_region-malloc.h>
+
+/* We need mem_region to accept __location__ */
+#define is_rodata(p) true
+#include "../mem_region.c"
+#include "../malloc.c"
+
+/* But we need device tree to make copies of names. */
+#undef is_rodata
+#define is_rodata(p) false
+#include "../../libc/string/strdup.c"
+
+#include "../device.c"
+#include <assert.h>
+#include <stdio.h>
+
+void lock(struct lock *l)
+{
+	assert(!l->lock_val);
+	l->lock_val++;
+}
+
+void unlock(struct lock *l)
+{
+	assert(l->lock_val);
+	l->lock_val--;
+}
+
+bool lock_held_by_me(struct lock *l)
+{
+	return l->lock_val;
+}
+
+#define TEST_HEAP_ORDER 14
+#define TEST_HEAP_SIZE (1ULL << TEST_HEAP_ORDER)
+
+static void add_mem_node(uint64_t start, uint64_t len)
+{
+	struct dt_node *mem;
+	u64 reg[2];
+	char *name;
+
+	name = (char*)malloc(sizeof("memory@") + STR_MAX_CHARS(reg[0]));
+	assert(name);
+
+	/* reg contains start and length */
+	reg[0] = cpu_to_be64(start);
+	reg[1] = cpu_to_be64(len);
+
+	sprintf(name, "memory@%llx", (long long)start);
+
+	mem = dt_new(dt_root, name);
+	dt_add_property_string(mem, "device_type", "memory");
+	dt_add_property(mem, "reg", reg, sizeof(reg));
+	free(name);
+}
+
+void add_chip_dev_associativity(struct dt_node *dev __attribute__((unused)))
+{
+}
+
+struct test_region {
+	uint64_t	start;
+	uint64_t	end;
+};
+
+static struct test {
+	struct test_region	regions[3];
+	bool			reserved;
+} tests[] = {
+	/* empty region set */
+	{ { { 0 } }, false },
+
+	/* single exact match */
+	{ { { 0x1000, 0x2000 }, }, true },
+
+	/* overlap downwards */
+	{ { { 0x0fff, 0x2000 }, }, true },
+
+	/* overlap upwards */
+	{ { { 0x1000, 0x2001 }, }, true },
+
+	/* missing first byte */
+	{ { { 0x1001, 0x2000 }, }, false },
+
+	/* missing last byte */
+	{ { { 0x1000, 0x1fff }, }, false },
+
+	/* two regions, full coverage, split before start of range */
+	{ { { 0x0500, 0x1000 }, { 0x1000, 0x2500 } }, true },
+
+	/* two regions, full coverage, split after start of range */
+	{ { { 0x0500, 0x1001 }, { 0x1001, 0x2500 } }, true },
+
+	/* two regions, full coverage, split at middle of range */
+	{ { { 0x0500, 0x1500 }, { 0x1500, 0x2500 } }, true },
+
+	/* two regions, full coverage, split before end of range */
+	{ { { 0x0500, 0x1fff }, { 0x1fff, 0x2500 } }, true },
+
+	/* two regions, full coverage, split after end of range */
+	{ { { 0x0500, 0x2000 }, { 0x2000, 0x2500 } }, true },
+
+	/* two regions, missing byte in middle of range */
+	{ { { 0x0500, 0x14ff }, { 0x1500, 0x2500 } }, false },
+
+	/* two regions, missing byte after start of range */
+	{ { { 0x0500, 0x1000 }, { 0x1001, 0x2500 } }, false },
+
+	/* two regions, missing byte before end of range */
+	{ { { 0x0500, 0x1fff }, { 0x2000, 0x2500 } }, false },
+};
+
+static void run_test(struct test *test)
+{
+	struct test_region *r;
+	bool reserved;
+
+	list_head_init(&regions);
+
+	mem_region_init();
+
+	/* create our reservations */
+	for (r = test->regions; r->start; r++)
+		mem_reserve_hw("r", r->start, r->end - r->start);
+
+	reserved = mem_range_is_reserved(0x1000, 0x1000);
+
+	if (reserved != test->reserved)	{
+		struct mem_region *r;
+		fprintf(stderr, "test failed; got %s, expected %s\n",
+				reserved ? "reserved" : "unreserved",
+				test->reserved ? "reserved" : "unreserved");
+
+		fprintf(stderr, "reserved regions:\n");
+
+		list_for_each(&regions, r, list) {
+			fprintf(stderr, "\t: %08x[%08x] %s\n",
+					r->start, r->len, r->name);
+		}
+		exit(EXIT_FAILURE);
+	}
+}
+
+
+int main(void)
+{
+	unsigned int i;
+	void *buf;
+
+	/* Use malloc for the heap, so valgrind can find issues. */
+	skiboot_heap.start = (long)real_malloc(TEST_HEAP_SIZE);
+	skiboot_heap.len = TEST_HEAP_SIZE;
+
+	/* shift the OS reserve area out of the way of our playground */
+	skiboot_os_reserve.start = 0x100000;
+	skiboot_os_reserve.len = 0x1000;
+
+	dt_root = dt_new_root("");
+	dt_add_property_cells(dt_root, "#address-cells", 2);
+	dt_add_property_cells(dt_root, "#size-cells", 2);
+
+	buf = real_malloc(1024*1024);
+	add_mem_node((unsigned long)buf, 1024*1024);
+
+	for (i = 0; i < ARRAY_SIZE(tests); i++)
+		run_test(&tests[i]);
+
+	dt_free(dt_root);
+	real_free(buf);
+	real_free((void *)(long)skiboot_heap.start);
+	return 0;
+}

commit 084f1deb99f10b210ac2751b60b65f35ffca2a22

Ensure reserved memory ranges are exposed correctly to host (fix corrupted SLW image)
Memory regions in skiboot have an interesting life cycle. First, we get
a bunch from the initial device tree or hdat specifying some existing
reserved ranges (as well as adding some of our own if they're missing)
but we also get ranges for the entirety of RAM.

The idea is that we can do node local allocations for per node resources
(which we do) and then, just prior to booting linux, we copy the reserved
memory regions to expose to linux along with a set of reserver regions
to cover the node local allocations.

The problem was that mem_range_is_reserved() was wanting subtle different
semantics for memory region type than region_is_reserved() provided.

That is, we were overriding the meaning of REGION_SKIBOOT_HEAP to mean both
"this is reserved by skiboot" *and* "this is a memory region that covers
all of memory and will be shrunk to cover just the memory we have allocated
for it just before we boot the payload (linux)".

So what would happen is we would ask "hey, is the memory holding the SLW
image reserved?" and we'd get the answer of "yes" but referring to the memory
region that covers the entirety of memory in a NUMA node, *not* meaning
our intent of "this will be reserved when we start linux".

To fix this, introduce a new memory region type REGION_MEMORY. This has
the semantics of a memory region that covers a block of memory that we can
allocate from (using local_alloc) and that the part that was allocated
will be passed to linux as reserved, but that the entire range will not
be reserved.

So our new semantics are:
- region_is_reservable() is true if the region *MAY* be reserved
  (i.e. is the regions that cover the whole of memory OR is explicitly reserved)
- region_is_reserved() is true if the region *WILL* be reserved
  (i.e. is explicitly reserved)

This way we check that the SLW image is explicitly reserved and if it isn't,
we reserve it.

Fixes: 58033e44

core/mem_region.c
@@ -133,7 +133,8 @@ static struct alloc_hdr *next_hdr(const struct mem_region *region,
 static void init_allocatable_region(struct mem_region *region)
 {
 	struct free_hdr *f = region_start(region);
-	assert(region->type == REGION_SKIBOOT_HEAP);
+	assert(region->type == REGION_SKIBOOT_HEAP ||
+	       region->type == REGION_MEMORY);
 	f->hdr.num_longs = region->len / sizeof(long);
 	f->hdr.free = true;
 	f->hdr.prev_free = false;
@@ -256,9 +257,14 @@ static void bad_header(const struct mem_region *region,
 	abort();
 }
 
-static bool region_is_reserved(struct mem_region *region)
+static bool region_is_reservable(struct mem_region *region)
 {
 	return region->type != REGION_OS;
+}
+
+static bool region_is_reserved(struct mem_region *region)
+{
+	return region->type != REGION_OS && region->type != REGION_MEMORY;
 }
 
 void mem_dump_allocs(void)
@@ -269,7 +275,8 @@ void mem_dump_allocs(void)
 	/* Second pass: populate property data */
 	printf("Memory regions:\n");
 	list_for_each(&regions, region, list) {
-		if (region->type != REGION_SKIBOOT_HEAP)
+		if (!(region->type == REGION_SKIBOOT_HEAP ||
+		      region->type == REGION_MEMORY))
 			continue;
 		printf("  0x%012llx..%012llx : %s\n",
 		       (long long)region->start,
@@ -299,7 +306,8 @@ int64_t mem_dump_free(void)
 
 	printf("Free space in HEAP memory regions:\n");
 	list_for_each(&regions, region, list) {
-		if (region->type != REGION_SKIBOOT_HEAP)
+		if (!(region->type == REGION_SKIBOOT_HEAP ||
+		      region->type == REGION_MEMORY))
 			continue;
 		region_free = 0;
 
@@ -335,7 +343,8 @@ static void *__mem_alloc(struct mem_region *region, size_t size, size_t align,
 	assert(is_rodata(location));
 
 	/* Unallocatable region? */
-	if (region->type != REGION_SKIBOOT_HEAP)
+	if (!(region->type == REGION_SKIBOOT_HEAP ||
+	      region->type == REGION_MEMORY))
 		return NULL;
 
 	/* First allocation? */
@@ -535,8 +544,9 @@ bool mem_check(const struct mem_region *region)
 	}
 
 	/* Not ours to play with, or empty?  Don't do anything. */
-	if (region->type != REGION_SKIBOOT_HEAP ||
-			region->free_list.n.next == NULL)
+	if (!(region->type == REGION_MEMORY ||
+	      region->type == REGION_SKIBOOT_HEAP) ||
+	    region->free_list.n.next == NULL)
 		return true;
 
 	/* Walk linearly. */
@@ -738,7 +748,8 @@ restart:
 		const struct dt_property *prop;
 		const __be32 *ids;
 
-		if (region->type != REGION_SKIBOOT_HEAP)
+		if (!(region->type == REGION_SKIBOOT_HEAP ||
+		      region->type == REGION_MEMORY))
 			continue;
 
 		/* Don't allocate from normal heap. */
@@ -950,7 +961,7 @@ void mem_region_init(void)
 		strcat(rname, i->name);
 		start = dt_get_address(i, 0, &len);
 		lock(&mem_region_lock);
-		region = new_region(rname, start, len, i, REGION_SKIBOOT_HEAP);
+		region = new_region(rname, start, len, i, REGION_MEMORY);
 		if (!region) {
 			prerror("MEM: Could not add mem region %s!\n", i->name);
 			abort();
@@ -1023,7 +1034,8 @@ void mem_region_release_unused(void)
 		uint64_t used_len;
 
 		/* If it's not allocatable, ignore it. */
-		if (r->type != REGION_SKIBOOT_HEAP)
+		if (!(r->type == REGION_SKIBOOT_HEAP ||
+		      r->type == REGION_MEMORY))
 			continue;
 
 		used_len = allocated_length(r);
@@ -1123,7 +1135,7 @@ void mem_region_add_dt_reserved(void)
 
 	/* First pass: calculate length of property data */
 	list_for_each(&regions, region, list) {
-		if (!region_is_reserved(region))
+		if (!region_is_reservable(region))
 			continue;
 		names_len += strlen(region->name) + 1;
 		ranges_len += 2 * sizeof(uint64_t);
@@ -1135,7 +1147,7 @@ void mem_region_add_dt_reserved(void)
 	printf("Reserved regions:\n");
 	/* Second pass: populate property data */
 	list_for_each(&regions, region, list) {
-		if (!region_is_reserved(region))
+		if (!region_is_reservable(region))
 			continue;
 		len = strlen(region->name) + 1;
 		memcpy(name, region->name, len);

include/mem_region.h
@@ -25,6 +25,9 @@ enum mem_region_type {
 	/* ranges allocatable by mem_alloc: this will be most of memory */
 	REGION_SKIBOOT_HEAP,
 
+	/* ranges allocatable by mem_alloc but shrunk (e.g. whole memory) */
+	REGION_MEMORY,
+
 	/* ranges used explicitly for skiboot, but not allocatable. eg .text */
 	REGION_SKIBOOT_FIRMWARE,
 

core/test/run-mem_region_init.c
@@ -164,7 +164,7 @@ int main(void)
 		    r == &skiboot_os_reserve)
 			builtins++;
 		else
-			assert(r->type == REGION_SKIBOOT_HEAP);
+			assert(r->type == REGION_MEMORY);
 		assert(mem_check(r));
 	}
 	assert(builtins == 5);

core/test/run-mem_region_release_unused.c
@@ -159,7 +159,7 @@ int main(void)
 		if (r == &skiboot_cpu_stacks)
 			continue;
 		if (r == other) {
-			assert(r->type == REGION_SKIBOOT_HEAP);
+			assert(r->type == REGION_MEMORY);
 			assert(r->len < 1024 * 1024);
 		} else {
 			assert(r->type == REGION_OS);