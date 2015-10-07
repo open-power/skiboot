open-power/skiboot

commit 55ae15b939fdb8bef393f954fe0639a579f580fa

Ensure we run pollers in cpu_wait_job()
In root causing a bug on AST BMC Alistair found that pollers weren't
being run for around 3800ms.

This was due to a wonderful accident that's probably about a year or more
old where:

In cpu_wait_job we have:
        unsigned long ticks = usecs_to_tb(5);
	...
	time_wait(ticks);

While in time_wait(), deciding on if to run pollers:
        unsigned long period = msecs_to_tb(5);
	...
	if (remaining >= period) {

Obviously, this means we never run pollers. Not ideal.

This patch ensures we run pollers every 5ms in cpu_wait_job() as well
as displaying how long we waited for a job if that wait was >1second.

Reported-by: Alistair Popple <alistair@popple.id.au>
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

core/cpu.c
@@ -129,16 +129,25 @@ bool cpu_poll_job(struct cpu_job *job)
 void cpu_wait_job(struct cpu_job *job, bool free_it)
 {
 	unsigned long ticks = usecs_to_tb(5);
+	unsigned long period = msecs_to_tb(5);
+	unsigned long time_waited = 0;
 
 	if (!job)
 		return;
 
 	while(!job->complete) {
 		time_wait(ticks);
+		time_waited+=ticks;
+		if (time_waited % period == 0)
+			opal_run_pollers();
 		lwsync();
 	}
 	lwsync();
 	smt_medium();
+
+	if (time_waited > msecs_to_tb(1000))
+		prlog(PR_DEBUG, "cpu_wait_job(%s) for %d\n",
+		      job->name, tb_to_msecs(time_waited));
 
 	if (free_it)
 		free(job);

commit 939874af72a6c4e79ebc2d4cb4ff1a806606aafb

Fix printf format warning
Fixes: 55ae15b
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

core/cpu.c
@@ -146,7 +146,7 @@ void cpu_wait_job(struct cpu_job *job, bool free_it)
 	smt_medium();
 
 	if (time_waited > msecs_to_tb(1000))
-		prlog(PR_DEBUG, "cpu_wait_job(%s) for %d\n",
+		prlog(PR_DEBUG, "cpu_wait_job(%s) for %lu\n",
 		      job->name, tb_to_msecs(time_waited));
 
 	if (free_it)