35451e78e897e68cfc72a69ea5e34afb334b048e
Stewart Smith <stewart@linux.vnet.ibm.com>
2015-11-11 15:35:11 +1100

    Revive hdata_to_dt and make it work again
    commit 27b49826e5f1d9c0008c0a5bf0953dd673a9b9e1 upstream
    
    Some functions were stubbed out which meant we hit assert rather
    than dumping out the device tree
    
    Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

--- hdata/test/stubs.c
+++ hdata/test/stubs.c
@@ -43,25 +43,7 @@ static void stub_function(void)
 #define STUB(fnname) \
 	void fnname(void) __attribute__((weak, alias ("stub_function")))
 
-STUB(fdt_begin_node);
-STUB(fdt_property);
-STUB(fdt_end_node);
-STUB(fdt_create);
-STUB(fdt_add_reservemap_entry);
-STUB(fdt_finish_reservemap);
-STUB(fdt_strerror);
-STUB(fdt_check_header);
-STUB(_fdt_check_node_offset);
-STUB(fdt_next_tag);
-STUB(fdt_string);
-STUB(fdt_get_name);
-STUB(dt_first);
-STUB(dt_next);
-STUB(dt_has_node_property);
-STUB(dt_get_address);
 STUB(op_display);
 STUB(fsp_preload_lid);
 STUB(fsp_wait_lid_loaded);
-STUB(get_ics_phandle);
-STUB(get_psi_interrupt);
 STUB(fsp_adjust_lid_side);
