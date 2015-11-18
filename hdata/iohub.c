open-power/skiboot

commit 0baf63f38757d356de3436ac0ab47511f98b8723

Drop support for p5ioc2
p5ioc2 is used by approximately 2 machines in the world, and has never
ever been a supported configuration.

Not only is the code virtually unused and very tricky to test, but
keeping it around is making life unnecessarily difficult:

 - It's more complexity to manage for things such as PCI slot support
 - It's more code for static analysis to cover, which means more time
   fixing bugs that affect no-one.
 - It's bloating every single install of skiboot for no benefit.
 - It's reducing coverage stats, which is sad.

Drop p5ioc2.

Signed-off-by: Daniel Axtens <dja@axtens.net>
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

hdata/iohub.c
@@ -22,7 +22,6 @@
 #include <ccan/str/str.h>
 #include <ccan/array_size/array_size.h>
 #include <device.h>
-#include <p5ioc2.h>
 #include <p7ioc.h>
 #include <vpd.h>
 #include <inttypes.h>
@@ -379,213 +378,6 @@ static void io_add_p8_cec_vpd(const struct HDIF_common_hdr *sp_iohubs)
 	io_get_lx_info(kwvpd, kwvpd_sz, 0, dt_root);
 }
 
-static struct dt_node *io_add_hea(const struct cechub_io_hub *hub,
-				  const void *sp_io)
-{
-	struct dt_node *np, *gnp;
-	uint64_t reg[2];
-	unsigned int i, vpd_sz;
-	uint8_t kw_sz;
-	const void *iokid, *vpd, *ccin;
-	const uint8_t *mac;
-	const struct HDIF_child_ptr *iokids;
-
-	/*
-	 * We have a table of supported dauther cards looked up
-	 * by CCIN. We don't use the 1008 slot map in the VPD.
-	 *
-	 * This is basically translated from BML and will do for
-	 * now especially since we don't really support p5ioc2
-	 * machine, this is just for lab use
-	 *
-	 * This is mostly untested on 10G ... we might need more
-	 * info about the PHY in that case
-	 */
-	const struct hea_iocard {
-		const char ccin[4];
-		struct {
-			uint32_t speed;
-			uint16_t ports;
-			uint16_t phy_id;
-		} pg[2];
-	} hea_iocards[] = {
-		{
-			.ccin = "1818", /* HV4 something */
-			.pg[0] = { .speed = 1000, .ports = 2, .phy_id = 0 },
-		},
-		{
-			.ccin = "1819", /* HV4 Titov Card */
-			.pg[0] = { .speed = 1000, .ports = 2, .phy_id = 0 },
-			.pg[1] = { .speed = 1000, .ports = 2, .phy_id = 0 },
-		},
-		{
-			.ccin = "1830", /* HV4 Sergei Card */
-			.pg[0] = { .speed = 10000, .ports = 1, .phy_id = 0 },
-			.pg[1] = { .speed = 10000, .ports = 1, .phy_id = 0 },
-		},
-		{
-			.ccin = "181A", /* L4 Evans Card */
-			.pg[1] = { .speed = 1000, .ports = 2, .phy_id = 0 },
-		},
-		{
-			.ccin = "181B", /* L4 Weber Card */
-			.pg[0] = { .speed = 10000, .ports = 1, .phy_id = 0 },
-			.pg[1] = { .speed = 10000, .ports = 1, .phy_id = 0 },
-		},
-		{
-			.ccin = "181C", /* HV4 Gibson Card */
-			.pg[0] = { .speed = 1000, .ports = 2, .phy_id = 0 },
-			.pg[1] = { .speed = 1000, .ports = 2, .phy_id = 0 },
-		},
-		{
-			.ccin = "2BC4", /* MR Riverside 2 */
-			.pg[0] = { .speed = 1000, .ports = 1, .phy_id = 1 },
-			.pg[1] = { .speed = 1000, .ports = 1, .phy_id = 1 },
-		},
-		{
-			.ccin = "2BC5", /* MR Lions 2 */
-			.pg[0] = { .speed = 10000, .ports = 1, .phy_id = 1 },
-			.pg[1] = { .speed = 10000, .ports = 1, .phy_id = 1 },
-		},
-		{
-			.ccin = "2BC6", /* MR Onion 2 */
-			.pg[0] = { .speed = 10000, .ports = 1, .phy_id = 1 },
-			.pg[1] = { .speed = 1000, .ports = 2, .phy_id = 1 },
-		},
-		{
-			.ccin = "266D", /* Jupiter Bonzai */
-			.pg[0] = { .speed = 1000, .ports = 2, .phy_id = 1 },
-			.pg[1] = { .speed = 1000, .ports = 2, .phy_id = 1 },
-		},
-		/* The blade use an IO KID that's a bit oddball and seems to
-		 * represent the backplane itself, but let's use it anyway
-		 *
-		 * XXX Probably want a different PHY type !
-		 */
-		{
-			.ccin = "531C", /* P7 Blade */
-			.pg[0] = { .speed = 1000, .ports = 2, .phy_id = 0 },
-		},
-	};
-	const struct hea_iocard *card = NULL;
-
-	/* WARNING: This makes quite a lot of nasty assumptions
-	 * that appear to hold true on the few machines I care
-	 * about, which is good enough for now. We don't officially
-	 * support p5ioc2 anyway...
-	 */
-
-	/* Get first IO KID, we only support one. Real support would
-	 * mean using the FRU ID and the SLCA to find the right "stuff"
-	 * but at this stage it's unnecessary
-	 */
-	iokids = HDIF_child_arr(sp_io, CECHUB_CHILD_IO_KIDS);
-	if (!CHECK_SPPTR(iokids)) {
-		prerror("HEA: no IOKID in HDAT child array !\n");
-		return NULL;
-	}
-	if (!iokids->count) {
-		prerror("HEA: IOKID count is 0 !\n");
-		return NULL;
-	}
-	if (be32_to_cpu(iokids->count) > 1) {
-		prlog(PR_WARNING, "HEA: WARNING ! More than 1 IO KID !!! (%d)\n",
-		       iokids->count);
-	}
-	iokid = HDIF_child(sp_io, iokids, 0, "IO KID");
-	if (!iokid) {
-		prerror("HEA: Failed to retrieve IO KID 0 !\n");
-		return NULL;
-	}
-
-	/* Grab VPD */
-	vpd = HDIF_get_idata(iokid, IOKID_KW_VPD, &vpd_sz);
-	if (!CHECK_SPPTR(vpd)) {
-		prerror("HEA: Failed to retrieve VPD from IO KID !\n");
-		return NULL;
-	}
-
-	/* Grab the MAC address */
-	mac = vpd_find(vpd, vpd_sz, "VINI", "B1", &kw_sz);
-	if (!mac || kw_sz < 8) {
-		prerror("HEA: Failed to retrieve MAC Address !\n");
-		return NULL;
-	}
-
-	/* Grab the CCIN (card ID) */
-	ccin = vpd_find(vpd, vpd_sz, "VINI", "CC", &kw_sz);
-	if (!ccin || kw_sz < 4) {
-		prerror("HEA: Failed to retrieve CCIN !\n");
-		return NULL;
-	}
-
-	/* Now we could try to parse the 1008 slot map etc... but instead
-	 * we'll do like BML and grab the CCIN & use it for known cards.
-	 * We also grab the MAC
-	 */
-	for (i = 0; i < ARRAY_SIZE(hea_iocards) && !card; i++) {
-		if (strncmp(hea_iocards[i].ccin, ccin, 4))
-			continue;
-		card = &hea_iocards[i];
-	}
-	if (!card) {
-		prerror("HEA: Unknown CCIN 0x%.4s!\n", (const char *)ccin);
-		return NULL;
-	}
-
-	/* Assume base address is BAR3 + 0x4000000000 */
-	reg[0] = be64_to_cpu(hub->gx_ctrl_bar3) + 0x4000000000;
-	reg[1] = 0xc0000000;
-
-	prlog(PR_DEBUG, "CEC:    * Adding HEA to P5IOC2, assuming GBA=0x%llx\n",
-	       (long long)reg[0]);
-	np = dt_new_addr(dt_root, "ibm,hea", reg[0]);
-	if (!np)
-		return NULL;
-
-	dt_add_property(np, "reg", reg, sizeof(reg));
-	dt_add_property_strings(np, "compatible", "ibm,p5ioc2-hea");
-	dt_add_property_cells(np, "#address-cells", 1);
-	dt_add_property_cells(np, "#size-cells", 0);
-	dt_add_property(np, "ibm,vpd", vpd, vpd_sz);
-	dt_add_property_cells(np, "#mac-address", mac[7]);
-	dt_add_property(np, "mac-address-base", mac, 6);
-	/* BUID is base + 0x30 */
-	dt_add_property(np, "interrupt-controller", NULL, 0);
-	dt_add_property_cells(np, "interrupt-base",
-			      ((be32_to_cpu(hub->buid_ext) << 9) | 0x30) << 4);
-	dt_add_property_cells(np, "interrupt-max-count", 128);
-
-	/* Always 2 port groups */
-	for (i = 0; i < 2; i++) {
-		unsigned int clause;
-
-		switch(card->pg[i].speed) {
-		case 1000:
-			clause = 0x22;
-			break;
-		case 10000:
-			clause = 0x45;
-			break;
-		default:
-			/* Unused port group */
-			continue;
-		}
-		gnp = dt_new_addr(np, "portgroup", i + 1);
-		if (!gnp)
-			continue;
-
-		dt_add_property_cells(gnp, "reg", i + 1);
-		dt_add_property_cells(gnp, "speed", card->pg[i].speed);
-		/* XX FIXME */
-		dt_add_property_strings(gnp, "phy-type", "mdio");
-		dt_add_property_cells(gnp, "phy-mdio-addr", card->pg[i].phy_id);
-		dt_add_property_cells(gnp, "phy-mdio-clause", clause);
-		dt_add_property_cells(gnp, "subports", card->pg[i].ports);
-	}
-	return np;
-}
-
 static void io_parse_fru(const void *sp_iohubs)
 {
 	unsigned int i;
@@ -642,12 +434,6 @@ static void io_parse_fru(const void *sp_iohubs)
 			prlog(PR_INFO, "CEC:     P7IOC !\n");
 			hn = io_add_p7ioc(hub, sp_iohubs);
 			io_add_common(hn, hub);
-			break;
-		case CECHUB_HUB_P5IOC2:
-			prlog(PR_INFO, "CEC:     P5IOC2 !\n");
-			hn = io_add_p5ioc2(hub, sp_iohubs);
-			io_add_common(hn, hub);
-			io_add_hea(hub, sp_iohubs);
 			break;
 		case CECHUB_HUB_MURANO:
 		case CECHUB_HUB_MURANO_SEGU:

core/init.c
@@ -734,7 +734,6 @@ void __noreturn main_cpu_entry(const void *fdt, u32 master_cpu)
 	opal_init_msg();
 
 	/* Probe IO hubs */
-	probe_p5ioc2();
 	probe_p7ioc();
 
 	/* Probe PHB3 on P8 */

hdata/spira.h
@@ -476,7 +476,6 @@ struct cechub_io_hub {
 #define CECHUB_HUB_FAB_BR1_PDT_PHB4	0x08 /* p7ioc only */
 #define CECHUB_HUB_FAB_BR1_PDT_PHB5	0x04 /* p7ioc only */
 	__be16		iohub_id;	/* the type of hub */
-#define CECHUB_HUB_P5IOC2	0x1061	/* from VPL1 */
 #define CECHUB_HUB_P7IOC	0x60e7	/* from VPL3 */
 #define CECHUB_HUB_MURANO	0x20ef	/* Murano from spec */
 #define CECHUB_HUB_MURANO_SEGU	0x0001	/* Murano+Seguso from spec */

hw/Makefile.inc
@@ -4,7 +4,7 @@ SUBDIRS += hw
 HW_OBJS  = xscom.o chiptod.o gx.o cec.o lpc.o lpc-uart.o psi.o
 HW_OBJS += homer.o slw.o occ.o fsi-master.o centaur.o
 HW_OBJS += nx.o nx-rng.o nx-crypto.o nx-842.o
-HW_OBJS += p7ioc.o p7ioc-inits.o p7ioc-phb.o p5ioc2.o p5ioc2-phb.o
+HW_OBJS += p7ioc.o p7ioc-inits.o p7ioc-phb.o
 HW_OBJS += phb3.o sfc-ctrl.o fake-rtc.o bt.o p8-i2c.o prd.o
 HW_OBJS += dts.o lpc-rtc.o npu.o npu-hw-procedures.o
 HW=hw/built-in.o

hw/cec.c
@@ -17,7 +17,6 @@
 #include <skiboot.h>
 #include <cec.h>
 #include <p7ioc.h>
-#include <p5ioc2.h>
 #include <interrupts.h>
 #include <opal-api.h>
 
@@ -53,19 +52,17 @@ void cec_reset(void)
 	}
 }
 
+/* This was only supported by p5ioc, which was dropped */
 static int64_t opal_pci_set_hub_tce_memory(uint64_t hub_id,
-					   uint64_t tce_mem_addr,
-					   uint64_t tce_mem_size)
+					   uint64_t tce_mem_addr __unused,
+					   uint64_t tce_mem_size __unused)
 {
 	struct io_hub *hub = cec_get_hub_by_id(hub_id);
 
 	if (!hub)
 		return OPAL_PARAMETER;
 
-	if (!hub->ops->set_tce_mem)
-		return OPAL_UNSUPPORTED;
-
-	return hub->ops->set_tce_mem(hub, tce_mem_addr, tce_mem_size);
+	return OPAL_UNSUPPORTED;
 }
 opal_call(OPAL_PCI_SET_HUB_TCE_MEMORY, opal_pci_set_hub_tce_memory, 3);
 

hw/p5ioc2.c
@@ -1,298 +1,0 @@
-/* Copyright 2013-2014 IBM Corp.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * 	http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
- * implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-#include <skiboot.h>
-#include <p5ioc2.h>
-#include <p5ioc2-regs.h>
-#include <cec.h>
-#include <gx.h>
-#include <opal.h>
-#include <interrupts.h>
-#include <device.h>
-#include <timebase.h>
-#include <vpd.h>
-#include <ccan/str/str.h>
-
-
-static int64_t p5ioc2_set_tce_mem(struct io_hub *hub, uint64_t address,
-				  uint64_t size)
-{
-	struct p5ioc2 *ioc = iohub_to_p5ioc2(hub);
-	int64_t rc;
-
-	printf("P5IOC2: set_tce_mem(0x%016llx size 0x%llx)\n",
-	       address, size);
-
-	/* The address passed must be naturally aligned */
-	if (address && !is_pow2(size))
-		return OPAL_PARAMETER;
-	if (address & (size - 1))
-		return OPAL_PARAMETER;
-
-	ioc->tce_base = address;
-	ioc->tce_size = size;
-
-	rc = gx_configure_tce_bar(ioc->host_chip, ioc->gx_bus,
-				  address, size);
-	if (rc)
-		return OPAL_INTERNAL_ERROR;
-	return OPAL_SUCCESS;
-}
-
-static int64_t p5ioc2_get_diag_data(struct io_hub *hub __unused,
-				   void *diag_buffer __unused,
-				   uint64_t diag_buffer_len __unused)
-{
-	/* XXX Not yet implemented */
-	return OPAL_UNSUPPORTED;
-}
-
-static const struct io_hub_ops p5ioc2_hub_ops = {
-	.set_tce_mem	= p5ioc2_set_tce_mem,
-	.get_diag_data	= p5ioc2_get_diag_data,
-};
-
-static void p5ioc2_inits(struct p5ioc2 *ioc)
-{
-	uint64_t val;
-	unsigned int p, n;
-
-	printf("P5IOC2: Initializing hub...\n");
-
-	/*
-	 * BML base inits
-	 */
-	/* mask off interrupt presentation timeout in FIRMC */
-	out_be64(ioc->regs + (P5IOC2_FIRMC | P5IOC2_REG_OR),
-		 0x0000080000000000UL);
-
-	/* turn off display alter mode */
-	out_be64(ioc->regs + (P5IOC2_CTL | P5IOC2_REG_AND),
-		 0xffffff7fffffffffUL);
-
-	/* setup hub and clustering interrupts BUIDs to 1 and 2 */
-	out_be64(ioc->regs + P5IOC2_SBUID, 0x0001000200000000UL);
-
-	/* setup old style MSI BUID (should be unused but set it up anyway) */
-	out_be32(ioc->regs + P5IOC2_BUCO, 0xf);
-
-	/* Set XIXO bit 0 needed for "enhanced" TCEs or else TCE
-	 * fetches appear as normal memory reads on GX causing
-	 * P7 to checkstop when a TCE DKill collides with them.
-	 */
-	out_be64(ioc->regs + P5IOC2_XIXO, in_be64(ioc->regs + P5IOC2_XIXO)
-		 | P5IOC2_XIXO_ENH_TCE);
-
-	/* Clear routing tables */
-	for (n = 0; n < 16; n++) {
-		for (p = 0; p < 8; p++)
-			out_be64(ioc->regs + P5IOC2_TxRTE(p,n), 0);
-	}
-	for (n = 0; n < 32; n++)
-		out_be64(ioc->regs + P5IOC2_BUIDRTE(n), 0);
-
-	/*
-	 * Setup routing. We use the same setup that pHyp appears
-	 * to do (after inspecting the various registers with SCOM)
-	 *
-	 * We assume the BARs are already setup by the FSP such
-	 * that BAR0 is 128G (8G region size) and BAR6 is
-	 * 256M (16M region size).
-	 *
-	 * The routing is based on what pHyp and BML do, each Calgary
-	 * get one slice of BAR6 and two slices of BAR0
-	 */
-	/* BAR 0 segments 0 & 1 -> CA0 */
-	out_be64(ioc->regs + P5IOC2_TxRTE(0,0),
-		 P5IOC2_TxRTE_VALID | P5IOC2_CA0_RIO_ID);
-	out_be64(ioc->regs + P5IOC2_TxRTE(0,1),
-		 P5IOC2_TxRTE_VALID | P5IOC2_CA0_RIO_ID);
-
-	/* BAR 0 segments 2 & 3 -> CA1 */
-	out_be64(ioc->regs + P5IOC2_TxRTE(0,2),
-		 P5IOC2_TxRTE_VALID | P5IOC2_CA1_RIO_ID);
-	out_be64(ioc->regs + P5IOC2_TxRTE(0,3),
-		 P5IOC2_TxRTE_VALID | P5IOC2_CA1_RIO_ID);
-
-	/* BAR 6 segments 0 -> CA0 */
-	out_be64(ioc->regs + P5IOC2_TxRTE(6,0),
-		 P5IOC2_TxRTE_VALID | P5IOC2_CA0_RIO_ID);
-
-	/* BAR 6 segments 1 -> CA0 */
-	out_be64(ioc->regs + P5IOC2_TxRTE(6,1),
-		 P5IOC2_TxRTE_VALID | P5IOC2_CA1_RIO_ID);
-
-	/*
-	 * BUID routing, we send entries 1 to CA0 and 2 to CA1
-	 * just like pHyp and make sure the base and mask are
-	 * both clear in SID to we route the whole 512 block
-	 */
-	val = in_be64(ioc->regs + P5IOC2_SID);
-	val = SETFIELD(P5IOC2_SID_BUID_BASE, val, 0);
-	val = SETFIELD(P5IOC2_SID_BUID_MASK, val, 0);
-	out_be64(ioc->regs + P5IOC2_SID, val);
-	out_be64(ioc->regs + P5IOC2_BUIDRTE(1),
-		 P5IOC2_BUIDRTE_VALID | P5IOC2_BUIDRTE_RR_RET |
-		 P5IOC2_CA0_RIO_ID);
-	out_be64(ioc->regs + P5IOC2_BUIDRTE(2),
-		 P5IOC2_BUIDRTE_VALID | P5IOC2_BUIDRTE_RR_RET |
-		 P5IOC2_CA1_RIO_ID);
-}
-
-static void p5ioc2_ca_init(struct p5ioc2 *ioc, int ca)
-{
-	void *regs = ca ? ioc->ca1_regs : ioc->ca0_regs;
-	uint64_t val;
-
-	printf("P5IOC2: Initializing Calgary %d...\n", ca);
-
-	/* Setup device BUID */
-	val = SETFIELD(CA_DEVBUID_MASK, 0ul, ca ? P5IOC2_CA1_BUID : P5IOC2_CA0_BUID);
-	out_be32(regs + CA_DEVBUID, val);
-
-	/* Setup HubID in TARm (and keep TCE clear, Linux will init that)
-	 *
-	 * BML and pHyp sets the values to 1 for CA0 and 4 for CA1. We
-	 * keep the TAR valid bit clear as well.
-	 */
-	val = SETFIELD(CA_TAR_HUBID, 0ul, ca ? 4 : 1);
-	val = SETFIELD(CA_TAR_ALTHUBID, val, ca ? 4 : 1);
-	out_be64(regs + CA_TAR0, val);
-	out_be64(regs + CA_TAR1, val);
-	out_be64(regs + CA_TAR2, val);
-	out_be64(regs + CA_TAR3, val);
-	
-	/* Bridge config register. We set it up to the same value as observed
-	 * under pHyp on a Juno machine. The difference from the IPL value is
-	 * that TCE buffers are enabled, discard timers are increased and
-	 * we disable response status to avoid errors.
-	 */
-	//out_be64(regs + CA_CCR, 0x5045DDDED2000000);
-	// disable memlimit:
-	out_be64(regs + CA_CCR, 0x5005DDDED2000000UL);
-
-	/* The system memory base/limit etc... setup will be done when the
-	 * user enables TCE via OPAL calls
-	 */
-}
-
-static void p5ioc2_create_hub(struct dt_node *np)
-{
-	struct p5ioc2 *ioc;
-	unsigned int i, id, irq;
-	char *path;
-
-	/* Use the BUID extension as ID and add it to device-tree */
-	id = dt_prop_get_u32(np, "ibm,buid-ext");
-	path = dt_get_path(np);	
-	printf("P5IOC2: Found at %s ID 0x%x\n", path, id);
-	free(path);
-	dt_add_property_cells(np, "ibm,opal-hubid", 0, id);
-
-	/* Load VPD LID */
-	vpd_preload(np);
-	vpd_iohub_load(np);
-
-	ioc = zalloc(sizeof(struct p5ioc2));
-	if (!ioc)
-		return;
-	ioc->hub.hub_id = id;
-	ioc->hub.ops = &p5ioc2_hub_ops;
-	ioc->dt_node = np;
-
-	/* We assume SBAR == GX0 + some hard coded offset */
-	ioc->regs = (void *)dt_get_address(np, 0, NULL);
-
-	/* For debugging... */
-	for (i = 0; i < 8; i++)
-		printf("P5IOC2: BAR%d = 0x%016llx M=0x%16llx\n", i,
-		       in_be64(ioc->regs + P5IOC2_BAR(i)),
-		       in_be64(ioc->regs + P5IOC2_BARM(i)));
-
-	ioc->host_chip = dt_get_chip_id(np);
-
-	ioc->gx_bus = dt_prop_get_u32(np, "ibm,gx-index");
-
-	/* Rather than reading the BARs in P5IOC2, we "know" that
-	 * BAR6 matches GX BAR 1 and BAR0 matches GX BAR 2. This
-	 * is a bit fishy but will work for the few machines this
-	 * is intended to work on
-	 */
-	ioc->bar6 = dt_prop_get_u64(np, "ibm,gx-bar-1");
-	ioc->bar0 = dt_prop_get_u64(np, "ibm,gx-bar-2");
-
-	printf("DT BAR6 = 0x%016llx\n", ioc->bar6);
-	printf("DT BAR0 = 0x%016llx\n", ioc->bar0);
-
-	/* We setup the corresponding Calgary register bases and memory
-	 * regions. Note: those cannot be used until the routing has
-	 * been setup by inits
-	 */
-	ioc->ca0_regs = (void *)ioc->bar6 + P5IOC2_CA0_REG_OFFSET;
-	ioc->ca1_regs = (void *)ioc->bar6 + P5IOC2_CA1_REG_OFFSET;
-	ioc->ca0_mm_region = ioc->bar0 + P5IOC2_CA0_MM_OFFSET;
-	ioc->ca1_mm_region = ioc->bar0 + P5IOC2_CA1_MM_OFFSET;
-
-	/* Base of our BUIDs, will be refined later */
-	ioc->buid_base = id << 9;
-
-	/* Add interrupts: XXX These are the hub interrupts, we should add the
-	 * calgary ones as well... but we don't handle any of them currently
-	 * anyway.
-	 */
-	irq = (ioc->buid_base + 1) << 4;
-	dt_add_property_cells(np, "interrupts", irq, irq + 1);
-	dt_add_property_cells(np, "interrupt-base", irq);
-
-
-	/* Now, we do the bulk of the inits */
-	p5ioc2_inits(ioc);
-	p5ioc2_ca_init(ioc, 0);
-	p5ioc2_ca_init(ioc, 1);
-
-	/* So how do we know what PHBs to create ? Let's try all of them
-	 * and we'll see if that causes problems. TODO: Use VPD !
-	 */
-	for (i = 0; i < 4; i++)
-		p5ioc2_phb_setup(ioc, &ioc->ca0_phbs[i], 0, i, true,
-				 ioc->buid_base + P5IOC2_CA0_BUID + i + 1);
-	for (i = 0; i < 4; i++)
-		p5ioc2_phb_setup(ioc, &ioc->ca1_phbs[i], 1, i, true,
-				 ioc->buid_base + P5IOC2_CA1_BUID + i + 1);
-
-	/* Reset delay... synchronous, hope we never do that as a
-	 * result of an OPAL callback. We shouldn't really need this
-	 * here and may fold it in the generic slot init sequence but
-	 * it's not like we care much about that p5ioc2 code...
-	 *
-	 * This is mostly to give devices a chance to settle after
-	 * having lifted the reset pin on PCI-X.
-	 */
-	time_wait_ms(1000);
-
-	printf("P5IOC2: Initialization complete\n");
-
-	cec_register(&ioc->hub);
-}
-
-void probe_p5ioc2(void)
-{
-	struct dt_node *np;
-
-	dt_for_each_compatible(dt_root, np, "ibm,p5ioc2")
-		p5ioc2_create_hub(np);
-}
-

hw/p7ioc.c
@@ -186,7 +186,6 @@ static int64_t p7ioc_get_diag_data(struct io_hub *hub,
 }
 
 static const struct io_hub_ops p7ioc_hub_ops = {
-	.set_tce_mem	= NULL, /* No set_tce_mem for p7ioc, we use FMTC */
 	.get_diag_data	= p7ioc_get_diag_data,
 	.reset		= p7ioc_reset,
 };

commit 578a86a887b609d0314dc660d4968469b2aa58de

Remove missed bit of p5ioc code
Probably due to bad merge by me

Fixes: 0baf63f38757
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

hdata/iohub.c
@@ -130,58 +130,6 @@ static void io_get_loc_code(const void *sp_iohubs, struct dt_node *hn, const cha
 	}
 }
 
-static struct dt_node *io_add_p5ioc2(const struct cechub_io_hub *hub,
-				     const void *sp_iohubs)
-{
-	struct dt_node *hn;
-	uint64_t reg[2];
-
-	const void *kwvpd;
-	unsigned int kwvpd_sz;
-
-	prlog(PR_DEBUG, "    GX#%d BUID_Ext = 0x%x\n",
-	      be32_to_cpu(hub->gx_index),
-	      be32_to_cpu(hub->buid_ext));
-	prlog(PR_DEBUG, "    GX BAR 0 = 0x%016"PRIx64"\n",
-	      be64_to_cpu(hub->gx_ctrl_bar0));
-	prlog(PR_DEBUG, "    GX BAR 1 = 0x%016"PRIx64"\n",
-	      be64_to_cpu(hub->gx_ctrl_bar1));
-	prlog(PR_DEBUG, "    GX BAR 2 = 0x%016"PRIx64"\n",
-	      be64_to_cpu(hub->gx_ctrl_bar2));
-	prlog(PR_DEBUG, "    GX BAR 3 = 0x%016"PRIx64"\n",
-	      be64_to_cpu(hub->gx_ctrl_bar3));
-	prlog(PR_DEBUG, "    GX BAR 4 = 0x%016"PRIx64"\n",
-	      be64_to_cpu(hub->gx_ctrl_bar4));
-
-	/* We assume SBAR == GX0 + some hard coded offset */
-	reg[0] = cleanup_addr(be64_to_cpu(hub->gx_ctrl_bar0) + P5IOC2_REGS_OFFSET);
-	reg[1] = 0x2000000;
-
-	hn = dt_new_addr(dt_root, "io-hub", reg[0]);
-	if (!hn)
-		return NULL;
-
-	dt_add_property(hn, "reg", reg, sizeof(reg));
-	dt_add_property_strings(hn, "compatible", "ibm,p5ioc2");
-
-	kwvpd = HDIF_get_idata(sp_iohubs, CECHUB_ASCII_KEYWORD_VPD, &kwvpd_sz);
-	if (kwvpd && kwvpd != sp_iohubs) {
-		/*
-		 * XX We don't know how to properly find the LXRn
-		 * record so for now we'll just try LXR0 and if not
-		 * found, we try LXR1
-		 */
-		if (!io_get_lx_info(kwvpd, kwvpd_sz, 0, hn))
-			io_get_lx_info(kwvpd, kwvpd_sz, 1, hn);
-	} else
-		prlog(PR_DEBUG, "CEC:     P5IOC2 Keywords not found.\n");
-
-	/* Get slots base loc code */
-	io_get_loc_code(sp_iohubs, hn, "ibm,io-base-loc-code");
-
-	return hn;
-}
-
 static struct dt_node *io_add_p7ioc(const struct cechub_io_hub *hub,
 				    const void *sp_iohubs)
 {