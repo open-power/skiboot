open-power/skiboot

commit a5299ba29b250aa9c87eb59d500fb1489c915af2

IPMI: Only log events that needs attention
Presently we are logging all the events to service processor (FSP/BMC).
But on BMC machines we should only log events that requires attention.

As per PEL spec, we should log events with severity >= 0x22 and "service
action flag" is "on". But in our case, all logs OPAL originagted logs
are makred as report externally.

So lets log all events that are originated from OAPL (presently all logs
as payload is not logging any PEL event) and  severity >= 0x22.

Signed-off-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>
Cc: Ananth N Mavinakayanahalli <ananth@in.ibm.com>
Cc: Alistair Popple <alistair@popple.id.au>
Cc: Jeremy Kerr <jk@ozlabs.org>
[stewart@linux.vnet.ibm.com: s/SEL:/IPMI:/ in prlog]
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

hw/ipmi/ipmi-sel.c
@@ -167,6 +167,13 @@ int ipmi_elog_commit(struct errorlog *elog_buf)
 {
 	struct ipmi_msg *msg;
 
+	/* Only log events that needs attention */
+	if (elog_buf->event_severity < OPAL_PREDICTIVE_ERR_FAULT_RECTIFY_REBOOT ||
+	    elog_buf->elog_origin != ORG_SAPPHIRE) {
+		prlog(PR_INFO, "IPMI: dropping non severe PEL event\n");
+		return 0;
+	}
+
 	/* We pass a large request size in to mkmsg so that we have a
 	 * large enough allocation to reuse the message to pass the
 	 * PEL data via a series of partial add commands.  */

commit 8dcb68e5ddb5db926c0c9a97c99d5e31cbc405ff

ipmi-sel: Fix memory leak in error path
Commit a5299ba2 dropped non-severe event from logging to BMC, but I forgot
to releaes the error log structure.

Fixes: a5299ba2 (IPMI: Only log events that needs attention)
Signed-off-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

hw/ipmi/ipmi-sel.c
@@ -404,6 +404,7 @@ int ipmi_elog_commit(struct errorlog *elog_buf)
 	if (elog_buf->event_severity < OPAL_PREDICTIVE_ERR_FAULT_RECTIFY_REBOOT ||
 	    elog_buf->elog_origin != ORG_SAPPHIRE) {
 		prlog(PR_INFO, "dropping non severe PEL event\n");
+		opal_elog_complete(elog_buf, true);
 		return 0;
 	}
 