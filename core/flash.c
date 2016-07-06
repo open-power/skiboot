f1f38e6ca8342737ee34d58f6a73abd05ea8d28c
Stewart Smith <stewart@linux.vnet.ibm.com>
2016-07-06 11:53:05 +1000

    Revert "flash: Use blocklevel to do ECC reads"
    Was causing boot failures on Garrison and Firestone (likely other
    OpenPower platforms)
    
    This reverts commit 74ba83462c64d6a987ed4785aee55309daf9ffb6.
    
    Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

--- core/flash.c
+++ core/flash.c
@@ -573,7 +573,7 @@ static int flash_load_resource(enum resource_id id, uint32_t subid,
 		goto out_unlock;
 	}
 
-	rc = ffs_init(0, flash->size, flash->bl, &ffs, 1);
+	rc = ffs_init(0, flash->size, flash->bl, &ffs, 0);
 	if (rc) {
 		prerror("FLASH: Can't open ffs handle\n");
 		goto out_unlock;
@@ -623,7 +623,7 @@ static int flash_load_resource(enum resource_id id, uint32_t subid,
 		goto out_free_ffs;
 	}
 
-	rc = blocklevel_read(flash->bl, part_start, buf, size);
+	rc = flash_read_corrected(flash->bl, part_start, buf, size, ecc);
 	if (rc) {
 		prerror("FLASH: failed to read %s partition\n", name);
 		goto out_free_ffs;
