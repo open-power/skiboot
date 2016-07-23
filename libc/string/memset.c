ad1b72c40762bc9c50f46d9fc2620faaa25e10bd
Benjamin Herrenschmidt <benh@kernel.crashing.org>
2016-07-24 09:27:05 +1000

    libc: Use 8-bytes stores for non-0 memset too
    Memory poisoning hammers this, so let's be a bit smart about it and
    avoid falling back to byte stores when the data is not 0
    
    Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

--- libc/string/memset.c
+++ libc/string/memset.c
@@ -18,6 +18,7 @@ void *
 memset(void *dest, int c, size_t size)
 {
 	unsigned char *d = (unsigned char *)dest;
+	unsigned long big_c = 0;
 
 #if defined(__powerpc__) || defined(__powerpc64__)
 	if (size > CACHE_LINE_SIZE && c==0) {
@@ -33,8 +34,14 @@ memset(void *dest, int c, size_t size)
 	}
 #endif
 
+	if (c) {
+		big_c = c;
+		big_c |= (big_c << 8) | big_c;
+		big_c |= (big_c << 16) | big_c;
+		big_c |= (big_c << 32) | big_c;
+	}
 	while (size >= 8 && c == 0) {
-		*((unsigned long long*)d) = 0ULL;
+		*((unsigned long *)d) = big_c;
 		d+=8;
 		size-=8;
 	}
