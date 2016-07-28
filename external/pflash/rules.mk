7f8c76cf8143793c6ab7efd50debb73018304407
Joel Stanley <joel@jms.id.au>
2016-07-28 14:15:58 +0930

    pflash: Fix the makefile
    Someone was a bit too keen with the cleanups last time. Restore the
    ability for pflash to build in non-shared mode.
    
    Fixes: c327eddd9b29 (pflash: Clean up makefiles and resolve build race)
    Signed-off-by: Joel Stanley <joel@jms.id.au>
    Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>
    (cherry picked from commit fd599965f723330da5ec55519c20cdb6aa2b3a2d)
    Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

--- external/pflash/rules.mk
+++ external/pflash/rules.mk
@@ -1,19 +1,22 @@
 .DEFAULT_GOAL := all
 
 override CFLAGS  += -O2 -Wall -I.
-PFLASH_OBJS    := pflash.o progress.o version.o common-arch_flash.o
-OBJS	:= $(PFLASH_OBJS) $(LIBFLASH_OBJS)
-EXE     = pflash
-sbindir?=/usr/sbin
+LIBFLASH_FILES	:= libflash.c libffs.c ecc.c blocklevel.c file.c
+LIBFLASH_OBJS	:= $(addprefix libflash-, $(LIBFLASH_FILES:.c=.o))
+LIBFLASH_SRC	:= $(addprefix libflash/,$(LIBFLASH_FILES))
+PFLASH_OBJS	:= pflash.o progress.o version.o common-arch_flash.o
+OBJS		:= $(PFLASH_OBJS) $(LIBFLASH_OBJS)
+EXE     	:= pflash
+sbindir		?= /usr/sbin
 
-PFLASH_VERSION ?= $(shell ../../make_version.sh $(EXE))
-LINKAGE?=static
+PFLASH_VERSION	?= $(shell ../../make_version.sh $(EXE))
+LINKAGE		?= static
 
 ifeq ($(LINKAGE),dynamic)
 include ../shared/rules.mk
-SHARED	:= ../shared/$(SHARED_NAME)
-OBJS	:= $(PFLASH_OBJS) $(SHARED)
-INSTALLDEPS+=install-shared
+SHARED		:= ../shared/$(SHARED_NAME)
+OBJS		:= $(PFLASH_OBJS) $(SHARED)
+INSTALLDEPS	+= install-shared
 
 install-shared:
 	$(MAKE) -C ../shared install PREFIX=$(PREFIX)
@@ -30,6 +33,11 @@ version.c: .version
 	fi) > $@
 
 %.o : %.c
+	$(Q_CC)$(CC) $(CFLAGS) -c $< -o $@
+
+$(LIBFLASH_SRC): | links
+
+$(LIBFLASH_OBJS): libflash-%.o : libflash/%.c
 	$(Q_CC)$(CC) $(CFLAGS) -c $< -o $@
 
 $(EXE): $(OBJS)
