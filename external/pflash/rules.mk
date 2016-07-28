open-power/skiboot

commit c327eddd9b291a0e6e54001fa3b1e547bad3fca2

pflash: Clean up makefiles and resolve build race
The pflash build process has regressed from when the were last fixed in
6c21c4ffaf82.

This patch resolves that issue and performs some cleanups:

 - Remove duplicated rules. Patches had moved rules into common files,
   but forgotten to remove them from the pflash makefiles.

 - Make assignements simply expanded variables where possible. Form the
   make manual:

     Functions referenced in the definition will be executed every time
     the variable is expanded.  This makes make run slower; worse, it
     causes the wildcard and shell functions to give unpredictable
     results because you cannot easily control when they are called, or
     even how many times.

     To avoid all the problems and inconveniences of recursively
     expanded variables, there is another flavor: simply expanded
     variables.

 - set the 'shared' target as a dependency of the libflash objects. This
   was the final piece to resolve the race condition.

The failed build could be reproduced by doing a `git clean -f -x` and
then running the following:

  $ make -j 32 CROSS_COMPILE=arm-linux-gnueabi- SKIBOOT_VERSION=5.2.4
  PFLASH_VERSION=5.2.4 V=1 -C external/pflash all LINKAGE=dynamic
  make: Entering directory '/home/joel/dev/skiboot/external/pflash'
  ln -sf ../../libflash ./libflash
  ln -sf ../../ccan ./ccan
  ln -sf ../common ./common
  cc -O2 -Wall -I. -c pflash.c -o pflash.o
  cc -O2 -Wall -I. -c progress.c -o progress.o
  make -C ../shared
  make[1]: Entering directory '/home/joel/dev/skiboot/external/shared'
  ln -sf ../../hw/ast-bmc/ast-sf-ctrl.c common/ast-sf-ctrl.c
  ln -sf ../../include/ast.h common/ast.h
  ln -sf arch_flash_arm_io.h common/io.h
  cc -O2 -Wall -I.  -c common/arch_flash_common.c -o
  common-arch_flash_common.o
  cc -O2 -Wall -I.  -c common/arch_flash_arm.c -o common-arch_flash_arm.o
  cc -O2 -Wall -I.  -c common/ast-sf-ctrl.c -o common-ast-sf-ctrl.o
  cc -O2 -Wall -I. -c version.c -o version.o

external/pflash/rules.mk
@@ -1,11 +1,8 @@
 .DEFAULT_GOAL := all
 
 override CFLAGS  += -O2 -Wall -I.
-PFLASH_OBJS    = pflash.o progress.o version.o common-arch_flash.o
-LIBFLASH_FILES := libflash.c libffs.c ecc.c blocklevel.c file.c
-LIBFLASH_OBJS := $(addprefix libflash-, $(LIBFLASH_FILES:.c=.o))
-LIBFLASH_SRC := $(addprefix libflash/,$(LIBFLASH_FILES))
-OBJS	= $(PFLASH_OBJS) $(LIBFLASH_OBJS)
+PFLASH_OBJS    := pflash.o progress.o version.o common-arch_flash.o
+OBJS	:= $(PFLASH_OBJS) $(LIBFLASH_OBJS)
 EXE     = pflash
 sbindir?=/usr/sbin
 
@@ -14,8 +11,8 @@ LINKAGE?=static
 
 ifeq ($(LINKAGE),dynamic)
 include ../shared/rules.mk
-SHARED=../shared/$(SHARED_NAME)
-OBJS=$(PFLASH_OBJS) $(SHARED)
+SHARED	:= ../shared/$(SHARED_NAME)
+OBJS	:= $(PFLASH_OBJS) $(SHARED)
 INSTALLDEPS+=install-shared
 
 install-shared:
@@ -33,11 +30,6 @@ version.c: .version
 	fi) > $@
 
 %.o : %.c
-	$(Q_CC)$(CC) $(CFLAGS) -c $< -o $@
-
-$(LIBFLASH_SRC): | links
-
-$(LIBFLASH_OBJS): libflash-%.o : libflash/%.c
 	$(Q_CC)$(CC) $(CFLAGS) -c $< -o $@
 
 $(EXE): $(OBJS)

external/pflash/Makefile
@@ -1,5 +1,5 @@
 include rules.mk
-GET_ARCH = ../../external/common/get_arch.sh
+GET_ARCH := ../../external/common/get_arch.sh
 include ../../external/common/rules.mk
 
 all: links arch_links $(EXE)

external/shared/Makefile
@@ -1,23 +1,30 @@
 .DEFAULT_GOAL := all
-GET_ARCH = ../../external/common/get_arch.sh
+GET_ARCH := ../../external/common/get_arch.sh
 include ../../external/common/rules.mk
 include rules.mk
 
 PREFIX ?= /usr/local/
-LIBDIR = $(PREFIX)/lib
-INCDIR = $(PREFIX)/include/libflash
+LIBDIR := $(PREFIX)/lib
+INCDIR := $(PREFIX)/include/libflash
 
 ifneq ($(ARCH), ARCH_ARM)
 CFLAGS += -m64
 endif
 CFLAGS += -Werror -Wall -g2 -ggdb -I. -fPIC
 
-LIBFLASH_OBJS = libflash-file.o libflash-libflash.o libflash-libffs.o libflash-ecc.o libflash-blocklevel.o
-ARCHFLASH_OBJS = common-arch_flash.o
-OBJS = $(LIBFLASH_OBJS) $(ARCHFLASH_OBJS)
+LIBFLASH_OBJS := libflash-file.o libflash-libflash.o libflash-libffs.o \
+	libflash-ecc.o libflash-blocklevel.o
+ARCHFLASH_OBJS := common-arch_flash.o
+OBJS := $(LIBFLASH_OBJS) $(ARCHFLASH_OBJS)
 
-LIBFLASH_H = libflash/file.h libflash/libflash.h libflash/libffs.h libflash/ffs.h libflash/ecc.h libflash/blocklevel.h libflash/errors.h
-ARCHFLASH_H = common/arch_flash.h
+LIBFLASH_H := libflash/file.h libflash/libflash.h libflash/libffs.h \
+	libflash/ffs.h libflash/ecc.h libflash/blocklevel.h libflash/errors.h
+ARCHFLASH_H := common/arch_flash.h
+
+LIBFLASH_FILES := libflash.c libffs.c ecc.c blocklevel.c file.c
+LIBFLASH_SRC := $(addprefix libflash/,$(LIBFLASH_FILES))
+
+$(LIBFLASH_SRC): | links
 
 $(LIBFLASH_OBJS) : libflash-%.o : libflash/%.c
 	$(CC) $(CFLAGS) $(CPPFLAGS) -c $< -o $@

commit fd599965f723330da5ec55519c20cdb6aa2b3a2d

pflash: Fix the makefile
Someone was a bit too keen with the cleanups last time. Restore the
ability for pflash to build in non-shared mode.

Fixes: c327eddd9b29 (pflash: Clean up makefiles and resolve build race)
Signed-off-by: Joel Stanley <joel@jms.id.au>
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

external/pflash/rules.mk
@@ -1,19 +1,22 @@
 .DEFAULT_GOAL := all
 
 override CFLAGS  += -O2 -Wall -I.
-PFLASH_OBJS    := pflash.o progress.o version.o common-arch_flash.o
-OBJS	:= $(PFLASH_OBJS) $(LIBFLASH_OBJS)
-EXE     = pflash
-sbindir?=/usr/sbin
+LIBFLASH_FILES	:= libflash.c libffs.c ecc.c blocklevel.c file.c
+LIBFLASH_OBJS	:= $(addprefix libflash-, $(LIBFLASH_FILES:.c=.o))
+LIBFLASH_SRC	:= $(addprefix libflash/,$(LIBFLASH_FILES))
+PFLASH_OBJS	:= pflash.o progress.o version.o common-arch_flash.o
+OBJS		:= $(PFLASH_OBJS) $(LIBFLASH_OBJS)
+EXE     	:= pflash
+sbindir		?= /usr/sbin
 
-PFLASH_VERSION ?= $(shell ../../make_version.sh $(EXE))
-LINKAGE?=static
+PFLASH_VERSION	?= $(shell ../../make_version.sh $(EXE))
+LINKAGE		?= static
 
 ifeq ($(LINKAGE),dynamic)
 include ../shared/rules.mk
-SHARED	:= ../shared/$(SHARED_NAME)
-OBJS	:= $(PFLASH_OBJS) $(SHARED)
-INSTALLDEPS+=install-shared
+SHARED		:= ../shared/$(SHARED_NAME)
+OBJS		:= $(PFLASH_OBJS) $(SHARED)
+INSTALLDEPS	+= install-shared
 
 install-shared:
 	$(MAKE) -C ../shared install PREFIX=$(PREFIX)
@@ -30,6 +33,11 @@ version.c: .version
 	fi) > $@
 
 %.o : %.c
+	$(Q_CC)$(CC) $(CFLAGS) -c $< -o $@
+
+$(LIBFLASH_SRC): | links
+
+$(LIBFLASH_OBJS): libflash-%.o : libflash/%.c
 	$(Q_CC)$(CC) $(CFLAGS) -c $< -o $@
 
 $(EXE): $(OBJS)