open-power/skiboot

commit 81a538a678edf666568ca4adffe074b3dbce6dc3

core/flash: Fix passing pointer instead of value
flash_find_subpartition() accepts a pointer to a boolean variable
indicating ecc for a region of flash and passes the pointer directly
to flash_read_corrected() which actually only wants the value. This
has always worked probably because there has always been ECC on
sub partitions.

How there aren't any warnings triggered by this condition escapes me.

Fixes: 6c26bc7 ("libflash: move ffs_flash_read into libflash")
Signed-off-by: Cyril Bur <cyril.bur@au1.ibm.com>
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

core/flash.c
@@ -455,7 +455,7 @@ static int flash_find_subpartition(struct blocklevel_device *bl, uint32_t subid,
 
 	/* Get the TOC */
 	rc = flash_read_corrected(bl, *start, header,
-			FLASH_SUBPART_HEADER_SIZE, ecc);
+			FLASH_SUBPART_HEADER_SIZE, *ecc);
 	if (rc) {
 		prerror("FLASH: flash subpartition TOC read failed %i\n", rc);
 		goto end;

commit f3c7237ffca29d9f7f4284db23c7154ed8bb4854

core/flash: More pointer vs value fixes
It appears the inital patch was incomplete and coverity has caught
more issues. The initial patch addressed one aspect of passing
pointers instead of values, actual function prototypes needed to be
changed and callers adjusted.

Fixes coverity 134277 and 134278

Fixes: 81a538a ("core/flash: Fix passing pointer instead of value")
Signed-off-by: Cyril Bur <cyril.bur@au1.ibm.com>
Reviewed-by: Mukesh Ojha <mukesh02@linux.vnet.ibm.com>
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

core/flash.c
@@ -437,7 +437,7 @@ struct flash_hostboot_header {
 /* start and total size include ECC */
 static int flash_find_subpartition(struct blocklevel_device *bl, uint32_t subid,
 				   uint32_t *start, uint32_t *total_size,
-				   bool *ecc)
+				   bool ecc)
 {
 	struct flash_hostboot_header *header;
 	char eyecatcher[5];
@@ -455,7 +455,7 @@ static int flash_find_subpartition(struct blocklevel_device *bl, uint32_t subid,
 
 	/* Get the TOC */
 	rc = flash_read_corrected(bl, *start, header,
-			FLASH_SUBPART_HEADER_SIZE, *ecc);
+			FLASH_SUBPART_HEADER_SIZE, ecc);
 	if (rc) {
 		prerror("FLASH: flash subpartition TOC read failed %i\n", rc);
 		goto end;
@@ -610,7 +610,7 @@ static int flash_load_resource(enum resource_id id, uint32_t subid,
 	/* Find the sub partition if required */
 	if (subid != RESOURCE_SUBID_NONE) {
 		rc = flash_find_subpartition(flash->bl, subid, &part_start,
-					     &part_size, &ecc);
+					     &part_size, ecc);
 		if (rc)
 			goto out_free_ffs;
 	}