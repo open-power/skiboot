open-power/skiboot

commit 86640b032d79ff08b01888731e43e3187c3e92b2

pflash: Fix printf format warning
pflash.c: In function ‘print_flash_info’:
pflash.c:138:27: warning: format ‘%d’ expects argument of type ‘int’, but argument 2 has type ‘uint64_t {aka long unsigned int}’ [-Wformat=]
  printf("Total size    = %dMB \n", fl_total_size >> 20);

Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

external/pflash/pflash.c
@@ -29,6 +29,7 @@
 #include <limits.h>
 #include <arpa/inet.h>
 #include <assert.h>
+#include <inttypes.h>
 
 #include <libflash/libflash.h>
 #include <libflash/libffs.h>
@@ -135,7 +136,7 @@ static void print_flash_info(uint32_t toc)
 	printf("Flash info:\n");
 	printf("-----------\n");
 	printf("Name          = %s\n", fl_name);
-	printf("Total size    = %dMB \n", fl_total_size >> 20);
+	printf("Total size    = %"PRIu64"dMB \n", fl_total_size >> 20);
 	printf("Erase granule = %dKB \n", fl_erase_granule >> 10);
 
 	if (bmc_flash)

commit 8c3f5cdca9a644afc62b2acdc9bdf4ea3673d213

pflash: remove stray d in from info message
Fixes: 86640b032d79ff0 (pflash: Fix printf format warning)
Signed-off-by: Joel Stanley <joel@jms.id.au>
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

external/pflash/pflash.c
@@ -136,7 +136,7 @@ static void print_flash_info(uint32_t toc)
 	printf("Flash info:\n");
 	printf("-----------\n");
 	printf("Name          = %s\n", fl_name);
-	printf("Total size    = %"PRIu64"dMB \n", fl_total_size >> 20);
+	printf("Total size    = %"PRIu64"MB \n", fl_total_size >> 20);
 	printf("Erase granule = %dKB \n", fl_erase_granule >> 10);
 
 	if (bmc_flash)