open-power/skiboot

commit 56ad053c3e8bf0764ad5878cb018f00a389d30cf

Add i2c Nuvoton TPM 2.0 Driver
This adds the 1/5 step performed by the TPM I2C Nuvoton driver to
transmit a command to the TPM device. In this step the driver
checks if the TPM device is ready to receive a new command.

This adds the 2/5 step performed by the TPM I2C Nuvoton driver to
transmit a command to the TPM device. In this step the driver
writes a given command to master I2C FIFO.

This adds the 3/5 step performed by the TPM I2C Nuvoton driver to
transmit a command to the TPM device. In this step the driver
sets the TPMGO bit in the I2C master status register to indicate that
the command stored in the FIFO can be sent to the TPM device.

This adds the 4/5 step performed by the TPM I2C Nuvoton driver to
transmit a command to the TPM device. In this step the driver
reads from the I2C master FIFO the result that the TPM device returned
for the last command sent.

This adds the 5/5 step performed by the TPM I2C Nuvoton driver to
transmit a command to the TPM device. In this step the driver
sets the COMMAND_READY bit in the status register to indicate that the
TPM device is ready to receive a new command.

This adds the probe function to the TPM Nuvoton driver and also updates
the tpm_init() in tpm_chip.c to call the probe function.

Signed-off-by: Claudio Carvalho <cclaudio@linux.vnet.ibm.com>
[stewart@linux.vnet.ibm.com: squash commits into one]
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

libstb/drivers/tpm_i2c_nuvoton.c
@@ -1,0 +1,504 @@
+/* Copyright 2013-2016 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/****************************************************************************
+ * THIS DRIVER WAS DEVELOPED BASED ON:
+ * https://github.com/open-power/hostboot/blob/master-p8/src/usr/i2c/tpmdd.C
+ ****************************************************************************/
+
+#include <timebase.h>
+#include <skiboot.h>
+#include <device.h>
+#include <i2c.h>
+#include "../status_codes.h"
+#include "../tpm_chip.h"
+#include "tpm_i2c_interface.h"
+#include "tpm_i2c_nuvoton.h"
+
+//#define DBG(fmt, ...) prlog(PR_DEBUG, fmt, ##__VA_ARGS__)
+#define DBG(fmt, ...)
+
+#define DRIVER_NAME "i2c_tpm_nuvoton"
+
+/*
+ * Timings between various states or transitions within the interface protocol
+ * as defined in the TCG PC Client Platform TPM Profile specification, Revision
+ * 00.43.
+ */
+#define TCG_PTP_TIMEOUT_A	750
+#define TCG_PTP_TIMEOUT_B	2000
+#define TCG_PTP_TIMEOUT_D	30
+
+/* I2C interface offsets */
+#define NUVOTON_TPM_STS			0x00
+#define NUVOTON_TPM_BURST_COUNT		0x01
+#define NUVOTON_TPM_DATA_FIFO_W		0x20
+#define NUVOTON_TPM_DATA_FIFO_R		0x40
+
+/* Bit masks for the TPM STATUS register */
+#define TCG_PTP_STS_VALID		0x80
+#define TCG_PTP_STS_COMMAND_READY	0x40
+#define TCG_PTP_STS_GO			0x20
+#define TCG_PTP_STS_DATA_AVAIL		0x10
+#define TCG_PTP_STS_EXPECT		0x08
+
+
+/* TPM Driver values */
+#define MAX_STSVALID_POLLS 	5   /* Max poll of 50ms (5*10ms) */
+#define TPM_TIMEOUT_INTERVAL	10
+
+static struct tpm_dev *tpm_device = NULL;
+
+static int tpm_status_write_byte(uint8_t byte)
+{
+	uint8_t value = byte;
+	return tpm_i2c_request_send(tpm_device->bus_id, tpm_device->xscom_base,
+				    SMBUS_WRITE, NUVOTON_TPM_STS, 1, &value,
+				    sizeof(value));
+}
+
+static int tpm_read_sts_reg_valid(uint8_t* value)
+{
+	int polls, rc;
+
+	for(polls=0; polls<=MAX_STSVALID_POLLS; polls++) {
+		rc = tpm_i2c_request_send(tpm_device->bus_id,
+					  tpm_device->xscom_base, SMBUS_READ,
+					  NUVOTON_TPM_STS, 1, value, sizeof(uint8_t));
+		if (rc < 0)
+			return rc;
+		if (rc == 0  &&
+		    ((*value & TCG_PTP_STS_VALID) == TCG_PTP_STS_VALID))
+			return 0;
+		/* Wait TPM STS register be settled */
+		time_wait_ms(5);
+	}
+	value = 0;
+	/**
+	 * @fwts-label TPMValidBitTimeout
+	 * @fwts-advice The valid bit of the tpm status register is taking
+	 * longer to be settled. Either the wait time needs to be increased
+	 * or the TPM device is not functional.
+	 */
+	prlog(PR_ERR, "TPM: valid bit not settled. Timeout.\n");
+	return STB_TPM_TIMEOUT;
+}
+
+static bool tpm_is_command_ready(int* rc)
+{
+	uint8_t value = 0;
+	*rc = tpm_i2c_request_send(tpm_device->bus_id, tpm_device->xscom_base,
+				   SMBUS_READ, NUVOTON_TPM_STS, 1, &value,
+				   sizeof(value));
+	if (*rc == 0  &&
+	   ((value & TCG_PTP_STS_COMMAND_READY) == TCG_PTP_STS_COMMAND_READY)){
+		DBG("---- TPM is command ready\n");
+		return true;
+	}
+	return false;
+}
+
+static int tpm_poll_for_command_ready(void)
+{
+	int rc, polls, delay;
+	/*
+	 * The first write to command ready may just abort an
+	 * outstanding command, so we poll twice
+	 */
+	for (polls=0; polls<2; polls++) {
+		rc = tpm_status_write_byte(TCG_PTP_STS_COMMAND_READY);
+		if (rc < 0) {
+			return rc;
+		}
+		for (delay = 0; delay < TCG_PTP_TIMEOUT_B;
+		     delay += TPM_TIMEOUT_INTERVAL) {
+			if (tpm_is_command_ready(&rc))
+				return rc;
+			time_wait_ms(TPM_TIMEOUT_INTERVAL);
+		}
+		DBG("--- Command ready polling, delay %d/%d\n",
+		    delay, TCG_PTP_TIMEOUT_B);
+	}
+	/**
+	 * @fwts-label TPMCommandReadyBitTimeout
+	 * @fwts-advice The command ready bit of the tpm status register is
+	 * taking longer to be settled. Either the wait time need to be
+	 * increased or the TPM device is not functional.
+	 */
+	prlog(PR_ERR, "TPM: command ready polling timeout\n");
+	return STB_TPM_TIMEOUT;
+}
+
+static bool tpm_is_expecting(int* rc)
+{
+	uint8_t value = 0;
+	*rc = tpm_read_sts_reg_valid(&value);
+	if (*rc == 0  &&
+	    (( value & TCG_PTP_STS_EXPECT) == TCG_PTP_STS_EXPECT))
+		return true;
+	return false;
+}
+
+static bool tpm_is_data_avail(int* rc)
+{
+	uint8_t value = 0;
+
+	*rc = tpm_read_sts_reg_valid(&value);
+
+	if (*rc == 0 && (( value &
+			   TCG_PTP_STS_DATA_AVAIL) == TCG_PTP_STS_DATA_AVAIL))
+		return true;
+
+	return false;
+}
+
+static int tpm_poll_for_data_avail(void)
+{
+	int delay, rc;
+
+	for (delay = 0; delay < TCG_PTP_TIMEOUT_A;
+	     delay += TPM_TIMEOUT_INTERVAL) {
+		if (tpm_is_data_avail(&rc)) {
+			DBG("---- read FIFO. Data available. delay=%d/%d\n",
+			    delay, TCG_PTP_TIMEOUT_A);
+			return rc;
+		}
+		time_wait_ms(TPM_TIMEOUT_INTERVAL);
+	}
+	/**
+	 * @fwts-label TPMDataAvailBitTimeout
+	 * @fwts-advice The data avail bit of the tpm status register is taking
+	 * longer to be settled. Either the wait time need to be increased or
+	 * the TPM device is not functional.
+	 */
+	prlog(PR_ERR, "TPM: read FIFO. Polling timeout, delay=%d/%d\n",
+	      delay, TCG_PTP_TIMEOUT_A);
+	return STB_TPM_TIMEOUT;
+}
+
+static int tpm_read_burst_count(uint8_t* burst_count)
+{
+	int rc = 0;
+	/* In i2C, burstCount is 1 byte */
+	rc = tpm_i2c_request_send(tpm_device->bus_id, tpm_device->xscom_base,
+				  SMBUS_READ, NUVOTON_TPM_BURST_COUNT, 1,
+				  burst_count, sizeof(uint8_t));
+	DBG("---- burst_count=%d rc=%d\n", *burst_count, rc);
+	if (rc < 0)
+		*burst_count = 0;
+	return rc;
+}
+
+static int tpm_write_fifo(uint8_t* buf, size_t buflen)
+{
+	uint8_t burst_count = 0;
+	int delay = 0;
+	int rc;
+	size_t curByte = 0;
+	uint8_t* bytePtr = buf;
+	uint8_t* curBytePtr = NULL;
+	/*
+	 * We will transfer the command except for the last byte
+	 * that will be transfered separately to allow for
+	 * overflow checking
+	 */
+	size_t length = buflen - 1;
+	size_t tx_len = 0;
+
+	do {
+		rc = tpm_read_burst_count(&burst_count);
+		if (rc < 0) {
+			return rc;
+		} else if (burst_count == 0) {
+			/* Need to delay to allow the TPM time */
+			time_wait_ms(TPM_TIMEOUT_INTERVAL);
+			delay += TPM_TIMEOUT_INTERVAL;
+			continue;
+		}
+		/*
+		 * Send in some data
+		 */
+		curBytePtr = &(bytePtr[curByte]);
+		tx_len = (curByte + burst_count > length ?
+			  (length - curByte) : burst_count);
+		rc = tpm_i2c_request_send(tpm_device->bus_id,
+					  tpm_device->xscom_base,
+					  SMBUS_WRITE, NUVOTON_TPM_DATA_FIFO_W,
+					  1, curBytePtr, tx_len);
+		curByte += tx_len;
+		DBG("%s write FIFO sent %zd bytes."
+		    " burstcount polling delay=%d/%d, rc=%d\n",
+		    (rc) ? "!!!!" : "----", curByte, delay,
+		    TCG_PTP_TIMEOUT_D, rc);
+		delay = 0;
+		if (rc < 0)
+			return rc;
+
+		if (!tpm_is_expecting(&rc)) {
+			/**
+			 * @fwts-label TPMWriteFifoOverflow1
+			 * @fwts-advice The write to the TPM FIFO overflowed,
+			 * the TPM is not expecting more data. This indicates a bug
+			 * in the TPM device driver.
+			 */
+			prlog(PR_ERR, "TPM: write FIFO overflow1\n");
+			return STB_TPM_OVERFLOW;
+		}
+		/* Everything but the last byte sent? */
+		if (curByte >= length)
+			break;
+	} while (delay < TCG_PTP_TIMEOUT_D);
+
+	if (delay < TCG_PTP_TIMEOUT_D) {
+		/*
+		 *  Send the final byte
+		 */
+		delay = 0;
+		do {
+			rc = tpm_read_burst_count(&burst_count);
+			if (rc < 0) {
+				return rc;
+			} else if (burst_count == 0) {
+				/* Need to delay to allow the TPM time */
+				time_wait_ms(TPM_TIMEOUT_INTERVAL);
+				delay += TPM_TIMEOUT_INTERVAL;
+				continue;
+			}
+			curBytePtr = &(bytePtr[curByte]);
+			rc = tpm_i2c_request_send(tpm_device->bus_id,
+						  tpm_device->xscom_base,
+						  SMBUS_WRITE,
+						  NUVOTON_TPM_DATA_FIFO_W, 1,
+						  curBytePtr, 1);
+			DBG("%s write FIFO sent last byte, delay=%d/%d,"
+			    " rc=%d\n",
+			    (rc) ? "!!!!" : "----", delay,
+			    TCG_PTP_TIMEOUT_D, rc);
+			break;
+		} while (delay < TCG_PTP_TIMEOUT_D);
+	}
+
+	if (delay >= TCG_PTP_TIMEOUT_D) {
+		/**
+		 * @fwts-label TPMWriteBurstcountBitTimeout
+		 * @fwts-advice The burstcount bit of the tpm status register is
+		 * taking longer to be settled. Either the wait time need to be
+		 * increased or the TPM device is not functional.
+		 */
+		prlog(PR_ERR, "TPM: write FIFO, burstcount polling timeout."
+		      " delay=%d/%d\n", delay, TCG_PTP_TIMEOUT_D);
+		return STB_TPM_TIMEOUT;
+	}
+	if (rc == 0) {
+		if (tpm_is_expecting(&rc)) {
+			 /**
+			 * @fwts-label TPMWriteFifoOverflow2
+			 * @fwts-advice The write to the TPM FIFO overflowed.
+			 * It is expecting more data even though we think we
+			 * are done. This indicates a bug in the TPM device
+			 * driver.
+			 */
+			prlog(PR_ERR, "TPM: write FIFO overflow2\n");
+			return STB_TPM_OVERFLOW;
+		}
+	}
+	return rc;
+}
+
+static int tpm_read_fifo(uint8_t* buf, size_t* buflen)
+{
+	int rc;
+	uint8_t burst_count;
+	int delay = 0;
+	size_t curByte = 0;
+	uint8_t* bytePtr = (uint8_t*)buf;
+	uint8_t* curBytePtr = NULL;
+
+	rc = tpm_poll_for_data_avail();
+
+	if (rc == 0) {
+		do {
+			rc = tpm_read_burst_count(&burst_count);
+			if (rc < 0) {
+				break;
+			} else if (burst_count == 0) {
+				/* Need to delay to allow the TPM time */
+				time_wait_ms(TPM_TIMEOUT_INTERVAL);
+				delay += TPM_TIMEOUT_INTERVAL;
+				continue;
+			}
+			/* Buffer overflow check */
+			if (curByte + burst_count > *buflen)
+			{
+				 /**
+				 * @fwts-label TPMReadFifoOverflow1
+				 * @fwts-advice The read from TPM FIFO overflowed. It is
+				 * expecting more data even though we think we are done.
+				 * This indicates a bug in the TPM device driver.
+				 */
+				prlog(PR_ERR, "TPM: read FIFO overflow1. delay %d/%d\n",
+				      delay, TCG_PTP_TIMEOUT_D);
+				rc = STB_TPM_OVERFLOW;
+			}
+			/*
+			 *  Read some data
+			 */
+			curBytePtr = &(bytePtr[curByte]);
+			rc = tpm_i2c_request_send(tpm_device->bus_id,
+						  tpm_device->xscom_base,
+						  SMBUS_READ,
+						  NUVOTON_TPM_DATA_FIFO_R, 1,
+						  curBytePtr, burst_count);
+			curByte += burst_count;
+			DBG("%s read FIFO. received %zd bytes. burstcount"
+			    " polling delay=%d/%d, rc=%d\n",
+			    (rc) ? "!!!!" : "----", curByte, delay,
+			    TCG_PTP_TIMEOUT_D, rc);
+			delay = 0;
+			if (rc < 0)
+				break;
+			if (!tpm_is_data_avail(&rc))
+				break;
+		} while (delay < TCG_PTP_TIMEOUT_D);
+	}
+
+	if (rc == 0 && delay >= TCG_PTP_TIMEOUT_D) {
+		/**
+		 * @fwts-label TPMReadBurstcountBitTimeout
+		 * @fwts-advice The burstcount bit of the tpm status register is
+		 * taking longer to be settled. Either the wait time needs to be
+		 * increased or the TPM device is not functional.
+		 */
+		prlog(PR_ERR, "TPM: read FIFO, burstcount polling timeout."
+			  " delay=%d/%d\n",
+			  delay, TCG_PTP_TIMEOUT_D);
+		return STB_TPM_TIMEOUT;
+	}
+	if (rc == 0)
+		*buflen = curByte;
+	else
+		*buflen = 0;
+	return rc;
+}
+
+static int tpm_transmit(struct tpm_dev *dev, uint8_t* buf, size_t cmdlen,
+			size_t* buflen)
+{
+	int rc = 0;
+	if (!dev) {
+		/**
+		 * @fwts-label TPMDeviceNotInitialized
+		 * @fwts-advice TPM device is not initialized. This indicates a
+		 * bug in the tpm_transmit() caller
+		 */
+		prlog(PR_ERR, "TPM: tpm device not initialized\n");
+		return STB_ARG_ERROR;
+	}
+	tpm_device = dev;
+	DBG("**** %s: dev %#x/%#x buf %016llx cmdlen %zu"
+	    " buflen %zu ****\n",
+	    __func__, dev->bus_id, dev->xscom_base, *(uint64_t*) buf,
+	    cmdlen, *buflen);
+
+	DBG("step 1/5: check command ready\n");
+	if (!tpm_is_command_ready(&rc)) {
+		if (rc < 0)
+			goto out;
+		rc = tpm_poll_for_command_ready();
+		if (rc < 0)
+			goto out;
+	}
+
+	DBG("step 2/5: write FIFO\n");
+	rc = tpm_write_fifo(buf, cmdlen);
+	if (rc < 0)
+		goto out;
+
+	DBG("step 3/5: write tpmgo\n");
+	rc = tpm_status_write_byte(TCG_PTP_STS_GO);
+	if (rc < 0)
+		goto out;
+
+	DBG("step 4/5: read FIFO\n");
+	rc = tpm_read_fifo(buf, buflen);
+	if (rc < 0)
+		goto out;
+
+	DBG("step 5/5: write command ready\n");
+	rc = tpm_status_write_byte(TCG_PTP_STS_COMMAND_READY);
+
+out:
+	DBG("**** tpm_transmit %s, rc=%d ****\n",
+	    (rc) ? "ERROR" : "SUCCESS", rc);
+	return rc;
+}
+
+static struct tpm_driver tpm_i2c_nuvoton_driver = {
+	.name     = DRIVER_NAME,
+	.transmit = tpm_transmit,
+};
+
+void tpm_i2c_nuvoton_probe(void)
+{
+	struct tpm_dev *tpm_device = NULL;
+	struct dt_node *node = NULL;
+
+	dt_for_each_compatible(dt_root, node, "nuvoton,npct650") {
+		if (!dt_node_is_enabled(node))
+			continue;
+		tpm_device = (struct tpm_dev*) malloc(sizeof(struct tpm_dev));
+		assert(tpm_device);
+		/*
+		 * Read TPM device address and bus id. Make sure the properties
+		 * really exist if the default value is returned.
+		 */
+		tpm_device->xscom_base = dt_prop_get_u32_def(node, "reg", 0);
+		if (!tpm_device->xscom_base &&
+		    !dt_find_property(node, "reg")) {
+			/*
+			 * @fwts-label NuvotonRegNotFound
+			 * @fwts-advice reg property not found. This indicates
+			 * a Hostboot bug if the property really doesn't exist
+			 * in the tpm node.
+			 */
+			prlog(PR_ERR, "NUVOTON: reg property not found, "
+			      "tpm node %p\n", node);
+			goto disable;
+		}
+		tpm_device->bus_id = dt_prop_get_u32_def(node->parent,
+							 "ibm,opal-id", 0);
+		if (!tpm_device->bus_id &&
+		    !dt_find_property(node->parent, "ibm,opal-id")) {
+			/*
+			 * @fwts-label NuvotonIbmOpalIdNotFound
+			 * @fwts-advice ibm,opal-id property not found. This
+			 * indicates a Hostboot bug if the property really
+			 * doesn't exist in the tpm node.
+			 */
+			prlog(PR_ERR, "NUVOTON: ibm,opal-id property not "
+			      "found, tpm node parent %p\n", node->parent);
+			goto disable;
+		}
+		if (tpm_register_chip(node, tpm_device,
+				      &tpm_i2c_nuvoton_driver))
+			free(tpm_device);
+	}
+	return;
+disable:
+	dt_add_property_string(node, "status", "disabled");
+	prlog(PR_NOTICE, "TPM: tpm node %p disabled\n", node);
+	free(tpm_device);
+}

libstb/drivers/Makefile.inc
@@ -4,7 +4,7 @@ DRIVERS_DIR = libstb/drivers
 
 SUBDIRS += $(DRIVERS_DIR)
 
-DRIVERS_SRCS = romcode.c tpm_i2c_interface.c
+DRIVERS_SRCS = romcode.c tpm_i2c_interface.c tpm_i2c_nuvoton.c
 DRIVERS_OBJS = $(DRIVERS_SRCS:%.c=%.o)
 DRIVERS = $(DRIVERS_DIR)/built-in.o
 

libstb/drivers/tpm_i2c_nuvoton.h
@@ -14,19 +14,9 @@
  * limitations under the License.
  */
 
-#ifndef __STB_STATUS_CODES_H
-#define __STB_STATUS_CODES_H
+#ifndef __TPM_I2C_NUVOTON_H
+#define __TPM_I2C_NUVOTON_H
 
-/*  general return codes */
-#define STB_ERROR		-1
-#define STB_ARG_ERROR		-2
-#define STB_DRIVER_ERROR	-3
+extern void tpm_i2c_nuvoton_probe(void);
 
-/* secure boot */
-#define STB_VERIFY_FAILED  		-100
-
-/* TPM */
-#define STB_TPM_OVERFLOW	-300
-#define STB_TPM_TIMEOUT	-301
-
-#endif /* __STB_STATUS_CODES_H */
+#endif /* __TPM_I2C_NUVOTON_H */

libstb/tpm_chip.c
@@ -21,6 +21,7 @@
 #include "status_codes.h"
 #include "container.h"
 #include "tpm_chip.h"
+#include "drivers/tpm_i2c_nuvoton.h"
 
 static struct list_head tpm_list = LIST_HEAD_INIT(tpm_list);
 
@@ -78,6 +79,7 @@ void tpm_init(void)
 	list_head_init(&tpm_list);
 
 	/* tpm drivers supported */
+	tpm_i2c_nuvoton_probe();
 
 	if (list_empty(&tpm_list))
 		/**

commit 454d0be05bcd1de6ea263c9b625a63a57a6098fc

tpm_i2c_nuvoton: handle errors after checking the tpm fifo status
The functions tpm_is_expecting() and tpm_is_data_avail() ignore the
errors returned by the tpm-i2c interface.

This adds code to handle erros after checking the tpm fifo status. The
tpm_is_expecting() and tpm_is_data_avail() functions are replaced by
tpm_wait_for_fifo_status().

Fixes: 56ad053c3e8bf0764ad5878cb018f00a389d30cf
Signed-off-by: Claudio Carvalho <cclaudio@linux.vnet.ibm.com>
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

libstb/drivers/tpm_i2c_nuvoton.c
@@ -77,30 +77,6 @@ static bool tpm_check_status(uint8_t status, uint8_t mask, uint8_t expected)
 	return ((status & mask) == expected);
 }
 
-static int tpm_read_sts_reg_valid(uint8_t* value)
-{
-	int polls, rc;
-
-	for(polls=0; polls<=MAX_STSVALID_POLLS; polls++) {
-		rc = tpm_status_read_byte(TPM_STS, value);
-		if (rc < 0)
-			return rc;
-		if (tpm_check_status(*value, TPM_STS_VALID, TPM_STS_VALID))
-			return 0;
-		/* Wait TPM STS register be settled */
-		time_wait_ms(5);
-	}
-	value = 0;
-	/**
-	 * @fwts-label TPMValidBitTimeout
-	 * @fwts-advice The valid bit of the tpm status register is taking
-	 * longer to be settled. Either the wait time needs to be increased
-	 * or the TPM device is not functional.
-	 */
-	prlog(PR_ERR, "TPM: valid bit not settled. Timeout.\n");
-	return STB_TPM_TIMEOUT;
-}
-
 static int tpm_wait_for_command_ready(void)
 {
 	int rc, delay;
@@ -168,39 +144,59 @@ static int tpm_set_command_ready(void)
 	return STB_TPM_TIMEOUT;
 }
 
-static bool tpm_is_expecting(int* rc)
+static int tpm_wait_for_fifo_status(uint8_t mask, uint8_t expected)
 {
-	uint8_t value = 0;
-	*rc = tpm_read_sts_reg_valid(&value);
-	if (*rc < 0)
-		return false;
-	if (tpm_check_status(value, TPM_STS_EXPECT, TPM_STS_EXPECT))
-		return true;
-	return false;
+	int retries, rc;
+	uint8_t status;
+
+	for(retries = 0; retries <= MAX_STSVALID_POLLS; retries++) {
+		rc = tpm_status_read_byte(TPM_STS, &status);
+		if (rc < 0) {
+			/**
+			 * @fwts-label TPMReadFifoStatus
+			 * @fwts-advice Either the tpm device or the tpm-i2c
+			 * interface doesn't seem to be working properly. Check
+			 * the return code (rc) for further details.
+			 */
+			prlog(PR_ERR, "NUVOTON: fail to read fifo status: "
+			      "mask %x, expected %x, rc=%d\n", mask, expected,
+			      rc);
+			return STB_DRIVER_ERROR;
+		}
+		if (tpm_check_status(status, mask, expected))
+			return 0;
+		/* Wait TPM STS register be settled */
+		time_wait_ms(5);
+	}
+	return STB_TPM_TIMEOUT;
 }
 
-static bool tpm_is_data_avail(int* rc)
-{
-	uint8_t value = 0;
-
-	*rc = tpm_read_sts_reg_valid(&value);
-	if (*rc < 0)
-		return false;
-	if (tpm_check_status(value, TPM_STS_DATA_AVAIL, TPM_STS_DATA_AVAIL))
-		return true;
-	return false;
-}
-
-static int tpm_poll_for_data_avail(void)
+static int tpm_wait_for_data_avail(void)
 {
 	int delay, rc;
+	uint8_t status;
 
 	for (delay = 0; delay < TPM_TIMEOUT_A;
 	     delay += TPM_TIMEOUT_INTERVAL) {
-		if (tpm_is_data_avail(&rc)) {
+
+		rc = tpm_status_read_byte(TPM_STS, &status);
+		if (rc < 0) {
+			/**
+			 * @fwts-label TPMReadDataAvail
+			 * @fwts-advice Either the tpm device or the tpm-i2c
+			 * interface doesn't seem to be working properly. Check
+			 * the return code (rc) for further details.
+			 */
+			prlog(PR_ERR, "NUVOTON: fail to read sts.dataAvail, "
+			      "rc=%d\n", rc);
+			return STB_DRIVER_ERROR;
+		}
+		if (tpm_check_status(status,
+				     TPM_STS_VALID | TPM_STS_DATA_AVAIL,
+				     TPM_STS_VALID | TPM_STS_DATA_AVAIL)) {
 			DBG("---- read FIFO. Data available. delay=%d/%d\n",
 			    delay, TPM_TIMEOUT_A);
-			return rc;
+			return 0;
 		}
 		time_wait_ms(TPM_TIMEOUT_INTERVAL);
 	}
@@ -210,7 +206,7 @@ static int tpm_poll_for_data_avail(void)
 	 * longer to be settled. Either the wait time need to be increased or
 	 * the TPM device is not functional.
 	 */
-	prlog(PR_ERR, "TPM: read FIFO. Polling timeout, delay=%d/%d\n",
+	prlog(PR_ERR, "NUVOTON: timeout on sts.dataAvail, delay=%d/%d\n",
 	      delay, TPM_TIMEOUT_A);
 	return STB_TPM_TIMEOUT;
 }
@@ -291,14 +287,19 @@ static int tpm_write_fifo(uint8_t* buf, size_t buflen)
 		if (rc < 0)
 			return rc;
 
-		if (!tpm_is_expecting(&rc)) {
+		rc = tpm_wait_for_fifo_status(TPM_STS_VALID | TPM_STS_EXPECT,
+					      TPM_STS_VALID | TPM_STS_EXPECT);
+		if (rc == STB_DRIVER_ERROR)
+			return rc;
+		if (rc == STB_TPM_TIMEOUT) {
 			/**
-			 * @fwts-label TPMWriteFifoOverflow1
+			 * @fwts-label TPMWriteFifoNotExpecting
 			 * @fwts-advice The write to the TPM FIFO overflowed,
-			 * the TPM is not expecting more data. This indicates a bug
-			 * in the TPM device driver.
+			 * the TPM is not expecting more data. This indicates a
+			 * bug in the TPM device driver.
 			 */
-			prlog(PR_ERR, "TPM: write FIFO overflow1\n");
+			prlog(PR_ERR, "NUVOTON: write FIFO overflow, not expecting "
+			      "more data\n");
 			return STB_TPM_OVERFLOW;
 		}
 	} while (curByte < length);
@@ -318,20 +319,34 @@ static int tpm_write_fifo(uint8_t* buf, size_t buflen)
 	DBG("%s write FIFO sent last byte, rc=%d\n",
 	    (rc) ? "!!!!" : "----", TPM_TIMEOUT_D, rc);
 
-	if (rc == 0) {
-		if (tpm_is_expecting(&rc)) {
-			 /**
-			 * @fwts-label TPMWriteFifoOverflow2
-			 * @fwts-advice The write to the TPM FIFO overflowed.
-			 * It is expecting more data even though we think we
-			 * are done. This indicates a bug in the TPM device
-			 * driver.
-			 */
-			prlog(PR_ERR, "TPM: write FIFO overflow2\n");
-			return STB_TPM_OVERFLOW;
-		}
+	if (rc < 0) {
+		/**
+		 * @fwts-label TPMWriteFifo
+		 * @fwts-advice The tpm-i2c interface doesn't seem to be
+		 * working properly. Check the return code (rc) for
+		 * further details.
+		 */
+		prlog(PR_ERR, "NUVOTON: fail to write fifo (last byte), "
+		      "curByte=%zd, rc=%d\n", curByte, rc);
+		return STB_DRIVER_ERROR;
 	}
-	return rc;
+	rc = tpm_wait_for_fifo_status(TPM_STS_VALID | TPM_STS_EXPECT,
+				      TPM_STS_VALID | TPM_STS_EXPECT);
+	if (rc == STB_DRIVER_ERROR)
+		return rc;
+	if (rc == 0) {
+		 /**
+		 * @fwts-label TPMWriteFifoExpecting
+		 * @fwts-advice The write to the TPM FIFO overflowed.
+		 * It is expecting more data even though we think we
+		 * are done. This indicates a bug in the TPM device
+		 * driver.
+		 */
+		prlog(PR_ERR, "TPM: write FIFO overflow, expecting "
+		      "more data\n");
+		return STB_TPM_OVERFLOW;
+	}
+	return 0;
 }
 
 static int tpm_read_fifo(uint8_t* buf, size_t* buflen)
@@ -341,7 +356,7 @@ static int tpm_read_fifo(uint8_t* buf, size_t* buflen)
 	uint8_t* bytePtr = (uint8_t*)buf;
 	uint8_t* curBytePtr = NULL;
 
-	rc = tpm_poll_for_data_avail();
+	rc = tpm_wait_for_data_avail();
 	if (rc < 0)
 		goto error;
 
@@ -377,7 +392,12 @@ static int tpm_read_fifo(uint8_t* buf, size_t* buflen)
 		    (rc) ? "!!!!" : "----", curByte, rc);
 		if (rc < 0)
 			goto error;
-	} while (tpm_is_data_avail(&rc));
+		rc = tpm_wait_for_fifo_status(
+					  TPM_STS_VALID | TPM_STS_DATA_AVAIL,
+					  TPM_STS_VALID | TPM_STS_DATA_AVAIL);
+		if (rc == STB_DRIVER_ERROR)
+			goto error;
+	} while (rc == 0);
 
 	*buflen = curByte;
 	return 0;