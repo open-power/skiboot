8b00ce79a32f9a7dedd392f8a64a672fcbd8d1e0
Oliver O'Halloran <oohall@gmail.com>
2016-12-21 15:52:26 +1100

    mambo: move callouts into separate header file
    This just moves the mambo callout handlers into a private header file so
    we don't have to continue junking up the main platform file.
    
    Signed-off-by: Oliver O'Halloran <oohall@gmail.com>
    Reviewed-by: Andrew Donnellan <andrew.donnellan@au1.ibm.com>
    Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

--- platforms/mambo/mambo.c
+++ platforms/mambo/mambo.c
@@ -24,41 +24,14 @@
 #include <time-utils.h>
 #include <time.h>
 
+#include "mambo.h"
+
 static bool mambo_probe(void)
 {
 	if (!dt_find_by_path(dt_root, "/mambo"))
 		return false;
 
 	return true;
-}
-
-static inline unsigned long callthru0(int command)
-{
-	register uint64_t c asm("r3") = command;
-	asm volatile (".long 0x000eaeb0":"=r" (c):"r"(c));
-	return (c);
-}
-
-static inline unsigned long callthru2(int command, unsigned long arg1,
-				      unsigned long arg2)
-{
-	register unsigned long c asm("r3") = command;
-	register unsigned long a1 asm("r4") = arg1;
-	register unsigned long a2 asm("r5") = arg2;
-	asm volatile (".long 0x000eaeb0":"=r" (c):"r"(c), "r"(a1), "r"(a2));
-	return (c);
-}
-
-static inline unsigned long callthru3(int command, unsigned long arg1,
-				      unsigned long arg2, unsigned long arg3)
-{
-	register unsigned long c asm("r3") = command;
-	register unsigned long a1 asm("r4") = arg1;
-	register unsigned long a2 asm("r5") = arg2;
-	register unsigned long a3 asm("r6") = arg3;
-	asm volatile (".long 0x000eaeb0":"=r" (c):"r"(c), "r"(a1), "r"(a2),
-		      "r"(a3));
-	return (c);
 }
 
 #define BD_INFO_SYNC		0
@@ -68,16 +41,6 @@ static inline unsigned long callthru3(int command, unsigned long arg1,
 #define BD_INFO_CHANGE		4
 
 #define BD_SECT_SZ		512
-
-/* Mambo callthru commands */
-#define SIM_WRITE_CONSOLE_CODE	0
-#define SIM_EXIT_CODE		31
-#define SIM_READ_CONSOLE_CODE	60
-#define SIM_GET_TIME_CODE	70
-#define SIM_CALL_TCL		86
-#define SIM_BOGUS_DISK_READ	116
-#define SIM_BOGUS_DISK_WRITE	117
-#define SIM_BOGUS_DISK_INFO	118
 
 static inline int callthru_disk_read(int id, void *buf, unsigned long sect,
 				     unsigned long nrsect)
