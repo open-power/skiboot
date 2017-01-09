293ca03683bfcd2c787b7e6f7c760238ba8eeae3
Oliver O'Halloran <oohall@gmail.com>
2017-01-10 10:26:24 +1100

    init: print the FDT blob size in decimal
    Changes the skiboot output so that rather than printing the FDT address
    and size in hex it prints the address in hex and the size in decimal.
    Doing this lets you read the FDT blob with getmemproc and some
    copy+paste.
    
    Signed-off-by: Oliver O'Halloran <oohall@gmail.com>
    Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

--- core/init.c
+++ core/init.c
@@ -541,7 +541,7 @@ void __noreturn load_and_boot_kernel(bool is_reboot)
 	printf("INIT: stdout-path: %s\n", stdoutp ? stdoutp : "");
 
 
-	printf("INIT: Starting kernel at 0x%llx, fdt at %p (size 0x%x)\n",
+	printf("INIT: Starting kernel at 0x%llx, fdt at %p %u bytes)\n",
 	       kernel_entry, fdt, fdt_totalsize(fdt));
 
 	debug_descriptor.state_flags |= OPAL_BOOT_COMPLETE;
