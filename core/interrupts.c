open-power/skiboot

commit 539487bae4b77d66b5966f5396609f28312bd6ae

Always create opal-interrupts property
Even if empty. The existing Linux code doesn't properly NULL check
the result of of_get_property() and will allocate an array of an
undefined size if the property is missing, while an empty property
is harmless.

Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

core/interrupts.c
@@ -186,10 +186,12 @@ void add_opal_interrupts(void)
 	unlock(&irq_lock);
 
 	/* The opal-interrupts property has one cell per interrupt,
-	 * it is not a standard interrupt property
+	 * it is not a standard interrupt property.
+	 *
+	 * Note: Even if empty, create it, otherwise some bogus error
+	 * handling in Linux can cause problems.
 	 */
-	if (irqs)
-		dt_add_property(opal_node, "opal-interrupts", irqs, count * 4);
+	dt_add_property(opal_node, "opal-interrupts", irqs, count * 4);
 }
 
 /*

commit 3b38c498dba1bf1248f79c55d2bcf91c8c2826b7

core/interrupts: free interrupt names after adding to DT
Fixes: 539487bae4b77
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

core/interrupts.c
@@ -244,6 +244,7 @@ void add_opal_interrupts(void)
 	dt_add_property(opal_node, "opal-interrupts-names", names, tns);
 
 	free(irqs);
+	free(names);
 }
 
 /*