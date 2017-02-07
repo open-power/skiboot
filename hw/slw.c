open-power/skiboot

commit c3ea33f173ec3dc021d84372b50f1c13dd4b07ed

hw/slw: only enable supported STOP states
This patch adds parsing the ibm,enabled-stop-levels field when building
the /ibm,opal/power-mgt/ node so that only the STOP levels that have
been advertised by firmware are used.

Signed-off-by: Oliver O'Halloran <oohall@gmail.com>
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

hw/slw.c
@@ -637,6 +637,7 @@ void add_cpu_idle_state_properties(void)
 	u64 *pm_ctrl_reg_val_buf;
 	u64 *pm_ctrl_reg_mask_buf;
 	u32 supported_states_mask;
+	u32 stop_levels;
 
 	/* Variables to track buffer length */
 	u8 name_buf_len;
@@ -676,6 +677,12 @@ void add_cpu_idle_state_properties(void)
 		states = power9_cpu_idle_states;
 		nr_states = ARRAY_SIZE(power9_cpu_idle_states);
 		has_stop_inst = true;
+
+		stop_levels = dt_prop_get_u32_def(power_mgt,
+			"ibm,enabled-stop-levels", 0);
+		if (!stop_levels)
+			prerror("SLW: No stop levels available. Power saving is disabled!\n");
+
 	} else if (chip->type == PROC_CHIP_P8_MURANO ||
 	    chip->type == PROC_CHIP_P8_VENICE ||
 	    chip->type == PROC_CHIP_P8_NAPLES) {
@@ -753,6 +760,13 @@ void add_cpu_idle_state_properties(void)
 		/* For each state, check if it is one of the supported states. */
 		if (!(states[i].flags & supported_states_mask))
 			continue;
+
+		/* We can only use the stop levels that HB has made available */
+		if (has_stop_inst) {
+			u32 level = 1ul << states[i].pm_ctrl_reg_val;
+			if (!(stop_levels & level))
+				continue;
+		}
 
 		/*
 		 * If a state is supported add each of its property

commit 0500d504b13eb95046bcea9b17ad6787ff18e843

slw: Fix parsing of supported STOP states
Bits[0-15] are used to indicate the supported STOP states in
"ibm,enabled-stop-levels" DT property and STOP0 is starting from
Bit0. This patch fixes the parsing to read the appropriate bits.

Fixes: c3ea33f173ec ("hw/slw: only enable supported STOP states")
Signed-off-by: Shilpasri G Bhat <shilpa.bhat@linux.vnet.ibm.com>
Reviewed-by: Oliver O'Halloran <oohall@gmail.com>
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

hw/slw.c
@@ -763,8 +763,10 @@ void add_cpu_idle_state_properties(void)
 
 		/* We can only use the stop levels that HB has made available */
 		if (has_stop_inst) {
-			u32 level = 1ul << states[i].pm_ctrl_reg_val;
-			if (!(stop_levels & level))
+			u32 level = 31 - (states[i].pm_ctrl_reg_val &
+					 OPAL_PM_PSSCR_RL_MASK);
+
+			if (!(stop_levels & (1ul << level)))
 				continue;
 		}
 