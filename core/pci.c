open-power/skiboot

commit 41b14f9452c25f3dd74a8304763a3cc58bff019f

pci: Wait 20ms before checking presence detect on PCIe
As the PHB presence logic has a debounce timer that can take
a while to settle.

Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

core/pci.c
@@ -1641,6 +1641,11 @@ void pci_init_slots(void)
 {
 	unsigned int i;
 
+	/* Some PHBs may need that long to debounce the presence detect
+	 * after HW initialization.
+	 */
+	time_wait_ms(20);
+
 	prlog(PR_NOTICE, "PCI: Resetting PHBs...\n");
 	pci_do_jobs(pci_reset_phb);
 

commit 9253de5bcab2800c1ac1f5372ba5354543fa2e67

PCI: only wait 20ms for PHB logic to settle if we detected any PHBs
Otherwise, in simulators such as Mambo, we needlessly wait 20ms, which
takes much longer than 20ms to do, because simulator.

Fixes: 41b14f9452c25f3dd74a8304763a3cc58bff019f
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

core/pci.c
@@ -1644,7 +1644,12 @@ void pci_init_slots(void)
 	/* Some PHBs may need that long to debounce the presence detect
 	 * after HW initialization.
 	 */
-	time_wait_ms(20);
+	for (i = 0; i < ARRAY_SIZE(phbs); i++) {
+		if (phbs[i]) {
+			time_wait_ms(20);
+			break;
+		}
+	}
 
 	prlog(PR_NOTICE, "PCI: Resetting PHBs...\n");
 	pci_do_jobs(pci_reset_phb);