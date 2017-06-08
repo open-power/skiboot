open-power/skiboot

commit 52aed80bddd5eed94c537f2bb0b846e4b5683728

mambo: Add a reservation for the initramfs
On most systems the initramfs is loaded inside the part of memory
reserved for the OS [0x0-0x30000000] and skiboot will never touch it.
On mambo it's loaded at 0x80000000 and if you're unlucky skiboot can
allocate over the top of it and corrupt the initramfs blob.

There might be the downside that the kernel cannot re-use the initramfs
memory since it's marked as reserved, but the kernel might also free it
anyway.

Fixes: 65612f120735
Signed-off-by: Oliver O'Halloran <oohall@gmail.com>
Tested-by: Michael Ellerman <mpe@ellerman.id.au>
[stewart@linux.vnet.ibm.com: add Fixes]
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

external/mambo/skiboot.tcl
@@ -222,6 +222,11 @@ mysim of addprop $reserved_memory int "#size-cells" 2
 mysim of addprop $reserved_memory int "#address-cells" 2
 mysim of addprop $reserved_memory empty "ranges" ""
 
+set initramfs_res [mysim of addchild $reserved_memory "initramfs" ""]
+set reg [list $cpio_start $cpio_size ]
+mysim of addprop $initramfs_res array64 "reg" reg
+mysim of addprop $initramfs_res empty "name" "initramfs"
+
 set fake_nvram_node [mysim of addchild $reserved_memory "ibm,fake-nvram" ""]
 set reg [list $fake_nvram_start $fake_nvram_size ]
 mysim of addprop $fake_nvram_node array64 "reg" reg

commit 9e888546f1ec322ac74b1e58b670d294ae3ea4fc

mambo: fix cpio/initramfs reservation
We didn't init cpio_size in the no cpio case.

Fixes: 52aed80bddd5eed94c537f2bb0b846e4b5683728
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

external/mambo/skiboot.tcl
@@ -189,6 +189,7 @@ mysim of addprop $xscom_node byte_array "compatible" $compat
 # Load any initramfs
 set cpio_start 0x80000000
 set cpio_end $cpio_start
+set cpio_size 0
 if { [info exists env(SKIBOOT_INITRD)] } {
 
     set cpios [split $env(SKIBOOT_INITRD) ","]