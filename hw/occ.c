open-power/skiboot

commit 3c64f79ad2220d03a0af49d44f0548e08da54a44

platform/zz: Acknowledge OCC_LOAD mbox message in ZZ
In P9 FSP box, OCC image is pre-loaded. So do not handle the load
command and send SUCCESS to FSP on recieving OCC_LOAD mbox message.

Signed-off-by: Shilpasri G Bhat <shilpa.bhat@linux.vnet.ibm.com>
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

hw/occ.c
@@ -1082,6 +1082,19 @@ static void occ_do_load(u8 scope, u32 dbob_id __unused, u32 seq_id)
 	if (err)
 		return;
 
+	if (proc_gen == proc_gen_p9) {
+		/* OCC is pre-loaded in P9, so send SUCCESS to FSP */
+		rsp = fsp_mkmsg(FSP_CMD_LOAD_OCC_STAT, 2, 0, seq_id);
+		if (rsp)
+			rc = fsp_queue_msg(rsp, fsp_freemsg);
+		if (rc) {
+			log_simple_error(&e_info(OPAL_RC_OCC_LOAD),
+				"OCC: Error %d queueing FSP OCC LOAD STATUS msg", rc);
+			fsp_freemsg(rsp);
+		}
+		return;
+	}
+
 	/*
 	 * Check if hostservices lid caching is complete. If not, queue
 	 * the load request.
@@ -1413,8 +1426,8 @@ void occ_p9_interrupt(uint32_t chip_id)
 
 void occ_fsp_init(void)
 {
-	/* OCC is P8 only */
-	if (proc_gen != proc_gen_p8)
+	/* OCC is  supported in P8 and P9 */
+	if (proc_gen < proc_gen_p8)
 		return;
 
 	/* If we have an FSP, register for notifications */

platforms/ibm-fsp/zz.c
@@ -44,10 +44,16 @@ static uint32_t ibm_fsp_occ_timeout(void)
 	return 60;
 }
 
+static void zz_init(void)
+{
+	hservices_init();
+	ibm_fsp_init();
+}
+
 DECLARE_PLATFORM(zz) = {
 	.name			= "ZZ",
 	.probe			= zz_probe,
-	.init			= ibm_fsp_init,
+	.init			= zz_init,
 	.exit			= ibm_fsp_exit,
 	.cec_power_down		= ibm_fsp_cec_power_down,
 	.cec_reboot		= ibm_fsp_cec_reboot,

commit 41ba89da0460ad792ee67986008a4e021599efa6

occ: Set return variable to correct value
When entering this section of code rc will be zero. If fsp_mkmsg() fails
the code responsible for printing an error message won't be set.
Resetting rc should allow for the error case to trigger if fsp_mkmsg
fails.

Fixes: 3c64f79ad2220d03a0af49d44f0548e08da54a44
Signed-off-by: Cyril Bur <cyril.bur@au1.ibm.com>
[stewart@linux.vnet.ibm.com: add Fixes]
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

hw/occ.c
@@ -1083,6 +1083,7 @@ static void occ_do_load(u8 scope, u32 dbob_id __unused, u32 seq_id)
 		return;
 
 	if (proc_gen == proc_gen_p9) {
+		rc = -ENOMEM;
 		/* OCC is pre-loaded in P9, so send SUCCESS to FSP */
 		rsp = fsp_mkmsg(FSP_CMD_LOAD_OCC_STAT, 2, 0, seq_id);
 		if (rsp)