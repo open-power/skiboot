0c301f6ff80512aa95616c947626762e346a363f
Stewart Smith <stewart@linux.vnet.ibm.com>
2017-07-12 17:57:03 +1000

    opal-ci/fetch-debian-jessie-installer: follow redirects
    Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

--- opal-ci/fetch-debian-jessie-installer.sh
+++ opal-ci/fetch-debian-jessie-installer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl http://ftp.debian.org/debian/dists/jessie/main/installer-ppc64el/current/images/netboot/debian-installer/ppc64el/vmlinux -o debian-jessie-vmlinux
-curl http://ftp.debian.org/debian/dists/jessie/main/installer-ppc64el/current/images/netboot/debian-installer/ppc64el/initrd.gz -o debian-jessie-initrd.gz
+curl -L http://ftp.debian.org/debian/dists/jessie/main/installer-ppc64el/current/images/netboot/debian-installer/ppc64el/vmlinux -o debian-jessie-vmlinux
+curl -L http://ftp.debian.org/debian/dists/jessie/main/installer-ppc64el/current/images/netboot/debian-installer/ppc64el/initrd.gz -o debian-jessie-initrd.gz
