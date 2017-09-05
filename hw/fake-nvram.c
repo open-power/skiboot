fbdb11fb04345896e208ccf001689360c202ba89
Joel Stanley <joel@jms.id.au>
2017-09-05 15:51:48 +1000

    hw/fake-nvram: Fix space between if and open paren
    Signed-off-by: Joel Stanley <joel@jms.id.au>
    Reviewed-by: Michael Neuling <mikey@neuling.org>
    Reviewed-by: Russell Currey <ruscur@russell.cc>
    Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

--- hw/fake-nvram.c
+++ hw/fake-nvram.c
@@ -26,7 +26,7 @@ int fake_nvram_info(uint32_t *total_size)
 {
 	nvram_region = find_mem_region("ibm,fake-nvram");
 
-	if(!nvram_region)
+	if (!nvram_region)
 		return OPAL_HARDWARE;
 
 	*total_size = nvram_region->len;
@@ -36,7 +36,7 @@ int fake_nvram_info(uint32_t *total_size)
 
 int fake_nvram_start_read(void *dst, uint32_t src, uint32_t len)
 {
-	if(!nvram_region)
+	if (!nvram_region)
 		return -ENODEV;
 
 	lock(&fake_nvram_lock);
@@ -50,7 +50,7 @@ int fake_nvram_start_read(void *dst, uint32_t src, uint32_t len)
 
 int fake_nvram_write(uint32_t offset, void *src, uint32_t size)
 {
-	if(!nvram_region)
+	if (!nvram_region)
 		return OPAL_HARDWARE;
 
 	lock(&fake_nvram_lock);
