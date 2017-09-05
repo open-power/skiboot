open-power/skiboot

commit 694546c1f72bfafca515fe8402bd96d0cf03c333

hdata/vpd: Parse the OpenPOWER OPFR record
Parse the OpenPOWER FRU VPD (OPFR) record on OpenPOWER instead
of the VINI records.

TODO: Add the dts and updates for make check for P9 OPFR systems once
the HDAT in those are complete.

Signed-off-by: Ananth N Mavinakayanahalli <ananth@linux.vnet.ibm.com>
Reviewed-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

hdata/vpd.c
@@ -226,6 +226,45 @@ static const struct card_info *card_info_lookup(char *ccin)
 	return NULL;
 }
 
+/*
+ * For OpenPOWER, we only decipher OPFR records. While OP HDAT have VINI
+ * records too, populating the fields in there is optional. Also, there
+ * is an overlap in the fields contained therein.
+ */
+static void vpd_opfr_parse(struct dt_node *node,
+		const void *fruvpd, unsigned int fruvpd_sz)
+{
+	const void *kw;
+	uint8_t sz;
+
+	/* Vendor Name */
+	kw = vpd_find(fruvpd, fruvpd_sz, "OPFR", "VN", &sz);
+	if (kw)
+		dt_add_property_nstr(node, "vendor", kw, sz);
+
+	/* FRU Description */
+	kw = vpd_find(fruvpd, fruvpd_sz, "OPFR", "DR", &sz);
+	if (kw)
+		dt_add_property_nstr(node, "description", kw, sz);
+
+	/* Part number */
+	kw = vpd_find(fruvpd, fruvpd_sz, "OPFR", "VP", &sz);
+	if (kw)
+		dt_add_property_nstr(node, "part-number", kw, sz);
+
+	/* Serial number */
+	kw = vpd_find(fruvpd, fruvpd_sz, "OPFR", "VS", &sz);
+	if (kw)
+		dt_add_property_nstr(node, "serial-number", kw, sz);
+
+	/* Build date in BCD */
+	kw = vpd_find(fruvpd, fruvpd_sz, "OPFR", "MB", &sz);
+	if (kw)
+		dt_add_property_nstr(node, "build-date", kw, sz);
+
+	return;
+}
+
 static void vpd_vini_parse(struct dt_node *node,
 			   const void *fruvpd, unsigned int fruvpd_sz)
 {
@@ -446,7 +485,11 @@ struct dt_node *dt_add_vpd_node(const struct HDIF_common_hdr *hdr,
 	if (vpd_valid(fruvpd, fruvpd_sz)
 	    && !dt_find_property(node, "ibm,vpd")) {
 		dt_add_property(node, "ibm,vpd", fruvpd, fruvpd_sz);
-		vpd_vini_parse(node, fruvpd, fruvpd_sz);
+
+		if (op_platform)
+			vpd_opfr_parse(node, fruvpd, fruvpd_sz);
+		else
+			vpd_vini_parse(node, fruvpd, fruvpd_sz);
 	}
 
 	return node;

commit 2a73bbed50c3daefb88a362f29ef6f415e05043b

hdata: Fix vpd parse
We detect vpd data format in vpd_parse() and set "op_platform" variable.
dt_add_vpd_node() depends on op_platform to select parser type.

There are places where we call dt_add_vpd_node() even before calling
vpd_parse(). And in such cases it will not parse vpd data properly.

This patch fixes above issue.

Fixes: 694546c1 (hdata/vpd: Parse the OpenPOWER OPFR record)
CC: Ananth N Mavinakayanahalli <ananth@linux.vnet.ibm.com>
Signed-off-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>
Acked-by: Ananth N Mavinakayanahalli <ananth@linux.vnet.ibm.com>
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

hdata/vpd.c
@@ -23,8 +23,6 @@
 #include "hdata.h"
 #include <inttypes.h>
 
-static bool op_platform;
-
 struct card_info {
 	const char *ccin; 	/* Customer card identification number */
 	const char *description;
@@ -492,7 +490,7 @@ struct dt_node *dt_add_vpd_node(const struct HDIF_common_hdr *hdr,
 	    && !dt_find_property(node, "ibm,vpd")) {
 		dt_add_property(node, "ibm,vpd", fruvpd, fruvpd_sz);
 
-		if (op_platform)
+		if (vpd_find_record(fruvpd, fruvpd_sz, "OPFR", NULL))
 			vpd_opfr_parse(node, fruvpd, fruvpd_sz);
 		else
 			vpd_vini_parse(node, fruvpd, fruvpd_sz);
@@ -607,10 +605,9 @@ static void sysvpd_parse(void)
 	}
 
 	/* Look for the new OpenPower "OSYS" first */
-	if (vpd_find_record(sysvpd, sysvpd_sz, "OSYS", NULL)) {
-		op_platform = true;
+	if (vpd_find_record(sysvpd, sysvpd_sz, "OSYS", NULL))
 		sysvpd_parse_opp(sysvpd, sysvpd_sz);
-	} else
+	else
 		sysvpd_parse_legacy(sysvpd, sysvpd_sz);
 
 	dt_add_model_name();