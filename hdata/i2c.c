open-power/skiboot

commit 41dc3eb4495c451a405974570f604622a3f829ef

hdata: Check the Host I2C devices array version
Currently this is not populated on FSP machines which causes some
obnoxious errors to appear in the boot log. We also only want to
parse version 1 of this structure since future versions will completely
change the array item format.

Cc: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>
Signed-off-by: Oliver O'Halloran <oohall@gmail.com>
[stewart@linux.vnet.ibm.com: return -1 on error rather than 1]
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

hdata/i2c.c
@@ -149,6 +149,11 @@ static bool is_zeros(const void *p, size_t size)
 	return true;
 }
 
+struct host_i2c_hdr {
+	const struct HDIF_array_hdr hdr;
+	__be32 version;
+};
+
 int parse_i2c_devs(const struct HDIF_common_hdr *hdr, int idata_index,
 	struct dt_node *xscom)
 {
@@ -156,7 +161,9 @@ int parse_i2c_devs(const struct HDIF_common_hdr *hdr, int idata_index,
 	const struct hdat_i2c_type *type;
 	const struct i2c_dev *dev;
 	const char *label, *name, *compat;
+	const struct host_i2c_hdr *ahdr;
 	uint32_t i2c_addr;
+	uint32_t version;
 	uint32_t size;
 	int i, count;
 
@@ -165,6 +172,32 @@ int parse_i2c_devs(const struct HDIF_common_hdr *hdr, int idata_index,
 	 * and will need updating for new processors
 	 */
 	assert(proc_gen == proc_gen_p9);
+
+	/*
+	 * Emit an error if we get a newer version. This is an interim measure
+	 * until the new version format is finalised.
+	 */
+	ahdr = HDIF_get_idata(hdr, idata_index, &size);
+	if (!ahdr || !size)
+		return -1;
+
+	/*
+	 * Some hostboots don't correctly fill the version field. On these
+	 * the offset from the start of the header to the start of the array
+	 * is 16 bytes.
+	 */
+	if (be32_to_cpu(ahdr->hdr.offset) == 16) {
+		version = 1;
+		prerror("I2C: HDAT device array has no version! Assuming v1\n");
+	} else {
+		version = be32_to_cpu(hdr->version);
+	}
+
+	if (version != 1) {
+		prerror("I2C: HDAT version %d not supported! THIS IS A BUG\n",
+			version);
+		return -1;
+	}
 
 	count = HDIF_get_iarray_size(hdr, idata_index);
 	for (i = 0; i < count; i++) {

commit f0379b84b9088796d61287a3ab31bf5a54519df7

hdata/i2c: add __packed to the host_i2c_hdr structure
This adds __packed to the host_i2c_hdr structure since it defines an
offset that refers to the beginning of the structure.

Fixes: 41dc3eb4495c451a405974570f604622a3f829ef
Signed-off-by: Claudio Carvalho <cclaudio@linux.vnet.ibm.com>
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

hdata/i2c.c
@@ -152,7 +152,7 @@ static bool is_zeros(const void *p, size_t size)
 struct host_i2c_hdr {
 	const struct HDIF_array_hdr hdr;
 	__be32 version;
-};
+} __packed;
 
 int parse_i2c_devs(const struct HDIF_common_hdr *hdr, int idata_index,
 	struct dt_node *xscom)