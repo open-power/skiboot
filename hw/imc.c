open-power/skiboot

commit 712837cedca06644d2e8dbb12e1cf535fbbd78ba

skiboot/imc: Update the nest_pmus array with occ/gpe microcode uav updates
OOC/gpe nest microcode maintains the list of individual nest units
supported. Sync the recent updates to the UAV with nest_pmus array.

For reference occ/gpr microcode link for the UAV:
https://github.com/open-power/occ/blob/master/src/occ_gpe1/gpe1_24x7.h

Signed-off-by: Madhavan Srinivasan <maddy@linux.vnet.ibm.com>
[stewart@linux.vnet.ibm.com: add in reference to ucode]
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

hw/imc.c
@@ -60,7 +60,9 @@ char const *nest_pmus[] = {
 	"phb0",
 	"phb1",
 	"phb2",
-	"resvd",
+	"phb3",
+	"phb4",
+	"phb5",
 	"nx",
 	"capp0",
 	"capp1",
@@ -69,13 +71,14 @@ char const *nest_pmus[] = {
 	"alink0",
 	"alink1",
 	"alink2",
+	"alink3",
 	"nvlink0",
 	"nvlink1",
 	"nvlink2",
 	"nvlink3",
 	"nvlink4",
 	"nvlink5",
-	/* reserved bits : 48 - 64 */
+	/* reserved bits : 51 - 63 */
 };
 
 /*

commit a9cf7c586871bdd8187760862c2df8bb2fbb2d6f

hw/imc: Use ARRAY_SIZE instead of static macro
disable_unavailable_units() loops through nest_pmus array
to filter out the unsupported nest units from the imc
catalog dtb. Current code use a static macro ('MAX_NEST_UNITS')
for array limit, instead use ARRAY_SIZE. This will avoid updates
to static macro when updating the nest_pmus array.

Fixes: 712837cedca06 ('skiboot/imc: Update the nest_pmus array with occ/gpe microcode uav updates')
Signed-off-by: Madhavan Srinivasan <maddy@linux.vnet.ibm.com>
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

hw/imc.c
@@ -321,7 +321,7 @@ static void disable_unavailable_units(struct dt_node *dev)
 			avl_vec = (0xffULL) << 56;
 	}
 
-	for (i = 0; i < MAX_NEST_UNITS; i++) {
+	for (i = 0; i < ARRAY_SIZE(nest_pmus); i++) {
 		if (!(PPC_BITMASK(i, i) & avl_vec)) {
 			/* Check if the device node exists */
 			target = dt_find_by_name(dev, nest_pmus[i]);

include/imc.h
@@ -119,8 +119,6 @@ struct imc_chip_cb
 #define NEST_IMC_ENABLE			0x1
 #define NEST_IMC_DISABLE		0x2
 
-#define MAX_NEST_UNITS			48
-
 /*
  * Core IMC SCOMs
  */