open-power/skiboot

commit 85a1de35cbe47618e9d4104880f182121806af4d

fast-boot: occ: Re-parse the pstate table during fast-boot
OCC shares the frequency list to host by copying the pstate table to
main memory in HOMER. This table is parsed during boot to create
device-tree properties for frequency and pstate IDs. OCC can update
the pstate table to present a new set of frequencies to the host. But
host will remain oblivious to these changes unless it is re-inited
with the updated device-tree CPU frequency properties. So this patch
allows to re-parse the pstate table and update the device-tree
properties during fast-reboot.

OCC updates the pstate table when asked to do so using pstate-table
bias command. And this is mainly used by WOF team for
characterization purposes.

Signed-off-by: Shilpasri G Bhat <shilpa.bhat@linux.vnet.ibm.com>
Tested-by: Vaidyanathan Srinivasan <svaidy@linux.vnet.ibm.com>
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

hw/occ.c
@@ -1556,8 +1556,24 @@ void occ_pstates_init(void)
 	if (proc_gen < proc_gen_p8)
 		return;
 	/* Handle fast reboots */
-	if (occ_pstates_initialized)
-		return;
+	if (occ_pstates_initialized) {
+		struct dt_node *power_mgt;
+		int i;
+		const char *props[] = {
+				"ibm,pstate-core-max",
+				"ibm,pstate-frequencies-mhz",
+				"ibm,pstate-ids",
+				"ibm,pstate-max",
+				"ibm,pstate-min",
+				"ibm,pstate-nominal",
+				"ibm,pstate-turbo",
+				"ibm,pstate-ultra-turbo",
+				};
+
+		power_mgt = dt_find_by_path(dt_root, "/ibm,opal/power-mgt");
+		for (i = 0; i < ARRAY_SIZE(props); i++)
+			dt_check_del_prop(power_mgt, props[i]);
+	}
 
 	switch (proc_gen) {
 	case proc_gen_p8:
@@ -1604,6 +1620,9 @@ void occ_pstates_init(void)
 			for_each_available_core_in_chip(c, chip->id)
 				cpu_pstates_prepare_core(chip, c, pstate_nom);
 	}
+
+	if (occ_pstates_initialized)
+		return;
 
 	/* Add opal_poller to poll OCC throttle status of each chip */
 	for_each_chip(chip)

core/init.c
@@ -486,6 +486,8 @@ void __noreturn load_and_boot_kernel(bool is_reboot)
 
 	ipmi_set_fw_progress_sensor(IPMI_FW_OS_BOOT);
 
+	occ_pstates_init();
+
 	if (!is_reboot) {
 		/* We wait for the nvram read to complete here so we can
 		 * grab stuff from there such as the kernel arguments
@@ -499,7 +501,6 @@ void __noreturn load_and_boot_kernel(bool is_reboot)
 		 * OCC takes few secs to boot.  Call this as late as
 		 * as possible to avoid delay.
 		 */
-		occ_pstates_init();
 		occ_sensors_init();
 
 	} else {

commit 92a303dfeeb61c73ea4d9226af6c4d874db6bc89

hw/occ: Fix fast-reboot crash in P8 platforms.
commit 85a1de35cbe4 ("fast-boot: occ: Re-parse the pstate table during fast-boot" )
breaks the fast-reboot on P8 platforms while reiniting the OCC pstates. On P8
platforms OPAL adds additional two properties #address-cells and #size-cells
under ibm,opal/power-mgmt/ DT node. While in fast-reboot same properties adding
back to the same node results in Duplicate properties and hence fast-reboot fails
with below traces.

[  541.410373292,5] OCC: All Chip Rdy after 0 ms
[  541.410488745,3] Duplicate property "#address-cells" in node /ibm,opal/power-mgt
[  541.410694290,0] Aborting!
CPU 0058 Backtrace:
 S: 0000000031d639d0 R: 000000003001367c   .backtrace+0x48
 S: 0000000031d63a60 R: 000000003001a03c   ._abort+0x4c
 S: 0000000031d63ae0 R: 00000000300267d8   .new_property+0xd8
 S: 0000000031d63b70 R: 0000000030026a28   .__dt_add_property_cells+0x30
 S: 0000000031d63c10 R: 000000003003ea3c   .occ_pstates_init+0x984
 S: 0000000031d63d90 R: 00000000300142d8   .load_and_boot_kernel+0x86c
 S: 0000000031d63e70 R: 000000003002586c   .fast_reboot_entry+0x358
 S: 0000000031d63f00 R: 00000000300029f4   fast_reset_entry+0x2c

This patch fixes this issue by removing these two properties on P8 while doing
OCC pstates re-init in fast-reboot code path.

Fixes: 85a1de35cbe4
Signed-off-by: Pridhiviraj Paidipeddi <ppaidipe@linux.vnet.ibm.com>
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

hw/occ.c
@@ -1568,6 +1568,8 @@ void occ_pstates_init(void)
 				"ibm,pstate-nominal",
 				"ibm,pstate-turbo",
 				"ibm,pstate-ultra-turbo",
+				"#address-cells",
+				"#size-cells",
 				};
 
 		power_mgt = dt_find_by_path(dt_root, "/ibm,opal/power-mgt");