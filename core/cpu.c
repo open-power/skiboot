open-power/skiboot

commit 514406fa44279996bfc9c85c1e4e53689d375e64

cpu: Remove pollers calling heuristics from cpu_wait_job
This will be handled by time_wait_ms(). Also remove a useless
smt_medium().

Note that this introduce a difference in behaviour: time_wait
will only call the pollers on the boot CPU while cpu_wait_job()
could call them on any. However, I can't think of a case where
this is a problem.

Signed-off-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

core/cpu.c
@@ -207,22 +207,18 @@ bool cpu_poll_job(struct cpu_job *job)
 
 void cpu_wait_job(struct cpu_job *job, bool free_it)
 {
-	unsigned long ticks = usecs_to_tb(5);
-	unsigned long period = msecs_to_tb(5);
 	unsigned long time_waited = 0;
 
 	if (!job)
 		return;
 
-	while(!job->complete) {
-		time_wait(ticks);
-		time_waited+=ticks;
-		if (time_waited % period == 0)
-			opal_run_pollers();
+	while (!job->complete) {
+		/* This will call OPAL pollers for us */
+		time_wait_ms(10);
+		time_waited += 10;
 		lwsync();
 	}
 	lwsync();
-	smt_medium();
 
 	if (time_waited > msecs_to_tb(1000))
 		prlog(PR_DEBUG, "cpu_wait_job(%s) for %lu\n",

commit 9b67f73a59473c1076016055eb43dcc72b7109cb

cpu_wait_job: Correctly report time spent waiting for job
Way back when, we got confused between timebase and ms,
so let's just use ms and be done with it.

Fixes: 514406fa44279996bfc9c85c1e4e53689d375e64
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

core/cpu.c
@@ -245,9 +245,9 @@ void cpu_wait_job(struct cpu_job *job, bool free_it)
 	}
 	lwsync();
 
-	if (time_waited > msecs_to_tb(1000))
-		prlog(PR_DEBUG, "cpu_wait_job(%s) for %lu\n",
-		      job->name, tb_to_msecs(time_waited));
+	if (time_waited > 1000)
+		prlog(PR_DEBUG, "cpu_wait_job(%s) for %lums\n",
+		      job->name, time_waited);
 
 	if (free_it)
 		free(job);