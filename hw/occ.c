open-power/skiboot

commit ac4272bf5e73de78bfb4cdffac8f57222b059f72

fast-reboot: occ: Delete OCC child nodes in /ibm, opal/power-mgt
Fast-reboot in P8 fails to re-init OCC data as there are chipwise OCC
nodes which are already present in the /ibm,opal/power-mgt node. These
per-chip nodes hold the voltage IDs for each pstate and these can be
changed on OCC pstate table biasing. So delete these before calling
the re-init code to re-parse and populate the pstate data.

Reported-by: Pridhiviraj Paidipeddi <ppaidipe@linux.vnet.ibm.com>
Signed-off-by: Shilpasri G Bhat <shilpa.bhat@linux.vnet.ibm.com>
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

hw/occ.c
@@ -1664,7 +1664,7 @@ void occ_pstates_init(void)
 		return;
 	/* Handle fast reboots */
 	if (occ_pstates_initialized) {
-		struct dt_node *power_mgt;
+		struct dt_node *power_mgt, *child;
 		int i;
 		const char *props[] = {
 				"ibm,pstate-core-max",
@@ -1682,6 +1682,10 @@ void occ_pstates_init(void)
 		power_mgt = dt_find_by_path(dt_root, "/ibm,opal/power-mgt");
 		for (i = 0; i < ARRAY_SIZE(props); i++)
 			dt_check_del_prop(power_mgt, props[i]);
+
+		dt_for_each_child(power_mgt, child)
+			if (!strncmp(child->name, "occ", 3))
+				dt_free(child);
 	}
 
 	switch (proc_gen) {

commit 7598ed90a6706158cce34596a33ededbb47af393

fast-reboot: occ: Only delete /ibm, opal/power-mgt nodes if they exist
Fixes: ac4272bf ("fast-reboot: occ: Delete OCC child nodes in /ibm, opal/power-mgt")
Fixes: CID 263053
Signed-off-by: Cyril Bur <cyril.bur@au1.ibm.com>
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

hw/occ.c
@@ -1696,12 +1696,14 @@ void occ_pstates_init(void)
 				};
 
 		power_mgt = dt_find_by_path(dt_root, "/ibm,opal/power-mgt");
-		for (i = 0; i < ARRAY_SIZE(props); i++)
-			dt_check_del_prop(power_mgt, props[i]);
+		if (power_mgt) {
+			for (i = 0; i < ARRAY_SIZE(props); i++)
+				dt_check_del_prop(power_mgt, props[i]);
 
-		dt_for_each_child(power_mgt, child)
-			if (!strncmp(child->name, "occ", 3))
-				dt_free(child);
+			dt_for_each_child(power_mgt, child)
+				if (!strncmp(child->name, "occ", 3))
+					dt_free(child);
+		}
 	}
 
 	switch (proc_gen) {