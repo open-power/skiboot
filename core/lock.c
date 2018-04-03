open-power/skiboot

commit a8e6cc3f47525f86ef1d69d69a477b6264d0f8ee

Don't detect lock timeouts when timebase is invalid
We can have threads waiting on hmi_lock who have an
invalid timebase. Because of this, we want to go poke
the register directly rather than rely on
this_cpu()->tb_invalid (which won't have been set yet).

Without this patch, you get something like this when
you're injecting timebase errors:
[10976.202052846,4] WARNING: Lock has been spinning for 10976394ms

Fixes: 84186ef0944c9413262f0974ddab3fb1343ccfe8
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

core/lock.c
@@ -140,6 +140,13 @@ static inline bool lock_timeout(unsigned long start)
 	unsigned long wait = tb_to_msecs(mftb());
 
 	if (wait - start > LOCK_TIMEOUT_MS) {
+		/*
+		 * If the timebase is invalid, we shouldn't
+		 * throw an error. This is possible with pending HMIs
+		 * that need to recover TB.
+		 */
+		if( !(mfspr(SPR_TFMR) & SPR_TFMR_TB_VALID))
+			return false;
 		prlog(PR_WARNING, "WARNING: Lock has been "\
 		      "spinning for %lums\n", wait - start);
 		backtrace();

commit 36f6c25c731446d2dafa1be3b412c387259f47bd

core/lock.c: ensure valid start value for lock spin duration warning
The previous fix in a8e6cc3f4 only addressed half of the problem, as
we could also get an invalid value for start, causing us to fail
in a weird way.

This was caught by the testcases.OpTestHMIHandling.HMI_TFMR_ERRORS
test in op-test-framework.

You'd get to this part of the test and get the erroneous lock
spinning warnings:

PATH=/usr/local/sbin:$PATH putscom -c 00000000 0x2b010a84 0003080000000000
0000080000000000
[  790.140976993,4] WARNING: Lock has been spinning for 790275ms
[  790.140976993,4] WARNING: Lock has been spinning for 790275ms
[  790.140976918,4] WARNING: Lock has been spinning for 790275ms

This patch checks the validity of timebase before setting start,
and only checks the lock timeout if we got a valid start value.

Fixes: a8e6cc3f47525f86ef1d69d69a477b6264d0f8ee
Fixes: 84186ef0944c9413262f0974ddab3fb1343ccfe8
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>
Reviewed-by: Mahesh Salgaonkar <mahesh@linux.vnet.ibm.com>
Tested-by: Pridhiviraj Paidipeddi <ppaidipe@linux.vnet.ibm.com>
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

core/lock.c
@@ -206,7 +206,7 @@ bool try_lock_caller(struct lock *l, const char *owner)
 void lock_caller(struct lock *l, const char *owner)
 {
 	bool timeout_warn = false;
-	unsigned long start;
+	unsigned long start = 0;
 
 	if (bust_locks)
 		return;
@@ -218,7 +218,13 @@ void lock_caller(struct lock *l, const char *owner)
 	add_lock_request(l);
 
 #ifdef DEBUG_LOCKS
-	start = tb_to_msecs(mftb());
+	/*
+	 * Ensure that we get a valid start value
+	 * as we may be handling TFMR errors and taking
+	 * a lock to do so, so timebase could be garbage
+	 */
+	if( (mfspr(SPR_TFMR) & SPR_TFMR_TB_VALID))
+		start = tb_to_msecs(mftb());
 #endif
 
 	for (;;) {
@@ -229,7 +235,7 @@ void lock_caller(struct lock *l, const char *owner)
 			barrier();
 		smt_medium();
 
-		if (!timeout_warn)
+		if (start && !timeout_warn)
 			timeout_warn = lock_timeout(start);
 	}
 