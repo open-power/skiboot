open-power/skiboot

commit 9750eee802f8d6396147ee4e046e1859949a0e55

hw/imc: pause microcode at boot
IMC nest counters has both in-band (ucode access) and out of
band access to it. Since not all nest counter configurations
are supported by ucode, out of band tools are used to characterize
other configuration.

So it is prefer to pause the nest microcode at boot to aid the
nest out of band tools. If the ucode not paused and OS does not
have IMC driver support, then out to band tools will race with
ucode and end up getting undesirable values. Patch to check and
pause the ucode at boot.

OPAL provides APIs to control IMC counters. OPAL_IMC_COUNTERS_INIT
is used to initialize these counters at boot. OPAL_IMC_COUNTERS_START
and OPAL_IMC_COUNTERS_STOP API calls should be used to start and pause
these IMC engines. `doc/opal-api/opal-imc-counters.rst` details the
OPAL APIs and their usage.

Signed-off-by: Madhavan Srinivasan <maddy@linux.vnet.ibm.com>
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

hw/imc.c
@@ -160,6 +160,18 @@ static struct imc_chip_cb *get_imc_cb(uint32_t chip_id)
 	return cb;
 }
 
+static void pause_microcode_at_boot(void)
+{
+	struct proc_chip *chip;
+	struct imc_chip_cb *cb;
+
+	for_each_chip(chip) {
+		cb = get_imc_cb(chip->id);
+		if (cb)
+			cb->imc_chip_command =  cpu_to_be64(NEST_IMC_DISABLE);
+	}
+}
+
 /*
  * Decompresses the blob obtained from the IMC pnor sub-partition
  * in "src" of size "src_size", assigns the uncompressed device tree
@@ -541,6 +553,18 @@ imc_mambo:
 
 	if (proc_chip_quirks & QUIRK_MAMBO_CALLOUTS)
 		return;
+
+	/*
+	 * IMC nest counters has both in-band (ucode access) and out of band
+	 * access to it. Since not all nest counter configurations are supported
+	 * by ucode, out of band tools are used to characterize other
+	 * configuration.
+	 *
+	 * If the ucode not paused and OS does not have IMC driver support,
+	 * then out to band tools will race with ucode and end up getting
+	 * undesirable values. Hence pause the ucode if it is already running.
+	 */
+	pause_microcode_at_boot();
 
 	/*
 	 * If the dt_attach_root() fails, "imc-counters" node will not be

commit afc89188010b4b33c78357eb9af1064690d324cb

hw/imc: Check for pause_microcode_at_boot() return status
pause_microcode_at_boot() loops through all the chip's ucode
control block and pause the ucode if it is in the running state.
But it does not fail if any of the chip's ucode is not initialised.

Add code to return a failure if ucode is not initialized in any
of the chip. Since pause_microcode_at_boot() is called just before
attaching the IMC device nodes in imc_init(), add code to check for
the function return.

Fixes: 9750eee802f8d ('hw/imc: pause microcode at boot')
Signed-off-by: Madhavan Srinivasan <maddy@linux.vnet.ibm.com>
Signed-off-by: Stewart Smith <stewart@linux.ibm.com>

hw/imc.c
@@ -164,7 +164,7 @@ static struct imc_chip_cb *get_imc_cb(uint32_t chip_id)
 	return cb;
 }
 
-static void pause_microcode_at_boot(void)
+static int pause_microcode_at_boot(void)
 {
 	struct proc_chip *chip;
 	struct imc_chip_cb *cb;
@@ -173,7 +173,11 @@ static void pause_microcode_at_boot(void)
 		cb = get_imc_cb(chip->id);
 		if (cb)
 			cb->imc_chip_command =  cpu_to_be64(NEST_IMC_DISABLE);
+		else
+			return -1; /* ucode is not init-ed */
 	}
+
+	return 0;
 }
 
 /*
@@ -606,7 +610,8 @@ imc_mambo:
 	 * then out to band tools will race with ucode and end up getting
 	 * undesirable values. Hence pause the ucode if it is already running.
 	 */
-	pause_microcode_at_boot();
+	if (pause_microcode_at_boot())
+		goto err;
 
 	/*
 	 * If the dt_attach_root() fails, "imc-counters" node will not be