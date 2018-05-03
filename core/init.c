open-power/skiboot

commit 293ca03683bfcd2c787b7e6f7c760238ba8eeae3

init: print the FDT blob size in decimal
Changes the skiboot output so that rather than printing the FDT address
and size in hex it prints the address in hex and the size in decimal.
Doing this lets you read the FDT blob with getmemproc and some
copy+paste.

Signed-off-by: Oliver O'Halloran <oohall@gmail.com>
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

core/init.c
@@ -541,7 +541,7 @@ void __noreturn load_and_boot_kernel(bool is_reboot)
 	printf("INIT: stdout-path: %s\n", stdoutp ? stdoutp : "");
 
 
-	printf("INIT: Starting kernel at 0x%llx, fdt at %p (size 0x%x)\n",
+	printf("INIT: Starting kernel at 0x%llx, fdt at %p %u bytes)\n",
 	       kernel_entry, fdt, fdt_totalsize(fdt));
 
 	debug_descriptor.state_flags |= OPAL_BOOT_COMPLETE;

commit 1d724fa588bc5bd05eed92690f73653a49207fb6

init: Fix trailing bracket in "Starting kernel"
We were getting:

 [    0.047155847,5] INIT: Starting kernel at 0x0, fdt at 0x30409d70 10390 bytes)

Now it says:

 [    0.048059045,5] INIT: Starting kernel at 0x0, fdt at 0x30409d80 10406 bytes

Fixes: 293ca03683bf ("init: print the FDT blob size in decimal")
Signed-off-by: Joel Stanley <joel@jms.id.au>
Signed-off-by: Stewart Smith <stewart@linux.ibm.com>

core/init.c
@@ -572,7 +572,7 @@ void __noreturn load_and_boot_kernel(bool is_reboot)
 	prlog(PR_DEBUG, "INIT: stdout-path: %s\n", stdoutp ? stdoutp : "");
 
 
-	printf("INIT: Starting kernel at 0x%llx, fdt at %p %u bytes)\n",
+	printf("INIT: Starting kernel at 0x%llx, fdt at %p %u bytes\n",
 	       kernel_entry, fdt, fdt_totalsize(fdt));
 
 	debug_descriptor.state_flags |= OPAL_BOOT_COMPLETE;