open-power/skiboot

commit b6a729e118f42dae88ebf70a09a7e2aa4f788fdc

Limit number of "Poller recursion detected" errors to display
In some error conditions, we could spiral out of control on this
and spend all of our time printing the exact same backtrace.

Limit it to 16 times, because 16 is a nice number.

Cc: stable
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>
Reviewed-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

core/opal.c
@@ -313,9 +313,10 @@ void opal_run_pollers(void)
 {
 	struct opal_poll_entry *poll_ent;
 	static int pollers_with_lock_warnings = 0;
+	static int poller_recursion = 0;
 
 	/* Don't re-enter on this CPU */
-	if (this_cpu()->in_poller) {
+	if (this_cpu()->in_poller && poller_recursion < 16) {
 		/**
 		 * @fwts-label OPALPollerRecursion
 		 * @fwts-advice Recursion detected in opal_run_pollers(). This
@@ -324,6 +325,9 @@ void opal_run_pollers(void)
 		 */
 		prlog(PR_ERR, "OPAL: Poller recursion detected.\n");
 		backtrace();
+		poller_recursion++;
+		if (poller_recursion == 16)
+			prlog(PR_ERR, "OPAL: Squashing future poller recursion warnings (>16).\n");
 		return;
 	}
 	this_cpu()->in_poller = true;

commit 92d1a4e923fa427ab73f52850381b08c1bcf5676

core/opal: Fix recursion check in opal_run_pollers()
An earlier commit introduced a counter variable poller_recursion to
limit to the number number of error messages shown when opal_pollers
are run recursively. However the check for the counter value was
placed in a way that the poller recursion was only detected first 16
times and then allowed afterwards.

This patch fixes this by moving the check for the counter value inside
the conditional branch with some re-factoring so that opal_poller
recursion is not erroneously allowed after poll_recursion is detected
first 16 times.

Fixes: b6a729e118f4 ("Limit number of Poller recursion detected errors to display")
Signed-off-by: Vaibhav Jain <vaibhav@linux.ibm.com>
Signed-off-by: Stewart Smith <stewart@linux.ibm.com>

core/opal.c
@@ -552,20 +552,25 @@ void opal_run_pollers(void)
 	bool was_in_poller;
 
 	/* Don't re-enter on this CPU, unless it was an OPAL re-entry */
-	if (this_cpu()->in_opal_call == 1 &&
-			this_cpu()->in_poller && poller_recursion < 16) {
+	if (this_cpu()->in_opal_call == 1 && this_cpu()->in_poller) {
+
 		/**
 		 * @fwts-label OPALPollerRecursion
 		 * @fwts-advice Recursion detected in opal_run_pollers(). This
 		 * indicates a bug in OPAL where a poller ended up running
 		 * pollers, which doesn't lead anywhere good.
 		 */
-		disable_fast_reboot("Poller recursion detected.");
-		prlog(PR_ERR, "OPAL: Poller recursion detected.\n");
-		backtrace();
 		poller_recursion++;
+		if (poller_recursion <= 16) {
+			disable_fast_reboot("Poller recursion detected.");
+			prlog(PR_ERR, "OPAL: Poller recursion detected.\n");
+			backtrace();
+
+		}
+
 		if (poller_recursion == 16)
 			prlog(PR_ERR, "OPAL: Squashing future poller recursion warnings (>16).\n");
+
 		return;
 	}
 	was_in_poller = this_cpu()->in_poller;