open-power/skiboot

commit 2b841bf0ef1b20d9544ffe641d6271b65b9efcc4

core/pci: Use cached vendor/device IDs in quirks
The PCI device vendor/device IDs have been cached to pd->vdid, no
need to pass them in pci_handle_quirk(). This also introduces two
macros to extract vendor/device fields and they are useful afterwards.
No logical changes introduced.

Signed-off-by: Gavin Shan <gwshan@linux.vnet.ibm.com>
Acked-by: Benjamin Herrenschmidt <benh@kernel.crashing.org>
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

include/pci.h
@@ -73,6 +73,8 @@ struct pci_device {
 
 	uint32_t		vdid;
 	uint32_t		sub_vdid;
+#define PCI_VENDOR_ID(x)	((x) & 0xFFFF)
+#define PCI_DEVICE_ID(x)	((x) >> 8)
 	uint32_t		class;
 	uint64_t		cap_list;
 	struct {

core/pci-quirk.c
@@ -70,17 +70,14 @@ static const struct pci_quirk quirk_table[] = {
 	{NULL}
 };
 
-void pci_handle_quirk(struct phb *phb,
-		      struct pci_device *pd,
-		      uint16_t vendor_id,
-		      uint16_t device_id)
+void pci_handle_quirk(struct phb *phb, struct pci_device *pd)
 {
 	const struct pci_quirk *quirks = quirk_table;
 
 	while (quirks->vendor_id) {
-		if (vendor_id == quirks->vendor_id &&
+		if (quirks->vendor_id == PCI_VENDOR_ID(pd->vdid) &&
 		    (quirks->device_id == PCI_ANY_ID ||
-		     device_id == quirks->device_id))
+		     quirks->device_id == PCI_DEVICE_ID(pd->vdid)))
 			quirks->fixup(phb, pd);
 		quirks++;
 	}

core/pci.c
@@ -1467,7 +1467,7 @@ static void pci_add_one_device_node(struct phb *phb,
 	if (intpin)
 		dt_add_property_cells(np, "interrupts", intpin);
 
-	pci_handle_quirk(phb, pd, vdid & 0xffff, vdid >> 16);
+	pci_handle_quirk(phb, pd);
 
 	/* XXX FIXME: Add a few missing ones such as
 	 *

include/pci-quirk.h
@@ -27,9 +27,6 @@ struct pci_quirk {
 	uint16_t device_id;
 };
 
-void pci_handle_quirk(struct phb *phb,
-		      struct pci_device *pd,
-		      uint16_t vendor_id,
-		      uint16_t device_id);
+void pci_handle_quirk(struct phb *phb, struct pci_device *pd);
 
 #endif /* __PCI_QUIRK_H */

commit 50dfd067835af53736ec8106b1f0e99339b54a81

pci: Fix PCI_DEVICE_ID()
The vendor ID is 16 bits not 8. This error leaves the top of the vendor
ID in the bottom bits of the device ID, which resulted in e.g. a failure
to run the PCI quirk for the AST VGA device.

Fixes: 2b841bf0ef1b ("core/pci: Use cached vendor/device IDs in quirks")
Signed-off-by: Andrew Jeffery <andrew@aj.id.au>
Signed-off-by: Stewart Smith <stewart@linux.ibm.com>

include/pci.h
@@ -76,7 +76,7 @@ struct pci_device {
 	uint32_t		vdid;
 	uint32_t		sub_vdid;
 #define PCI_VENDOR_ID(x)	((x) & 0xFFFF)
-#define PCI_DEVICE_ID(x)	((x) >> 8)
+#define PCI_DEVICE_ID(x)	((x) >> 16)
 	uint32_t		class;
 	uint64_t		cap_list;
 	struct {