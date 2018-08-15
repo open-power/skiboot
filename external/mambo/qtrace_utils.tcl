open-power/skiboot

commit cb835dbdf8758b1fb0cae0ef2f93b324d1c4c96e

external/mambo: conditionally source qtrace script
This automatically gives qtrace commads if the simulator provides
the capability.

Signed-off-by: Nicholas Piggin <npiggin@gmail.com>
Signed-off-by: Stewart Smith <stewart@linux.ibm.com>

external/mambo/qtrace_utils.tcl
@@ -1,10 +1,12 @@
-source $env(LIB_DIR)/perf/qtrace.tcl
+if { [file exists $env(LIB_DIR)/perf/qtrace.tcl] == 1} {
+    source $env(LIB_DIR)/perf/qtrace.tcl
 
-proc start_qtrace { { qtfile qtrace.qt } } {
-    QTrace::Initialize p9 mysim
-    QTrace::Start $qtfile mysim
-}
+    proc start_qtrace { { qtfile qtrace.qt } } {
+        QTrace::Initialize p9 mysim
+        QTrace::Start $qtfile mysim
+    }
 
-proc stop_qtrace { } {
-    QTrace::Stop mysim
+    proc stop_qtrace { } {
+        QTrace::Stop mysim
+    }
 }

external/mambo/skiboot.tcl
@@ -1,5 +1,6 @@
 # need to get images path defined early
 source $env(LIB_DIR)/ppc/util.tcl
+source qtrace_utils.tcl
 
 #
 # Call tclreadline's Loop to move to friendlier

commit b6605667cf80ca7429a9b696dd6474961919a8a5

don't fail fatally if qtrace can't be loaded
Helps with p9 public mambo on fedora at least

Fixes: cb835dbdf8758b1fb0cae0ef2f93b324d1c4c96e
Signed-off-by: Stewart Smith <stewart@linux.ibm.com>

external/mambo/qtrace_utils.tcl
@@ -1,5 +1,7 @@
 if { [file exists $env(LIB_DIR)/perf/qtrace.tcl] == 1} {
-    source $env(LIB_DIR)/perf/qtrace.tcl
+    if { [catch {source $env(LIB_DIR)/perf/qtrace.tcl} issue ] } {
+        puts "QTrace not available: $issue"
+    }
 
     proc start_qtrace { { qtfile qtrace.qt } } {
         QTrace::Initialize p9 mysim