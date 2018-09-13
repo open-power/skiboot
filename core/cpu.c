open-power/skiboot

commit 7a3f307ed0db8552a086dd0e4a45fe08e1807084

core/cpu: parallelise global CPU register setting jobs
On a 176 thread system, before:

[  122.319923233,5] OPAL: Switch to big-endian OS
[  126.317897467,5] OPAL: Switch to little-endian OS

after:

[  212.439299889,5] OPAL: Switch to big-endian OS
[  212.469323643,5] OPAL: Switch to little-endian OS

Signed-off-by: Nicholas Piggin <npiggin@gmail.com>
Signed-off-by: Stewart Smith <stewart@linux.ibm.com>

core/cpu.c
@@ -1304,17 +1304,30 @@ static void cpu_change_hid0(void *__req)
 static int64_t cpu_change_all_hid0(struct hid0_change_req *req)
 {
 	struct cpu_thread *cpu;
+	struct cpu_job **jobs;
+
+	jobs = zalloc(sizeof(struct cpu_job *) * cpu_max_pir + 1);
+	assert(jobs);
 
 	for_each_available_cpu(cpu) {
 		if (!cpu_is_thread0(cpu))
 			continue;
-		if (cpu == this_cpu()) {
-			cpu_change_hid0(req);
+		if (cpu == this_cpu())
 			continue;
-		}
-		cpu_wait_job(cpu_queue_job(cpu, "cpu_change_hid0",
-			cpu_change_hid0, req), true);
+		jobs[cpu->pir] = cpu_queue_job(cpu, "cpu_change_hid0",
+						cpu_change_hid0, req);
 	}
+
+	/* this cpu */
+	cpu_change_hid0(req);
+
+	for_each_available_cpu(cpu) {
+		if (jobs[cpu->pir])
+			cpu_wait_job(jobs[cpu->pir], true);
+	}
+
+	free(jobs);
+
 	return OPAL_SUCCESS;
 }
 
@@ -1342,15 +1355,29 @@ static void cpu_cleanup_one(void *param __unused)
 static int64_t cpu_cleanup_all(void)
 {
 	struct cpu_thread *cpu;
+	struct cpu_job **jobs;
+
+	jobs = zalloc(sizeof(struct cpu_job *) * cpu_max_pir + 1);
+	assert(jobs);
 
 	for_each_available_cpu(cpu) {
-		if (cpu == this_cpu()) {
-			cpu_cleanup_one(NULL);
+		if (cpu == this_cpu())
 			continue;
-		}
-		cpu_wait_job(cpu_queue_job(cpu, "cpu_cleanup",
-					   cpu_cleanup_one, NULL), true);
+		jobs[cpu->pir] = cpu_queue_job(cpu, "cpu_cleanup",
+						cpu_cleanup_one, NULL);
 	}
+
+	/* this cpu */
+	cpu_cleanup_one(NULL);
+
+	for_each_available_cpu(cpu) {
+		if (jobs[cpu->pir])
+			cpu_wait_job(jobs[cpu->pir], true);
+	}
+
+	free(jobs);
+
+
 	return OPAL_SUCCESS;
 }
 

commit df5da053c459369b4fc4a831dd09ce1859efe436

core/cpu: Fix memory allocation for job array
fixes: 7a3f307e core/cpu: parallelise global CPU register setting jobs

This bug would result in boot-hang on some configurations due to
cpu_wait_job() endlessly waiting for the last bogus jobs[cpu->pir] pointer.

Reported-by: Stephanie Swanson <swanman@us.ibm.com>
Reported-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>
Reviewed-by: Mahesh Salgaonkar <mahesh@linux.vnet.ibm.com>
Reviewed-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>
Reviewed-by: Nicholas Piggin <npiggin@gmail.com>
Signed-off-by: Vaidyanathan Srinivasan <svaidy@linux.vnet.ibm.com>
Signed-off-by: Stewart Smith <stewart@linux.vnet.ibm.com>

core/cpu.c
@@ -1373,7 +1373,7 @@ static int64_t cpu_change_all_hid0(struct hid0_change_req *req)
 	struct cpu_thread *cpu;
 	struct cpu_job **jobs;
 
-	jobs = zalloc(sizeof(struct cpu_job *) * cpu_max_pir + 1);
+	jobs = zalloc(sizeof(struct cpu_job *) * (cpu_max_pir + 1));
 	assert(jobs);
 
 	for_each_available_cpu(cpu) {
@@ -1424,7 +1424,7 @@ static int64_t cpu_cleanup_all(void)
 	struct cpu_thread *cpu;
 	struct cpu_job **jobs;
 
-	jobs = zalloc(sizeof(struct cpu_job *) * cpu_max_pir + 1);
+	jobs = zalloc(sizeof(struct cpu_job *) * (cpu_max_pir + 1));
 	assert(jobs);
 
 	for_each_available_cpu(cpu) {