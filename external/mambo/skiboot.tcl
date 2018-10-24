open-power/skiboot

commit cb835dbdf8758b1fb0cae0ef2f93b324d1c4c96e

external/mambo: conditionally source qtrace script
This automatically gives qtrace commads if the simulator provides
the capability.

Signed-off-by: Nicholas Piggin <npiggin@gmail.com>
Signed-off-by: Stewart Smith <stewart@linux.ibm.com>

external/mambo/skiboot.tcl
@@ -1,5 +1,6 @@
 # need to get images path defined early
 source $env(LIB_DIR)/ppc/util.tcl
+source qtrace_utils.tcl
 
 #
 # Call tclreadline's Loop to move to friendlier

external/mambo/qtrace_utils.tcl
@@ -1,10 +1,12 @@
-source $env(LIB_DIR)/perf/qtrace.tcl
+if { [file exists $env(LIB_DIR)/perf/qtrace.tcl] == 1} {
+    source $env(LIB_DIR)/perf/qtrace.tcl
 
-proc start_qtrace { { qtfile qtrace.qt } } {
-    QTrace::Initialize p9 mysim
-    QTrace::Start $qtfile mysim
-}
+    proc start_qtrace { { qtfile qtrace.qt } } {
+        QTrace::Initialize p9 mysim
+        QTrace::Start $qtfile mysim
+    }
 
-proc stop_qtrace { } {
-    QTrace::Stop mysim
+    proc stop_qtrace { } {
+        QTrace::Stop mysim
+    }
 }

commit ae546e0b6d977b13ed93d63c365376af87f0fbb6

external/mambo: Check for qtrace_utils.tcl before sourcing it
Commit cb835dbdf875 ('external/mambo: conditionally source qtrace script')
added qtrace_utils.tcl sourcing in skiboot.tcl without a check to see
whether it exists in the current directory. This broke running mambo from
another directory using skiboot.tcl. Patch adds a check.

Fixes: cb835dbdf875 ('external/mambo: conditionally source qtrace script')
Signed-off-by: Madhavan Srinivasan <maddy@linux.vnet.ibm.com>
Signed-off-by: Stewart Smith <stewart@linux.ibm.com>

external/mambo/skiboot.tcl
@@ -1,6 +1,8 @@
 # need to get images path defined early
 source $env(LIB_DIR)/ppc/util.tcl
-source qtrace_utils.tcl
+if { [file exists qtrace_utils.tcl] } then {
+	source qtrace_utils.tcl
+}
 
 #
 # Call tclreadline's Loop to move to friendlier