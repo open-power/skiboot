open-power/skiboot

commit 50ea35c2d07874755c03e6ae2bdf7a33ad2c768a

Warn on long OPAL calls
Measure entry/exit time for OPAL calls and warn appropriately if the
calls take too long (>100ms gets us a DEBUG log, > 1000ms gets us a
warning).

Signed-off-by: Stewart Smith <stewart@linux.ibm.com>

core/opal.c
@@ -167,6 +167,7 @@ int64_t opal_entry_check(struct stack_frame *eframe)
 		}
 	}
 
+	cpu->entered_opal_call_at = mftb();
 	return OPAL_SUCCESS;
 }
 
@@ -176,6 +177,8 @@ int64_t opal_exit_check(int64_t retval, struct stack_frame *eframe)
 {
 	struct cpu_thread *cpu = this_cpu();
 	uint64_t token = eframe->gpr[0];
+	uint64_t now = mftb();
+	uint64_t call_time = tb_to_msecs(now - cpu->entered_opal_call_at);
 
 	if (!cpu->in_opal_call) {
 		disable_fast_reboot("Un-accounted firmware entry");
@@ -192,6 +195,12 @@ int64_t opal_exit_check(int64_t retval, struct stack_frame *eframe)
 			      token, retval);
 			drop_my_locks(true);
 		}
+	}
+
+	if (call_time > 100) {
+		prlog((call_time < 1000) ? PR_DEBUG : PR_WARNING,
+		      "Spent %llu msecs in OPAL call %llu!\n",
+		      call_time, token);
 	}
 	return retval;
 }

include/cpu.h
@@ -62,6 +62,7 @@ struct cpu_thread {
 	void				*icp_regs;
 	uint32_t			in_opal_call;
 	uint32_t			quiesce_opal_call;
+	uint64_t entered_opal_call_at;
 	uint32_t			con_suspend;
 	struct list_head		locks_held;
 	bool				con_need_flush;

commit 37bce07eea2b317871810d186c75b3b9d73e71d9

Don't warn on "long" OPAL_RESYNC_TIMEBASE calls
On P8 this is called when we exit fastsleep, and we shouldn't measure
the "time" spent in the call for what (in retrospect) is an obvious
reason.

Fixes: 50ea35c2d07874755c03e6ae2bdf7a33ad2c768a
Signed-off-by: Stewart Smith <stewart@linux.ibm.com>

core/opal.c
@@ -197,7 +197,7 @@ int64_t opal_exit_check(int64_t retval, struct stack_frame *eframe)
 		}
 	}
 
-	if (call_time > 100) {
+	if (call_time > 100 && token != OPAL_RESYNC_TIMEBASE) {
 		prlog((call_time < 1000) ? PR_DEBUG : PR_WARNING,
 		      "Spent %llu msecs in OPAL call %llu!\n",
 		      call_time, token);