7735cc3546098c2970765d4d1c216895c651c8ad
Andrew Jeffery <andrew@aj.id.au>
2019-02-21 16:58:45 +1030

    test-ipmi-hiomap: Add erase-malformed tests
    Cc: stable
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>
    Signed-off-by: Stewart Smith <stewart@linux.ibm.com>

--- libflash/test/test-ipmi-hiomap.c
+++ libflash/test/test-ipmi-hiomap.c
@@ -2258,6 +2258,96 @@ static void test_hiomap_flush_malformed_large(void)
 	scenario_exit();
 }
 
+static const struct scenario_event
+scenario_hiomap_erase_malformed_small[] = {
+	{ .type = scenario_event_p, .p = &hiomap_ack_call, },
+	{ .type = scenario_event_p, .p = &hiomap_get_info_call, },
+	{ .type = scenario_event_p, .p = &hiomap_get_flash_info_call, },
+	{
+		.type = scenario_event_p,
+		.p = &hiomap_create_write_window_qs0l1_rs0l1_call,
+	},
+	{
+		.type = scenario_cmd,
+		.c = {
+			.req = {
+				.cmd = HIOMAP_C_ERASE,
+				.seq = 5,
+				.args = {
+					[0] = 0x00, [1] = 0x00,
+					[2] = 0x01, [3] = 0x00,
+				},
+			},
+			.resp_size = 1,
+			.resp = {
+				.cmd = HIOMAP_C_ERASE,
+				.seq = 5,
+			},
+		},
+	},
+	SCENARIO_SENTINEL,
+};
+
+static void test_hiomap_erase_malformed_small(void)
+{
+	struct blocklevel_device *bl;
+	struct ipmi_hiomap *ctx;
+	size_t len;
+
+	scenario_enter(scenario_hiomap_erase_malformed_small);
+	assert(!ipmi_hiomap_init(&bl));
+	ctx = container_of(bl, struct ipmi_hiomap, bl);
+	len = 1 << ctx->block_size_shift;
+	assert(bl->erase(bl, 0, len) > 0);
+	ipmi_hiomap_exit(bl);
+	scenario_exit();
+}
+
+static const struct scenario_event
+scenario_hiomap_erase_malformed_large[] = {
+	{ .type = scenario_event_p, .p = &hiomap_ack_call, },
+	{ .type = scenario_event_p, .p = &hiomap_get_info_call, },
+	{ .type = scenario_event_p, .p = &hiomap_get_flash_info_call, },
+	{
+		.type = scenario_event_p,
+		.p = &hiomap_create_write_window_qs0l1_rs0l1_call,
+	},
+	{
+		.type = scenario_cmd,
+		.c = {
+			.req = {
+				.cmd = HIOMAP_C_ERASE,
+				.seq = 5,
+				.args = {
+					[0] = 0x00, [1] = 0x00,
+					[2] = 0x01, [3] = 0x00,
+				},
+			},
+			.resp_size = 3,
+			.resp = {
+				.cmd = HIOMAP_C_ERASE,
+				.seq = 5,
+			},
+		},
+	},
+	SCENARIO_SENTINEL,
+};
+
+static void test_hiomap_erase_malformed_large(void)
+{
+	struct blocklevel_device *bl;
+	struct ipmi_hiomap *ctx;
+	size_t len;
+
+	scenario_enter(scenario_hiomap_erase_malformed_large);
+	assert(!ipmi_hiomap_init(&bl));
+	ctx = container_of(bl, struct ipmi_hiomap, bl);
+	len = 1 << ctx->block_size_shift;
+	assert(bl->erase(bl, 0, len) > 0);
+	ipmi_hiomap_exit(bl);
+	scenario_exit();
+}
+
 struct test_case {
 	const char *name;
 	void (*fn)(void);
@@ -2311,6 +2401,8 @@ struct test_case test_cases[] = {
 	TEST_CASE(test_hiomap_mark_dirty_malformed_large),
 	TEST_CASE(test_hiomap_flush_malformed_small),
 	TEST_CASE(test_hiomap_flush_malformed_large),
+	TEST_CASE(test_hiomap_erase_malformed_small),
+	TEST_CASE(test_hiomap_erase_malformed_large),
 	{ NULL, NULL },
 };
 
