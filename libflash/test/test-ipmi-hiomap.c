5ee7b0f70b044029ab366e45203b956271bfe756
Andrew Jeffery <andrew@aj.id.au>
2019-02-21 16:58:23 +1030

    test-ipmi-hiomap: Add erase-one-block test
    Cc: stable
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>
    Signed-off-by: Stewart Smith <stewart@linux.ibm.com>

--- libflash/test/test-ipmi-hiomap.c
+++ libflash/test/test-ipmi-hiomap.c
@@ -451,6 +451,24 @@ hiomap_create_write_window_qs1l1_rs1l1_call = {
 	},
 };
 
+static const struct scenario_event hiomap_erase_qs0l1_call = {
+	.type = scenario_cmd,
+	.c = {
+		.req = {
+			.cmd = HIOMAP_C_ERASE,
+			.seq = 5,
+			.args = {
+				[0] = 0x00, [1] = 0x00,
+				[2] = 0x01, [3] = 0x00,
+			},
+		},
+		.resp = {
+			.cmd = HIOMAP_C_ERASE,
+			.seq = 5,
+		},
+	},
+};
+
 static const struct scenario_event scenario_hiomap_init[] = {
 	{ .type = scenario_event_p, .p = &hiomap_ack_call, },
 	{ .type = scenario_event_p, .p = &hiomap_get_info_call, },
@@ -1128,6 +1146,41 @@ static void test_hiomap_protocol_event_during_write(void)
 }
 
 static const struct scenario_event
+scenario_hiomap_protocol_erase_one_block[] = {
+	{ .type = scenario_event_p, .p = &hiomap_ack_call, },
+	{ .type = scenario_event_p, .p = &hiomap_get_info_call, },
+	{ .type = scenario_event_p, .p = &hiomap_get_flash_info_call, },
+	{
+		.type = scenario_event_p,
+		.p = &hiomap_create_write_window_qs0l1_rs0l1_call,
+	},
+	{
+		.type = scenario_event_p,
+		.p = &hiomap_erase_qs0l1_call,
+	},
+	{
+		.type = scenario_event_p,
+		.p = &hiomap_flush_call,
+	},
+	SCENARIO_SENTINEL,
+};
+
+static void test_hiomap_protocol_erase_one_block(void)
+{
+	struct blocklevel_device *bl;
+	struct ipmi_hiomap *ctx;
+	size_t len;
+
+	scenario_enter(scenario_hiomap_protocol_erase_one_block);
+	assert(!ipmi_hiomap_init(&bl));
+	ctx = container_of(bl, struct ipmi_hiomap, bl);
+	len = 1 << ctx->block_size_shift;
+	assert(!bl->erase(bl, 0, len));
+	ipmi_hiomap_exit(bl);
+	scenario_exit();
+}
+
+static const struct scenario_event
 scenario_hiomap_protocol_persistent_error[] = {
 	{ .type = scenario_event_p, .p = &hiomap_ack_call, },
 	{ .type = scenario_event_p, .p = &hiomap_get_info_call, },
@@ -1178,6 +1231,7 @@ struct test_case test_cases[] = {
 	TEST_CASE(test_hiomap_protocol_write_two_blocks),
 	TEST_CASE(test_hiomap_protocol_event_before_write),
 	TEST_CASE(test_hiomap_protocol_event_during_write),
+	TEST_CASE(test_hiomap_protocol_erase_one_block),
 	TEST_CASE(test_hiomap_protocol_persistent_error),
 	{ NULL, NULL },
 };
