9ec4f741dda499af1b5fce4aae07ae536ed31b47
Andrew Jeffery <andrew@aj.id.au>
2019-02-21 16:58:49 +1030

    test-ipmi-hiomap: Add read-one-block-twice test
    [ Upstream commit b94bb54f6569af09e2b866871acc192862670126 ]
    
    Cc: stable
    Signed-off-by: Andrew Jeffery <andrew@aj.id.au>
    Signed-off-by: Stewart Smith <stewart@linux.ibm.com>
    Signed-off-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>

--- libflash/test/test-ipmi-hiomap.c
+++ libflash/test/test-ipmi-hiomap.c
@@ -927,6 +927,38 @@ static void test_hiomap_protocol_read_two_blocks(void)
 }
 
 static const struct scenario_event
+scenario_hiomap_protocol_read_one_block_twice[] = {
+	{ .type = scenario_event_p, .p = &hiomap_ack_call, },
+	{ .type = scenario_event_p, .p = &hiomap_get_info_call, },
+	{ .type = scenario_event_p, .p = &hiomap_get_flash_info_call, },
+	{
+		.type = scenario_event_p,
+		.p = &hiomap_create_read_window_qs0l1_rs0l1_call,
+	},
+	SCENARIO_SENTINEL,
+};
+
+static void test_hiomap_protocol_read_one_block_twice(void)
+{
+	struct blocklevel_device *bl;
+	struct ipmi_hiomap *ctx;
+	uint8_t *buf;
+	size_t len;
+
+	scenario_enter(scenario_hiomap_protocol_read_one_block_twice);
+	assert(!ipmi_hiomap_init(&bl));
+	ctx = container_of(bl, struct ipmi_hiomap, bl);
+	len = 1 << ctx->block_size_shift;
+	buf = calloc(1, len);
+	assert(buf);
+	assert(!bl->read(bl, 0, buf, len));
+	assert(!bl->read(bl, 0, buf, len));
+	free(buf);
+	ipmi_hiomap_exit(bl);
+	scenario_exit();
+}
+
+static const struct scenario_event
 scenario_hiomap_protocol_event_before_action[] = {
 	{ .type = scenario_event_p, .p = &hiomap_ack_call, },
 	{ .type = scenario_event_p, .p = &hiomap_get_info_call, },
@@ -2918,6 +2950,7 @@ struct test_case test_cases[] = {
 	TEST_CASE(test_hiomap_protocol_reset_recovery),
 	TEST_CASE(test_hiomap_protocol_read_one_block),
 	TEST_CASE(test_hiomap_protocol_read_two_blocks),
+	TEST_CASE(test_hiomap_protocol_read_one_block_twice),
 	TEST_CASE(test_hiomap_protocol_event_before_read),
 	TEST_CASE(test_hiomap_protocol_event_during_read),
 	TEST_CASE(test_hiomap_protocol_write_one_block),
