open-power/skiboot

commit 2c8f96534a978bb4cac3e4b7dd393a9cc4926555

hw/xscom: add missing P9P chip name
Signed-off-by: Nicholas Piggin <npiggin@gmail.com>
Signed-off-by: Stewart Smith <stewart@linux.ibm.com>

hw/xscom.c
@@ -859,7 +859,7 @@ void xscom_init(void)
 		struct proc_chip *chip;
 		const char *chip_name;
 		static const char *chip_names[] = {
-			"UNKNOWN", "P7", "P7+", "P8E", "P8", "P8NVL", "P9N", "P9C"
+			"UNKNOWN", "P7", "P7+", "P8E", "P8", "P8NVL", "P9N", "P9C", "P9"
 		};
 
 		chip = get_chip(gcid);

commit d318cdb3863fcf92288528bfed3b6e435cf6f0ef

hw/xscom: P9P rather than P9
Fixes: 2c8f96534a978bb4cac3e4b7dd393a9cc4926555
Signed-off-by: Stewart Smith <stewart@linux.ibm.com>

hw/xscom.c
@@ -859,7 +859,7 @@ void xscom_init(void)
 		struct proc_chip *chip;
 		const char *chip_name;
 		static const char *chip_names[] = {
-			"UNKNOWN", "P7", "P7+", "P8E", "P8", "P8NVL", "P9N", "P9C", "P9"
+			"UNKNOWN", "P7", "P7+", "P8E", "P8", "P8NVL", "P9N", "P9C", "P9P"
 		};
 
 		chip = get_chip(gcid);