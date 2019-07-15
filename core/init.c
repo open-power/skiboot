open-power/skiboot

commit 9fc0c1287ada4d3ba50dcb76bbc3fc9beaae0bb1

Move FSP specific op-panel calls to platform.exit()
We move the platform exit call much closer to executing the kernel,
which should all be safe, and in fact a much better time to do watchdog
related things.

Signed-off-by: Stewart Smith <stewart@linux.ibm.com>

core/init.c
@@ -534,9 +534,6 @@ void __noreturn load_and_boot_kernel(bool is_reboot)
 
 	op_display(OP_LOG, OP_MOD_INIT, 0x000A);
 
-	if (platform.exit)
-		platform.exit();
-
 	/* Load kernel LID */
 	if (!load_kernel()) {
 		op_display(OP_FATAL, OP_MOD_INIT, 1);
@@ -590,13 +587,6 @@ void __noreturn load_and_boot_kernel(bool is_reboot)
 
 	op_display(OP_LOG, OP_MOD_INIT, 0x000C);
 
-	/* Start the kernel */
-	if (!is_reboot)
-		op_panel_disable_src_echo();
-
-	/* Clear SRCs on the op-panel when Linux starts */
-	op_panel_clear_src();
-
 	mem_dump_free();
 
 	/* Dump the selected console */
@@ -610,6 +600,9 @@ void __noreturn load_and_boot_kernel(bool is_reboot)
 		prlog(PR_EMERG, "FATAL: Kernel is zeros, can't execute!\n");
 		assert(0);
 	}
+
+	if (platform.exit)
+		platform.exit();
 
 	/* Take processors out of nap */
 	cpu_set_sreset_enable(false);

platforms/ibm-fsp/common.c
@@ -193,6 +193,10 @@ void ibm_fsp_exit(void)
 
 	fsp_console_select_stdout();
 
+	op_panel_disable_src_echo();
+
+	/* Clear SRCs on the op-panel when Linux starts */
+	op_panel_clear_src();
 }
 
 int64_t ibm_fsp_cec_reboot(void)

commit f68b949dbc64ee866d630d9f873e1f725ace09c2

core/platform: Add finalise_dt() callback
Previously the platform.exit() callback was called before we created the
flattened device tree blob for Linux. Some platforms used this to add
various DT properties and this was broken in commit Fixes: 9fc0c1287ada
("Move FSP specific op-panel calls to platform.exit()") which moved the
exit callback to after the DTB had been created.

The logic for moving the time of the exit call makes some sense since we
want to terminate the IPMI watchdog timer as late as possible, but we
still need a way for the platform modify the DTB as late as possible.

This patch adds another platform callback (yay!) called finalise_dt()
which can be used to graft stuff into the DT.

Suggested-by: Vaidyanathan Srinivasan <svaidy@linux.ibm.com>
Signed-off-by: Oliver O'Halloran <oohall@gmail.com>
Reviewed-by: Vaidyanathan Srinivasan <svaidy@linux.ibm.com>
Signed-off-by: Stewart Smith <stewart@linux.ibm.com>

core/init.c
@@ -579,6 +579,9 @@ void __noreturn load_and_boot_kernel(bool is_reboot)
 
 	add_fast_reboot_dt_entries();
 
+	if (platform.finalise_dt)
+		platform.finalise_dt(is_reboot);
+
 	/* Create the device tree blob to boot OS. */
 	fdt = create_dtb(dt_root, false);
 	if (!fdt) {

include/platform.h
@@ -253,7 +253,13 @@ struct platform {
 	int		(*resource_loaded)(enum resource_id id, uint32_t idx);
 
 	/*
+	 * Executed just prior to creating the dtb for the kernel.
+	 */
+	void		(*finalise_dt)(bool is_reboot);
+
+	/*
 	 * Executed just prior to handing control over to the payload.
+	 * Used to terminate watchdogs, etc.
 	 */
 	void		(*exit)(void);
 