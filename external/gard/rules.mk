open-power/skiboot

commit 41109e5073bdd7eaf7da9eb81c824f3d79073881

gard: Use consistent name
During compilation we generates binary file as "gard" and during
installation we install it as "opal-gard". This seems to be
creating some confusion. Some people are thinking these are two
different tools. Hence lets use common name for gard tool
("opal-gard").

Cc: Oliver O'Halloran <oohall@gmail.com>
Cc: Andrew Jeffery <andrew@aj.id.au>
Signed-off-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>
Signed-off-by: Stewart Smith <stewart@linux.ibm.com>

external/gard/rules.mk
@@ -10,7 +10,7 @@ CCAN_OBJS	:= $(addprefix ccan-list-, $(CCAN_FILES:.c=.o))
 CCAN_SRC	:= $(addprefix ccan/list/,$(CCAN_FILES))
 OBJS     += $(LIBFLASH_OBJS) $(CCAN_OBJS)
 OBJS     += common-arch_flash.o
-EXE       = gard
+EXE       = opal-gard
 
 prefix = /usr/local/
 sbindir = $(prefix)/sbin

external/gard/test/add_test.sh
@@ -2,7 +2,7 @@
 #
 # this is a really dumb script for auto-generating test cases from known good-data
 #
-# usage: ./add_test <pass|fail> <inputfile> <testname> [gard subcommand]
+# usage: ./add_test <pass|fail> <inputfile> <testname> [opal-gard subcommand]
 #
 # e.g.
 #      ./add_test.sh fail blank.bin create-bad-instance create /sys256
@@ -47,7 +47,7 @@ echo "making $num-$name: f=$script_file, normally $test_type, cmd='$*'"
 cat > $script_file <<EOF
 #! /bin/sh
 
-run_binary "./gard" "-9 -p -e -f $file $*"
+run_binary "./opal-gard" "-9 -p -e -f $file $*"
 $check
 	fail_test
 fi
@@ -63,5 +63,5 @@ stderr_file="test/results/$num-$name.err"
 
 test_input="$name-$num-input"
 cp $file $test_input
-./gard -f $test_input -p -e $* 2>$stderr_file >$stdout_file
+./opal-gard -f $test_input -p -e $* 2>$stderr_file >$stdout_file
 rm -f $test_input

external/gard/test/results/02-usage.err
@@ -1,4 +1,4 @@
-Usage: ./gard [-a -e -f <file> -p] <command> [<args>]
+Usage: ./opal-gard [-a -e -f <file> -p] <command> [<args>]
 
 -8 --p8
 -9 --p9

external/gard/test/tests/00-list
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-run_binary "./gard" "-p -e -8 -f test/files/data1.bin list"
+run_binary "./opal-gard" "-p -e -8 -f test/files/data1.bin list"
 if [ "$?" -ne 0 ] ; then
 	fail_test
 fi

external/gard/test/tests/01-show_1
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-run_binary "./gard" "-p -e -8 -f test/files/data1.bin show 1"
+run_binary "./opal-gard" "-p -e -8 -f test/files/data1.bin show 1"
 if [ "$?" -ne 0 ] ; then
 	fail_test
 fi

external/gard/test/tests/02-usage
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-run_binary "./gard"
+run_binary "./opal-gard"
 if [ "$?" -ne 1 ] ; then
 	fail_test
 fi

external/gard/test/tests/03-show_1-p9
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-run_binary "./gard" "-p -e -f test/files/data-p9.bin --p9 show 1"
+run_binary "./opal-gard" "-p -e -f test/files/data-p9.bin --p9 show 1"
 if [ "$?" -ne 0 ] ; then
 	fail_test
 fi

external/gard/test/tests/04-create-bad-instance
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-run_binary "./gard" "-p -e -8 -f test/files/blank.bin create /sys256"
+run_binary "./opal-gard" "-p -e -8 -f test/files/blank.bin create /sys256"
 if [ "$?" -eq 0 ]; then
 	fail_test
 fi

commit 53baedfd19436e8cc33151a0c17cb57863f7303a

gard: fix installation rule
Use $(EXE) for the binary name instead of hard-coding. This completes
the "gard: Use consistent name" change from commit 41109e5073bd ("gard:
Use consistent name")

Fixes: 41109e5073bd ("gard: Use consistent name")
Signed-off-by: Dan Horák <dan@danny.cz>
[oliver: prefix in title, fixes tag]
Signed-off-by: Oliver O'Halloran <oohall@gmail.com>

external/gard/rules.mk
@@ -44,7 +44,7 @@ $(EXE): $(OBJS)
 	$(CC) $(LDFLAGS) $(CFLAGS) $^ -o $@
 
 install: all
-	install -D gard $(DESTDIR)$(sbindir)/opal-gard
-	install -D -m 0644 opal-gard.1 $(DESTDIR)$(mandir)/man1/opal-gard.1
+	install -D $(EXE) $(DESTDIR)$(sbindir)/$(EXE)
+	install -D -m 0644 $(EXE).1 $(DESTDIR)$(mandir)/man1/$(EXE).1
 
 