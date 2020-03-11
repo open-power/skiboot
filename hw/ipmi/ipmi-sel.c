open-power/skiboot

commit a2c74d834f66133ca0d20561ec7beb95fc06f045

ipmi: endian conversion
Signed-off-by: Nicholas Piggin <npiggin@gmail.com>
Signed-off-by: Oliver O'Halloran <oohall@gmail.com>

hw/ipmi/ipmi-sel.c
@@ -192,7 +192,7 @@ static void ipmi_init_esel_record(void)
 {
 	memset(&sel_record, 0, sizeof(struct sel_record));
 	sel_record.record_type = SEL_REC_TYPE_AMI_ESEL;
-	sel_record.generator_id = SEL_GENERATOR_ID_AMI;
+	sel_record.generator_id = cpu_to_le16(SEL_GENERATOR_ID_AMI);
 	sel_record.evm_ver = SEL_EVM_VER_2;
 	sel_record.sensor_type	= SENSOR_TYPE_SYS_EVENT;
 	sel_record.sensor_number =

commit 201019504b8ce8761809a732cea2c7f48d6b0096

esel: Fix OEM SEL generator ID
Fixes: a2c74d83 (ipmi: endian conversion)
Cc: Nicholas Piggin <npiggin@gmail.com>
Signed-off-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>
Signed-off-by: Oliver O'Halloran <oohall@gmail.com>

hw/ipmi/ipmi-sel.c
@@ -48,7 +48,7 @@
 #define SEL_REC_TYPE_AMI_ESEL	0xDF
 
 /* OEM SEL generator ID for AMI */
-#define SEL_GENERATOR_ID_AMI	0x2000
+#define SEL_GENERATOR_ID_AMI	0x0020
 
 /* IPMI SEL version */
 #define SEL_EVM_VER_1		0x03