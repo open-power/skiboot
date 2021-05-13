ff7c0852427a5fa90b6423c31410a7a711a9e3ed
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-05-13 12:18:40 +0000

    Deploy open-power/skiboot to github.com/open-power/skiboot.git:gh-pages

---
+++ doc/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'v6.7-45-g4b9ca02',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
