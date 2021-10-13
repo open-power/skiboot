open-power/skiboot

commit 11ce9612b3aab362e9139bf7a4e7198408af2832

move the __this_cpu register to r16, reserve r13-r15
There have been several bugs between Linux and OPAL caused by both
using r13 for their primary per-CPU data address. This patch moves
OPAL to use r16 for this, and prevents the compiler from touching
r13-r15 (r14,r15 allow Linux to use additional fixed registers in
future).

This helps code to be a little more robust, and may make crashes
in OPAL (or debugging with pdbg or in simulators) easier to debug by
having easy access to the PACA.

Later, if we allow interrupts (other than non-maskable) to be taken when
running in skiboot, Linux's interrupt return handler does not restore
r13 if the interrupt was taken in PR=0 state, which would corrupt the
skiboot r13 register, so this allows for the possibility, although it
will have to become a formal OPAL ABI requirement if we rely on it.

Signed-off-by: Nicholas Piggin <npiggin@gmail.com>
[oliver: x86_64 has an r13, but not an r16 so the tests broke]
Signed-off-by: Oliver O'Halloran <oohall@gmail.com>

wip: fix __this_cpu() in the test cases

asm/head.S
@@ -25,7 +25,7 @@
 	addi	stack_reg,stack_reg,EMERGENCY_CPU_STACKS_OFFSET@l;
 
 #define GET_CPU()							\
-	clrrdi	%r13,%r1,STACK_SHIFT
+	clrrdi	%r16,%r1,STACK_SHIFT
 
 #define SAVE_GPR(reg,sp)	std %r##reg,STACK_GPR##reg(sp)
 #define REST_GPR(reg,sp)	ld %r##reg,STACK_GPR##reg(sp)
@@ -403,7 +403,7 @@ boot_entry:
 	 * before relocation so we need to keep track of its location to wake
 	 * them up.
 	 */
-	mr	%r15,%r30
+	mr	%r18,%r30
 
 	/* Check if we need to copy ourselves up and update %r30 to
 	 * be our new offset
@@ -449,7 +449,7 @@ boot_entry:
 
 	/* Tell secondaries to move to second stage (relocated) spin loop */
 	LOAD_IMM32(%r3, boot_flag - __head)
-	add	%r3,%r3,%r15
+	add	%r3,%r3,%r18
 	li	%r0,1
 	stw	%r0,0(%r3)
 
@@ -464,18 +464,18 @@ boot_entry:
 	addi	%r3,%r3,8
 	bdnz	1b
 
-	/* Get our per-cpu pointer into r13 */
+	/* Get our per-cpu pointer into r16 */
 	GET_CPU()
 
 #ifdef STACK_CHECK_ENABLED
 	/* Initialize stack bottom mark to 0, it will be updated in C code */
 	li	%r0,0
-	std	%r0,CPUTHREAD_STACK_BOT_MARK(%r13)
+	std	%r0,CPUTHREAD_STACK_BOT_MARK(%r16)
 #endif
 	/* Initialize the stack guard */
 	LOAD_IMM64(%r3,STACK_CHECK_GUARD_BASE);
 	xor	%r3,%r3,%r31
-	std	%r3,0(%r13)
+	std	%r3,0(%r16)
 
 	/* Jump to C */
 	mr	%r3,%r27
@@ -536,7 +536,7 @@ secondary_not_found:
 	b	.
 
 call_relocate:
-	mflr	%r14
+	mflr	%r17
 	LOAD_IMM32(%r4,__dynamic_start - __head)
 	LOAD_IMM32(%r5,__rela_dyn_start - __head)
 	add	%r4,%r4,%r30
@@ -545,7 +545,7 @@ call_relocate:
 	bl	relocate
 	cmpwi	%r3,0
 	bne	1f
-	mtlr	%r14
+	mtlr	%r17
 	blr
 1:	/* Fatal relocate failure */
 	attn
@@ -592,12 +592,12 @@ reset_wakeup:
 	/* Get PIR */
 	mfspr	%r31,SPR_PIR
 
-	/* Get that CPU stack base and use it to restore r13 */
+	/* Get that CPU stack base and use it to restore r16 */
 	GET_STACK(%r1,%r31)
 	GET_CPU()
 
 	/* Restore original stack pointer */
-	ld	%r1,CPUTHREAD_SAVE_R1(%r13)
+	ld	%r1,CPUTHREAD_SAVE_R1(%r16)
 
 	/* Restore more stuff */
 	lwz	%r4,STACK_CR(%r1)
@@ -655,7 +655,7 @@ reset_fast_reboot_wakeup:
 	/* Get PIR */
 	mfspr	%r31,SPR_PIR
 
-	/* Get that CPU stack base and use it to restore r13 */
+	/* Get that CPU stack base and use it to restore r16 */
 	GET_STACK(%r1,%r31)
 	GET_CPU()
 
@@ -923,17 +923,17 @@ opal_entry:
 	std	%r9,STACK_GPR9(%r1)
 	std	%r10,STACK_GPR10(%r1)
 
-	/* Save Token (r0), LR and r13 */
+	/* Save Token (r0), LR and r16 */
 	mflr	%r12
 	std	%r0,STACK_GPR0(%r1)
-	std	%r13,STACK_GPR13(%r1)
+	std	%r16,STACK_GPR16(%r1)
 	std	%r12,STACK_LR(%r1)
 
 	/* Get the CPU thread */
 	GET_CPU()
 
 	/* Store token in CPU thread */
-	std	%r0,CPUTHREAD_CUR_TOKEN(%r13)
+	std	%r0,CPUTHREAD_CUR_TOKEN(%r16)
 
 	/* Mark the stack frame */
 	li	%r12,STACK_ENTRY_OPAL_API
@@ -975,14 +975,14 @@ opal_entry:
 	bl	opal_exit_check /* r3 is preserved */
 
 	/*
-	 * Restore r1 and r13 before decrementing in_opal_call.
-	 * Move per-cpu pointer to volatile r12, restore lr, r1, r13.
+	 * Restore r1 and r16 before decrementing in_opal_call.
+	 * Move per-cpu pointer to volatile r12, restore lr, r1, r16.
 	 */
 .Lreturn:
 	ld	%r12,STACK_LR(%r1)
 	mtlr	%r12
-	mr	%r12,%r13
-	ld	%r13,STACK_GPR13(%r1)
+	mr	%r12,%r16
+	ld	%r16,STACK_GPR16(%r1)
 	ld	%r1,STACK_GPR1(%r1)
 .Lreject:
 	sync 	/* release barrier vs quiescing */

Makefile.main
@@ -96,7 +96,14 @@ endif
 CFLAGS := -fno-strict-aliasing -pie -fpie -fno-pic -m64 -fno-asynchronous-unwind-tables
 CFLAGS += -mcpu=power8
 CFLAGS += -Wl,--oformat,elf64-powerpc -ggdb
-CFLAGS += $(call try-cflag,$(CC),-ffixed-r13)
+# r13,r14,r15 are preserved for OS to use as fixed registers.
+# These could be saved and restored in and out of skiboot, but it's more
+# robust to avoid touching them.
+CFLAGS += -ffixed-r13
+CFLAGS += -ffixed-r14
+CFLAGS += -ffixed-r15
+# r16 is skiboot's per-CPU data pointer.
+CFLAGS += -ffixed-r16
 CFLAGS += $(call try-cflag,$(CC),-std=gnu11)
 
 ifeq ($(LITTLE_ENDIAN),1)
@@ -127,7 +134,7 @@ endif
 
 # Check if the new parametrized stack protector option is supported
 # by gcc, otherwise disable stack protector
-STACK_PROT_CFLAGS := -mstack-protector-guard=tls -mstack-protector-guard-reg=r13
+STACK_PROT_CFLAGS := -mstack-protector-guard=tls -mstack-protector-guard-reg=r16
 STACK_PROT_CFLAGS += -mstack-protector-guard-offset=0
 HAS_STACK_PROT := $(call test_cflag,$(CC),$(STACK_PROT_CFLAGS))
 

asm/misc.S
@@ -214,7 +214,7 @@ enter_p8_pm_state:
 	bl	pm_save_regs
 
 	/* Save stack pointer in struct cpu_thread */
-	std	%r1,CPUTHREAD_SAVE_R1(%r13)
+	std	%r1,CPUTHREAD_SAVE_R1(%r16)
 
 	/* Winkle or nap ? */
 	cmpli	%cr0,0,%r3,0
@@ -222,7 +222,7 @@ enter_p8_pm_state:
 
 	/* nap sequence */
 	ptesync
-0:	ld	%r0,CPUTHREAD_SAVE_R1(%r13)
+0:	ld	%r0,CPUTHREAD_SAVE_R1(%r16)
 	cmpd	cr0,%r0,%r0
 	bne	0b
 	PPC_INST_NAP
@@ -230,7 +230,7 @@ enter_p8_pm_state:
 
 	/* rvwinkle sequence */
 1:	ptesync
-0:	ld	%r0,CPUTHREAD_SAVE_R1(%r13)
+0:	ld	%r0,CPUTHREAD_SAVE_R1(%r16)
 	cmpd	cr0,%r0,%r0
 	bne	0b
 	PPC_INST_RVWINKLE
@@ -251,7 +251,7 @@ enter_p9_pm_state:
 	bl	pm_save_regs
 
 	/* Save stack pointer in struct cpu_thread */
-	std	%r1,CPUTHREAD_SAVE_R1(%r13)
+	std	%r1,CPUTHREAD_SAVE_R1(%r16)
 
 	mtspr	SPR_PSSCR,%r3
 	PPC_INST_STOP

include/cpu.h
@@ -212,7 +212,12 @@ extern u8 get_available_nr_cores_in_chip(u32 chip_id);
 		core = next_available_core_in_chip(core, chip_id))
 
 /* Return the caller CPU (only after init_cpu_threads) */
-register struct cpu_thread *__this_cpu asm("r13");
+#ifndef __TEST__
+register struct cpu_thread *__this_cpu asm("r16");
+#else
+struct cpu_thread *__this_cpu;
+#endif
+
 static inline __nomcount struct cpu_thread *this_cpu(void)
 {
 	return __this_cpu;

commit ebef02621dedc111c9c75c98971fcb869b492052

HBRT: fix clobbered r16 when host services handlers are called
Skiboot is using r16 as a fixed register containing this CPU pointer,
but we can be called back into from hostboot via the host services
interface, where r16 may have been set by hostboot. Switch this back to
skiboot's CPU pointer before running host services handlers, and then
restore it to the hostboot value before returning.

Fixes: 11ce9612b3aa ("move the __this_cpu register to r16, reserve r13-r15")
Reported-by: Frederic Barrat <fbarrat@linux.ibm.com>
Signed-off-by: Nicholas Piggin <npiggin@gmail.com>
Reviewed-by: Frederic Barrat <fbarrat@linux.ibm.com>
Tested-by: Frederic Barrat <fbarrat@linux.ibm.com>
Signed-off-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>

asm/head.S
@@ -1124,3 +1124,13 @@ start_kernel_secondary:
 	mtspr	SPR_HSRR1,%r10
 	mfspr	%r3,SPR_PIR
 	hrfid
+
+.global restore_cpu_ptr_r16
+restore_cpu_ptr_r16:
+	GET_CPU()
+	blr
+
+.global set_cpu_ptr_r16
+set_cpu_ptr_r16:
+	mr	%r16,%r3
+	blr

include/skiboot.h
@@ -163,6 +163,11 @@ extern void start_kernel32(uint64_t entry, void* fdt,
 			   uint64_t mem_top) __noreturn;
 extern void start_kernel_secondary(uint64_t entry) __noreturn;
 
+/* Re-set r16 register with CPU pointer, based on stack (r1) value */
+extern void restore_cpu_ptr_r16(void);
+/* Set r16 register with value in 'r16' parameter */
+extern void set_cpu_ptr_r16(uint64_t r16);
+
 /* Get description of machine from HDAT and create device-tree */
 extern int parse_hdat(bool is_opal);
 

platforms/ibm-fsp/hostservices.c
@@ -168,6 +168,27 @@ struct runtime_interfaces {
 	void (*reserved[32])(void);
 };
 
+/*
+ * The host interfaces are called by hostboot, which considers r16 a regular
+ * non-volatile register that it may have used at the time of the call.
+ * Skiboot requires r16 to be set to the CPU pointer while it runs. So the
+ * host interface handlers must save HBRT's r16, switch r16 to the CPU pointer,
+ * do their thing, and then restore HBRT's r16 value before returning.
+ */
+static uint64_t host_interface_pre_fixup(void)
+{
+	uint64_t r16 = (uint64_t)__this_cpu;
+
+	restore_cpu_ptr_r16();
+
+	return r16;
+}
+
+static void host_interface_post_fixup(uint64_t r16)
+{
+	set_cpu_ptr_r16(r16);
+}
+
 static struct runtime_interfaces *hservice_runtime;
 
 static char *hbrt_con_buf = (char *)HBRT_CON_START;
@@ -187,6 +208,7 @@ static struct memcons hbrt_memcons __section(".data.memcons") = {
 
 static void hservice_putc(char c)
 {
+	uint64_t r16 = host_interface_pre_fixup();
 	uint32_t opos;
 
 	hbrt_con_buf[hbrt_con_pos++] = c;
@@ -207,15 +229,20 @@ static void hservice_putc(char c)
 		opos |= MEMCONS_OUT_POS_WRAP;
 	lwsync();
 	hbrt_memcons.out_pos = cpu_to_be32(opos);
+
+	host_interface_post_fixup(r16);
 }
 
 static void hservice_puts(const char *str)
 {
+	uint64_t r16 = host_interface_pre_fixup();
 	char c;
 
 	while((c = *(str++)) != 0)
 		hservice_putc(c);
 	hservice_putc(10);
+
+	host_interface_post_fixup(r16);
 }
 
 static void hservice_mark(void)
@@ -226,28 +253,48 @@ static void hservice_mark(void)
 
 static void hservice_assert(void)
 {
+	uint64_t r16 = host_interface_pre_fixup();
+
 	/**
 	 * @fwts-label HBRTassert
 	 * @fwts-advice HBRT triggered assert: you need to debug HBRT
 	 */
 	prlog(PR_EMERG, "HBRT: Assertion from hostservices\n");
 	abort();
+	/* Should not be reached... */
+	host_interface_post_fixup(r16);
 }
 
 static void *hservice_malloc(size_t size)
 {
-	return malloc(size);
+	uint64_t r16 = host_interface_pre_fixup();
+	void *mem;
+
+	mem = malloc(size);
+
+	host_interface_post_fixup(r16);
+
+	return mem;
 }
 
 static void hservice_free(void *ptr)
 {
+	uint64_t r16 = host_interface_pre_fixup();
 	free(ptr);
+	host_interface_post_fixup(r16);
 }
 
 
 static void *hservice_realloc(void *ptr, size_t size)
 {
-	return realloc(ptr, size);
+	uint64_t r16 = host_interface_pre_fixup();
+	void *mem;
+
+	mem = realloc(ptr, size);
+
+	host_interface_post_fixup(r16);
+
+	return mem;
 }
 
 struct hbrt_elog_ent {
@@ -319,6 +366,7 @@ static void hservice_start_elog_send(void)
 
 int hservice_send_error_log(uint32_t plid, uint32_t dsize, void *data)
 {
+	uint64_t r16 = host_interface_pre_fixup();
 	struct hbrt_elog_ent *ent;
 	void *abuf;
 
@@ -327,6 +375,9 @@ int hservice_send_error_log(uint32_t plid, uint32_t dsize, void *data)
 	/* We only know how to send error logs to FSP */
 	if (!fsp_present()) {
 		prerror("HBRT: Warning, error log from HBRT discarded !\n");
+
+		host_interface_post_fixup(r16);
+
 		return OPAL_UNSUPPORTED;
 	}
 	if (dsize > PSI_DMA_HBRT_LOG_WRITE_BUF_SZ) {
@@ -341,6 +392,9 @@ int hservice_send_error_log(uint32_t plid, uint32_t dsize, void *data)
 	ent = zalloc(sizeof(struct hbrt_elog_ent));
 	if (!ent) {
 		unlock(&hbrt_elog_lock);
+
+		host_interface_post_fixup(r16);
+
 		return OPAL_NO_MEM;
 	}
 
@@ -349,6 +403,9 @@ int hservice_send_error_log(uint32_t plid, uint32_t dsize, void *data)
 	if (!abuf) {
 		free(ent);
 		unlock(&hbrt_elog_lock);
+
+		host_interface_post_fixup(r16);
+
 		return OPAL_NO_MEM;
 	}
 	memset(abuf, 0, PSI_DMA_HBRT_LOG_WRITE_BUF_SZ);
@@ -361,21 +418,36 @@ int hservice_send_error_log(uint32_t plid, uint32_t dsize, void *data)
 		hservice_start_elog_send();
 	unlock(&hbrt_elog_lock);
 
+	host_interface_post_fixup(r16);
+
 	return 0;
 }
 
 static int hservice_scom_read(uint64_t chip_id, uint64_t addr, void *buf)
 {
-	return xscom_read(chip_id, addr, buf);
+	uint64_t r16 = host_interface_pre_fixup();
+	int ret;
+
+	ret = xscom_read(chip_id, addr, buf);
+
+	host_interface_post_fixup(r16);
+
+	return ret;
 }
 
 static int hservice_scom_write(uint64_t chip_id, uint64_t addr,
 			       const void *buf)
 {
+	uint64_t r16 = host_interface_pre_fixup();
 	uint64_t val;
+	int ret;
 
 	memcpy(&val, buf, sizeof(val));
-	return xscom_write(chip_id, addr, val);
+	ret = xscom_write(chip_id, addr, val);
+
+	host_interface_post_fixup(r16);
+
+	return ret;
 }
 
 struct hbrt_lid {
@@ -475,6 +547,7 @@ void hservices_lid_preload(void)
 
 static int hservice_lid_load(uint32_t lid, void **buf, size_t *len)
 {
+	uint64_t r16 = host_interface_pre_fixup();
 	struct hbrt_lid *hlid;
 
 	prlog(PR_INFO, "HBRT: Lid load request for 0x%08x\n", lid);
@@ -494,14 +567,24 @@ static int hservice_lid_load(uint32_t lid, void **buf, size_t *len)
 			*len = hlid->len;
 			prlog(PR_DEBUG, "HBRT: LID Serviced from cache,"
 			      " %x, len=0x%lx\n", hlid->id, hlid->len);
+
+			host_interface_post_fixup(r16);
+
 			return 0;
 		}
 	}
+
+	host_interface_post_fixup(r16);
+
 	return -ENOENT;
 }
 
 static int hservice_lid_unload(void *buf __unused)
 {
+	uint64_t r16 = host_interface_pre_fixup();
+
+	host_interface_post_fixup(r16);
+
 	/* We do nothing as the LID is held in cache */
 	return 0;
 }
@@ -526,15 +609,19 @@ static uint64_t hservice_get_reserved_mem(const char *name)
 
 static void hservice_nanosleep(uint64_t i_seconds, uint64_t i_nano_seconds)
 {
+	uint64_t r16 = host_interface_pre_fixup();
 	struct timespec ts;
 
 	ts.tv_sec = i_seconds;
 	ts.tv_nsec = i_nano_seconds;
 	nanosleep_nopoll(&ts, NULL);
+
+	host_interface_post_fixup(r16);
 }
 
 int hservice_wakeup(uint32_t i_core, uint32_t i_mode)
 {
+	uint64_t r16 = host_interface_pre_fixup();
 	struct cpu_thread *cpu;
 	int rc = OPAL_SUCCESS;
 
@@ -556,6 +643,8 @@ int hservice_wakeup(uint32_t i_core, uint32_t i_mode)
 		i_core <<= 2;
 		break;
 	default:
+		host_interface_post_fixup(r16);
+
 		return OPAL_UNSUPPORTED;
 	}
 
@@ -563,32 +652,49 @@ int hservice_wakeup(uint32_t i_core, uint32_t i_mode)
 	switch(i_mode) {
 	case 0: /* Assert special wakeup */
 		cpu = find_cpu_by_pir(i_core);
-		if (!cpu)
+		if (!cpu) {
+			host_interface_post_fixup(r16);
+
 			return OPAL_PARAMETER;
+		}
 		prlog(PR_TRACE, "HBRT: Special wakeup assert for core 0x%x,"
 		      " count=%d\n", i_core, cpu->hbrt_spec_wakeup);
 		if (cpu->hbrt_spec_wakeup == 0)
 			rc = dctl_set_special_wakeup(cpu);
 		if (rc == 0)
 			cpu->hbrt_spec_wakeup++;
+
+		host_interface_post_fixup(r16);
+
 		return rc;
+
 	case 1: /* Deassert special wakeup */
 		cpu = find_cpu_by_pir(i_core);
-		if (!cpu)
+		if (!cpu) {
+			host_interface_post_fixup(r16);
+
 			return OPAL_PARAMETER;
+		}
 		prlog(PR_TRACE, "HBRT: Special wakeup release for core"
 		      " 0x%x, count=%d\n", i_core, cpu->hbrt_spec_wakeup);
 		if (cpu->hbrt_spec_wakeup == 0) {
 			prerror("HBRT: Special wakeup clear"
 				" on core 0x%x with count=0\n",
 				i_core);
+
+			host_interface_post_fixup(r16);
+
 			return OPAL_WRONG_STATE;
 		}
 		/* What to do with count on errors ? */
 		cpu->hbrt_spec_wakeup--;
 		if (cpu->hbrt_spec_wakeup == 0)
 			rc = dctl_clear_special_wakeup(cpu);
+
+		host_interface_post_fixup(r16);
+
 		return rc;
+
 	case 2: /* Clear all special wakeups */
 		prlog(PR_DEBUG, "HBRT: Special wakeup release for all cores\n");
 		for_each_cpu(cpu) {
@@ -598,8 +704,15 @@ int hservice_wakeup(uint32_t i_core, uint32_t i_mode)
 				dctl_clear_special_wakeup(cpu);
 			}
 		}
+
+		host_interface_post_fixup(r16);
+
 		return OPAL_SUCCESS;
+
 	default:
+
+		host_interface_post_fixup(r16);
+
 		return OPAL_PARAMETER;
 	}
 }