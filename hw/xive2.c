open-power/skiboot

commit cd12ea6d8e1f384ef22c50777cb06b059fc3734c

interrupts: Do not advertise XICS support on P10
We only support the XIVE interface.

Signed-off-by: Cédric Le Goater <clg@kaod.org>
Signed-off-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>

hw/xive2.c
@@ -1975,6 +1975,11 @@ static void xive_create_mmio_dt_node(struct xive *x)
 
 }
 
+uint32_t xive2_get_phandle(void)
+{
+	return xive_dt_node->phandle;
+}
+
 static void xive_setup_forward_ports(struct xive *x, struct proc_chip *remote_chip)
 {
 	struct xive *remote_xive = remote_chip->xive;

core/interrupts.c
@@ -18,6 +18,7 @@
 #include <timer.h>
 #include <sbe-p8.h>
 #include <sbe-p9.h>
+#include <xive.h>
 
 /* ICP registers */
 #define ICP_XIRR		0x4	/* 32-bit access */
@@ -157,9 +158,14 @@ uint32_t get_psi_interrupt(uint32_t chip_id)
 
 struct dt_node *add_ics_node(void)
 {
-	struct dt_node *ics = dt_new_addr(dt_root, "interrupt-controller", 0);
+	struct dt_node *ics;
 	bool has_xive;
+	bool has_xive_only = proc_gen >= proc_gen_p10;
 
+	if (has_xive_only)
+		return NULL;
+
+	ics = dt_new_addr(dt_root, "interrupt-controller", 0);
 	if (!ics)
 		return NULL;
 
@@ -181,6 +187,10 @@ struct dt_node *add_ics_node(void)
 uint32_t get_ics_phandle(void)
 {
 	struct dt_node *i;
+	bool has_xive_only = proc_gen >= proc_gen_p10;
+
+	if (has_xive_only)
+		return xive2_get_phandle();
 
 	for (i = dt_first(dt_root); i; i = dt_next(dt_root, i)) {
 		if (streq(i->name, "interrupt-controller@0")) {

include/xive.h
@@ -79,6 +79,7 @@ bool xive2_cap_phb_pq_disable(void);
 bool xive2_cap_phb_abt(void);
 bool xive2_cap_store_eoi(void);
 int64_t xive2_reset(void);
+uint32_t xive2_get_phandle(void);
 
 uint32_t xive2_alloc_hw_irqs(uint32_t chip_id, uint32_t count, uint32_t align);
 uint32_t xive2_alloc_ipi_irqs(uint32_t chip_id, uint32_t count, uint32_t align);

commit d93ddbd39b4eeac0bc11dacbdadea76df2996c13

xive/p10:: Declare xive2 DT node as an interrupt-controller
This patch fixes errors seen when linux looks for the interrupt to use
for a device LSI:

     pci X:Y:Z of_irq_parse_pci: failed with rc=-22

The of/irq parsing code requires those new properties to be able to
map the interrupt specifier correctly. It was not needed before
comitting cd12ea6d8e1 ("interrupts: Do not advertise XICS support on
P10"), because the LSI mapping code was defaulting to the XICS
interrupt controller node, which is now removed (and had those
properties).

Fixes: cd12ea6d8e1 ("interrupts: Do not advertise XICS support on P10")
Signed-off-by: Frederic Barrat <fbarrat@linux.ibm.com>
Reviewed-by: Cédric Le Goater <clg@kaod.org>
Signed-off-by: Vasant Hegde <hegdevasant@linux.vnet.ibm.com>

hw/xive2.c
@@ -1955,7 +1955,12 @@ static void xive_create_mmio_dt_node(struct xive *x)
 			     tb + 3 * stride, stride);
 
 	dt_add_property_strings(xive_dt_node, "compatible",
-				"ibm,opal-xive-pe", "ibm,opal-intc");
+				"ibm,opal-xive-pe", "ibm,opal-xive-vc",
+				"ibm,opal-intc");
+
+	dt_add_property(xive_dt_node, "interrupt-controller", NULL, 0);
+	dt_add_property_cells(xive_dt_node, "#address-cells", 0);
+	dt_add_property_cells(xive_dt_node, "#interrupt-cells", 2);
 
 	dt_add_property_cells(xive_dt_node, "ibm,xive-eq-sizes",
 			      12, 16, 21, 24);