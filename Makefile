open-power/skiboot

commit 9cd556ca1e5f3371662b83388eda2c52fc3ea092

Makefile: Search for distro-provided cross-compiler
Search for different variations of cross-compiler prefixes that might be in
the PATH, in this order of preference:

1. powerpc64-linux-gcc
2. powerpc64le-linux-gcc
3. powerpc64-linux-gnu-gcc
4. powerpc64le-linux-gnu-gcc

The latter two are available as distro packages in at least Debian, Fedora
and Ubuntu.

Tested with GNU Make 3.82 (CentOS 7) and GNU make 4.2.1 (Fedora 30).

Signed-off-by: Thiago Jung Bauermann <bauerman@linux.ibm.com>
Reviewed-by: Andrew Donnellan <ajd@linux.ibm.com>
Tested-by: Andrew Donnellan <ajd@linux.ibm.com>
Signed-off-by: Oliver O'Halloran <oohall@gmail.com>

Makefile
@@ -16,7 +16,15 @@ ifdef CROSS_COMPILE
 endif
 ifneq ("$(ARCH)", "ppc64")
 ifneq ("$(ARCH)", "ppc64le")
+ifneq ($(shell which powerpc64-linux-gcc 2> /dev/null),)
 	CROSS ?= powerpc64-linux-
+else ifneq ($(shell which powerpc64le-linux-gcc 2> /dev/null),)
+	CROSS ?= powerpc64le-linux-
+else ifneq ($(shell which powerpc64-linux-gnu-gcc 2> /dev/null),)
+	CROSS ?= powerpc64-linux-gnu-
+else ifneq ($(shell which powerpc64le-linux-gnu-gcc 2> /dev/null),)
+	CROSS ?= powerpc64le-linux-gnu-
+endif
 endif
 endif
 

commit 3ffa501c4cfb58b6903014227247da1bdf79fd7e

Makefile: Fix detection of cross compiler
The Fedora containers don't have 'which' installed. This means the
detection of the cross compiler gives false negatives, leaving CROSS
undefined.

Instead use command -v, which outputs the path of the executable if it
exists.

Fixes: 9cd556ca1e5f ("Makefile: Search for distro-provided cross-compiler")
Signed-off-by: Joel Stanley <joel@jms.id.au>
Signed-off-by: Cédric Le Goater <clg@kaod.org>

Makefile
@@ -16,13 +16,13 @@ ifdef CROSS_COMPILE
 endif
 ifneq ("$(ARCH)", "ppc64")
 ifneq ("$(ARCH)", "ppc64le")
-ifneq ($(shell which powerpc64-linux-gcc 2> /dev/null),)
+ifneq ($(shell command -v powerpc64-linux-gcc 2> /dev/null),)
 	CROSS ?= powerpc64-linux-
-else ifneq ($(shell which powerpc64le-linux-gcc 2> /dev/null),)
+else ifneq ($(shell command -v powerpc64le-linux-gcc 2> /dev/null),)
 	CROSS ?= powerpc64le-linux-
-else ifneq ($(shell which powerpc64-linux-gnu-gcc 2> /dev/null),)
+else ifneq ($(shell command -v powerpc64-linux-gnu-gcc 2> /dev/null),)
 	CROSS ?= powerpc64-linux-gnu-
-else ifneq ($(shell which powerpc64le-linux-gnu-gcc 2> /dev/null),)
+else ifneq ($(shell command -v powerpc64le-linux-gnu-gcc 2> /dev/null),)
 	CROSS ?= powerpc64le-linux-gnu-
 endif
 endif