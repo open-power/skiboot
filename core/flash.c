open-power/skiboot

commit 27fcf2fa8350f4c326e37603674242d36e786975

Expose PNOR Flash partitions to host MTD driver via devicetree
This makes it possible for the host to directly address each
partition without requiring each application to directly parse
the FFS headers.  This has been in use for some time already to
allow BOOTKERNFW partition updates from the host.

Signed-off-by: Timothy Pearson <tpearson@raptorengineering.com>
Signed-off-by: Stewart Smith <stewart@linux.ibm.com>

core/flash.c
@@ -1,4 +1,5 @@
 /* Copyright 2013-2018 IBM Corp.
+ * Copyright 2018 Raptor Engineering, LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -39,6 +40,19 @@ struct flash {
 	uint64_t		size;
 	uint32_t		block_size;
 	int			id;
+};
+
+static struct {
+	enum resource_id	id;
+	uint32_t		subid;
+	char			name[PART_NAME_MAX+1];
+} part_name_map[] = {
+	{ RESOURCE_ID_KERNEL,	RESOURCE_SUBID_NONE,		"BOOTKERNEL" },
+	{ RESOURCE_ID_INITRAMFS,RESOURCE_SUBID_NONE,		"ROOTFS" },
+	{ RESOURCE_ID_CAPP,	RESOURCE_SUBID_SUPPORTED,	"CAPP" },
+	{ RESOURCE_ID_IMA_CATALOG,  RESOURCE_SUBID_SUPPORTED,	"IMA_CATALOG" },
+	{ RESOURCE_ID_VERSION,	RESOURCE_SUBID_NONE,		"VERSION" },
+	{ RESOURCE_ID_KERNEL_FW,	RESOURCE_SUBID_NONE,		"BOOTKERNFW" },
 };
 
 static LIST_HEAD(flashes);
@@ -206,7 +220,15 @@ static int flash_nvram_probe(struct flash *flash, struct ffs_handle *ffs)
 
 static struct dt_node *flash_add_dt_node(struct flash *flash, int id)
 {
+	int i;
+	int rc;
+	const char *name;
+	bool ecc;
+	struct ffs_handle *ffs;
+	int ffs_part_num, ffs_part_start, ffs_part_size;
 	struct dt_node *flash_node;
+	struct dt_node *partition_container_node;
+	struct dt_node *partition_node;
 
 	flash_node = dt_new_addr(opal_node, "flash", id);
 	dt_add_property_strings(flash_node, "compatible", "ibm,opal-flash");
@@ -220,6 +242,48 @@ static struct dt_node *flash_add_dt_node(struct flash *flash, int id)
 	/* we fix to 32-bits */
 	dt_add_property_cells(flash_node, "#address-cells", 1);
 	dt_add_property_cells(flash_node, "#size-cells", 1);
+
+	/* Add partition container node */
+	partition_container_node = dt_new(flash_node, "partitions");
+	dt_add_property_strings(partition_container_node, "compatible", "fixed-partitions");
+
+	/* we fix to 32-bits */
+	dt_add_property_cells(partition_container_node, "#address-cells", 1);
+	dt_add_property_cells(partition_container_node, "#size-cells", 1);
+
+	/* Add partitions */
+	for (i = 0, name = NULL; i < ARRAY_SIZE(part_name_map); i++) {
+		name = part_name_map[i].name;
+
+		rc = ffs_init(0, flash->size, flash->bl, &ffs, 1);
+		if (rc) {
+			prerror("FLASH: Can't open ffs handle\n");
+			continue;
+		}
+
+		rc = ffs_lookup_part(ffs, name, &ffs_part_num);
+		if (rc) {
+			/* This is not an error per-se, some partitions
+			 * are purposefully absent, don't spam the logs
+			 */
+		        prlog(PR_DEBUG, "FLASH: No %s partition\n", name);
+			continue;
+		}
+		rc = ffs_part_info(ffs, ffs_part_num, NULL,
+				   &ffs_part_start, NULL, &ffs_part_size, &ecc);
+		if (rc) {
+			prerror("FLASH: Failed to get %s partition info\n", name);
+			continue;
+		}
+
+		partition_node = dt_new_addr(partition_container_node, "partition", ffs_part_start);
+		dt_add_property_strings(partition_node, "label", name);
+		dt_add_property_cells(partition_node, "reg", ffs_part_start, ffs_part_size);
+	}
+
+	partition_node = dt_new_addr(partition_container_node, "partition", 0);
+	dt_add_property_strings(partition_node, "label", "PNOR");
+	dt_add_property_cells(partition_node, "reg", 0, flash->size);
 
 	return flash_node;
 }
@@ -431,18 +495,6 @@ opal_call(OPAL_FLASH_WRITE, opal_flash_write, 5);
 opal_call(OPAL_FLASH_ERASE, opal_flash_erase, 4);
 
 /* flash resource API */
-static struct {
-	enum resource_id	id;
-	uint32_t		subid;
-	char			name[PART_NAME_MAX+1];
-} part_name_map[] = {
-	{ RESOURCE_ID_KERNEL,	RESOURCE_SUBID_NONE,		"BOOTKERNEL" },
-	{ RESOURCE_ID_INITRAMFS,RESOURCE_SUBID_NONE,		"ROOTFS" },
-	{ RESOURCE_ID_CAPP,	RESOURCE_SUBID_SUPPORTED,	"CAPP" },
-	{ RESOURCE_ID_IMA_CATALOG,  RESOURCE_SUBID_SUPPORTED,	"IMA_CATALOG" },
-	{ RESOURCE_ID_VERSION,	RESOURCE_SUBID_NONE,		"VERSION" },
-};
-
 const char *flash_map_resource_name(enum resource_id id)
 {
 	int i;

include/platform.h
@@ -30,6 +30,7 @@ enum resource_id {
 	RESOURCE_ID_CAPP,
 	RESOURCE_ID_IMA_CATALOG,
 	RESOURCE_ID_VERSION,
+	RESOURCE_ID_KERNEL_FW,
 };
 #define RESOURCE_SUBID_NONE 0
 #define RESOURCE_SUBID_SUPPORTED 1

commit 124d1995ba07a992678c2472fc4d28b5b591b75f

flash: Make PNOR partition encoding size 64 bit safe.
Similar to commit: c043065cf923 ("flash: Make size 64 bit safe")
update the encoding for PNOR partition which is the partition that is mapping
the full disk 64 bit safe.

Without this mambo disk larger than 4G fails to mount with the below error:
[    2.075170] EXT4-fs (mtdblock0): bad geometry: block count 7864320 exceeds size of device (524288 blocks)

Fixes: 27fcf2fa8350 ("Expose PNOR Flash partitions to host MTD driver via devicetree")
Signed-off-by: Aneesh Kumar K.V <aneesh.kumar@linux.ibm.com>
Signed-off-by: Reza Arbab <arbab@linux.ibm.com>

core/flash.c
@@ -363,23 +363,22 @@ static struct dt_node *flash_add_dt_node(struct flash *flash, int id)
 	flash_node = dt_new_addr(opal_node, "flash", id);
 	dt_add_property_strings(flash_node, "compatible", "ibm,opal-flash");
 	dt_add_property_cells(flash_node, "ibm,opal-id", id);
+	/*
+	 * linux kernel reads this as u64 and not address-cells and size-cells encoded
+	 * value.
+	 */
 	dt_add_property_u64(flash_node, "reg", flash->size);
 	dt_add_property_cells(flash_node, "ibm,flash-block-size",
 			flash->block_size);
 	if (flash->no_erase)
 		dt_add_property(flash_node, "no-erase", NULL, 0);
 
-	/* we fix to 32-bits */
-	dt_add_property_cells(flash_node, "#address-cells", 1);
-	dt_add_property_cells(flash_node, "#size-cells", 1);
-
 	/* Add partition container node */
 	partition_container_node = dt_new(flash_node, "partitions");
 	dt_add_property_strings(partition_container_node, "compatible", "fixed-partitions");
 
-	/* we fix to 32-bits */
-	dt_add_property_cells(partition_container_node, "#address-cells", 1);
-	dt_add_property_cells(partition_container_node, "#size-cells", 1);
+	dt_add_property_cells(partition_container_node, "#address-cells", 2);
+	dt_add_property_cells(partition_container_node, "#size-cells", 2);
 
 	/* Add partitions */
 	for (i = 0, name = NULL; i < ARRAY_SIZE(part_name_map); i++) {
@@ -408,7 +407,12 @@ static struct dt_node *flash_add_dt_node(struct flash *flash, int id)
 
 		partition_node = dt_new_addr(partition_container_node, "partition", ffs_part_start);
 		dt_add_property_strings(partition_node, "label", name);
-		dt_add_property_cells(partition_node, "reg", ffs_part_start, ffs_part_size);
+		/*
+		 * flash API supports only 32 bit offset and size. So encode
+		 * the high bits as zero.
+		 */
+		dt_add_property_u64s(partition_node, "reg", ffs_part_start, ffs_part_size);
+
 		if (part_name_map[i].id != RESOURCE_ID_KERNEL_FW) {
 			/* Mark all partitions other than the full PNOR and the boot kernel
 			 * firmware as read only.  These two partitions are the only partitions
@@ -420,7 +424,11 @@ static struct dt_node *flash_add_dt_node(struct flash *flash, int id)
 
 	partition_node = dt_new_addr(partition_container_node, "partition", 0);
 	dt_add_property_strings(partition_node, "label", "PNOR");
-	dt_add_property_cells(partition_node, "reg", 0, flash->size);
+	/*
+	 * we can find larger size here due to support for more than 4G
+	 * mambo bogus disk. So encode them correctly.
+	 */
+	dt_add_property_u64s(partition_node, "reg", 0, flash->size);
 
 	return flash_node;
 }