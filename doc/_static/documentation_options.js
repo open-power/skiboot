69d88624c3610326a023380484c68991bf8a13e6
rpgrimm <ryan.grimm@gmail.com>
2023-11-27 17:53:47 +0000

    Deploying to gh-pages from @ open-power/skiboot@48265d110bba2493c503f1090e969dbd53c4e264 🚀

--- doc/_static/documentation_options.js
+++ doc/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '5c1dc62',
+    VERSION: '48265d1',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
