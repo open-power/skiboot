d063671fd8b8c0c4f7dd2ed34a01061cd93cbfc2
rarbab <arbab@panix.com>
2024-01-02 21:04:17 +0000

    Deploying to gh-pages from @ open-power/skiboot@326a4666bd69ca071c4f7d2eacf868b11099baee 🚀

--- doc/_static/documentation_options.js
+++ doc/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '48265d1',
+    VERSION: '326a466',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
